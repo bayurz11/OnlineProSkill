e0e77a4e5a3d19ebe0333b3309f751a67ba6cdc4
Bayu <bayurz1001@gmail.com>
2024-06-09 21:35:46 +0700

    up

---
+++ app/Http/Middleware/CheckAuth.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Check if user is authenticated
+        if (!auth()->check()) {
+            // User is not authenticated, redirect to login page
+            return redirect()->route('login');
+        }
+        return $next($request);
+    }
+}
