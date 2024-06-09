363ba1a1231015b136da6c2bf42939e57b0d3755
Bayu <bayurz1001@gmail.com>
2024-06-09 20:25:21 +0700

    up

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -16,7 +16,7 @@ class AuthController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            'password' => 'required|min:6|confirmed',
             // 'password_confirmation' => 'required|string|min:6|same:password',
         ]);
 
