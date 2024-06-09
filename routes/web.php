fb90243979751d219bc46321eb0a34561f9c62ed
Bayu <bayurz1001@gmail.com>
2024-06-09 16:01:51 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', [AuthController::class, 'index'])->name('login');
