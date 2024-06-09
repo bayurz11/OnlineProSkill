5f9059cebc3aac2fbba20d166c8137198ad8235b
Bayu <bayurz1001@gmail.com>
2024-06-09 19:19:22 +0700

    up

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,82 +12,74 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    // public function register(Request $request)
-    // {
-    //     // Validasi data yang diterima
-    //     $validator = Validator::make($request->all(), [
-    //         'name' => 'required|string|max:255',
-    //         'email' => 'required|string|email|max:255|unique:users',
-    //         'password' => 'required|string|min:6',
-    //         'password_confirmation' => 'required|string|min:6|same:password',
-    //     ]);
-
-    //     if ($validator->fails()) {
-    //         return response()->json([
-    //             'success' => false,
-    //             'massage' => 'Ada Kesalahan',
-    //             'data' => $validator->errors()
-    //         ]);
-    //     }
-    //     $input = $request->all();
-    //     $input['password'] = bcrypt($input['password']);
-    //     // Buat pengguna baru
-    //     $user = User::create($input);
-
-    //     $success['token'] = $user->createToken('auth_token')->plainTextToken;
-    //     $success['name'] = $user->name;
-
-    //     // Kembalikan data pengguna dan token
-    //     return response()->json([
-    //         'success' => true,
-    //         'massage' => 'Sukses Resgistrasi',
-    //         'data' => $success
-    //     ]);
-    // }
     public function register(Request $request)
     {
         // Validasi data yang diterima
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            'password' => 'required|string|min:6',
+            'password_confirmation' => 'required|string|min:6|same:password',
         ]);
 
         if ($validator->fails()) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Ada Kesalahan',
-                'data' => $validator->errors()
-            ]);
+            return redirect()->route('registrasi')->withErrors($validator)->withInput();
         }
-
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
         // Buat pengguna baru
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
+        $user = User::create($input);
 
-        // Tambahkan pengguna baru ke dalam tabel user_roles
-        $userRole = new UserRoles();
-        $userRole->user_id = $user->id;
-        $userRole->role_id = 1; // Sesuaikan dengan ID role yang sesuai
-        $userRole->save();
-
-        // Tambahkan user_id ke dalam tabel user_profiles
-        $userProfile = new UserProfile();
-        $userProfile->user_id = $user->id; // Masukkan user_id baru
-        $userProfile->save();
-
-        // Buat token
         $success['token'] = $user->createToken('auth_token')->plainTextToken;
         $success['name'] = $user->name;
 
         // Kembalikan data pengguna dan token
-        return response()->json([
-            'success' => true,
-            'message' => 'Sukses Registrasi',
-            'data' => $success
-        ]);
+        return redirect()->route('login')->with('success', 'Registrasi berhasil. Silakan login.');
     }
+    // public function register(Request $request)
+    // {
+    //     // Validasi data yang diterima
+    //     $validator = Validator::make($request->all(), [
+    //         'name' => 'required|string|max:255',
+    //         'email' => 'required|string|email|max:255|unique:users',
+    //         'password' => 'required|string|min:6|confirmed',
+    //     ]);
+
+    //     if ($validator->fails()) {
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'Ada Kesalahan',
+    //             'data' => $validator->errors()
+    //         ]);
+    //     }
+
+    //     // Buat pengguna baru
+    //     $user = User::create([
+    //         'name' => $request->name,
+    //         'email' => $request->email,
+    //         'password' => bcrypt($request->password),
+    //     ]);
+
+    //     // Tambahkan pengguna baru ke dalam tabel user_roles
+    //     $userRole = new UserRoles();
+    //     $userRole->user_id = $user->id;
+    //     $userRole->role_id = 1; // Sesuaikan dengan ID role yang sesuai
+    //     $userRole->save();
+
+    //     // Tambahkan user_id ke dalam tabel user_profiles
+    //     $userProfile = new UserProfile();
+    //     $userProfile->user_id = $user->id; // Masukkan user_id baru
+    //     $userProfile->save();
+
+    //     // Buat token
+    //     $success['token'] = $user->createToken('auth_token')->plainTextToken;
+    //     $success['name'] = $user->name;
+
+    //     // Kembalikan data pengguna dan token
+    //     return response()->json([
+    //         'success' => true,
+    //         'message' => 'Sukses Registrasi',
+    //         'data' => $success
+    //     ]);
+    // }
 }
