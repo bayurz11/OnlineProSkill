257800cb1f5a9bcadf36c8eb5f89d686d51d6e7e
Bayu <bayurz1001@gmail.com>
2024-06-09 21:14:27 +0700

    up

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,34 +12,6 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    // public function register(Request $request)
-    // {
-    //     // Validasi data yang diterima
-    //     $validator = Validator::make($request->all(), [
-    //         'name' => 'required|string|max:255',
-    //         'email' => 'required|string|email|max:255|unique:users',
-    //         'password' => 'required|string|min:6|confirmed',
-    //     ]);
-
-    //     if ($validator->fails()) {
-    //         return redirect()->route('registrasi')->withErrors($validator)->withInput();
-    //     }
-
-    //     $input = $request->all();
-    //     $input['password'] = bcrypt($input['password']);
-    //     $user = User::create($input);
-
-    //     $success['token'] = $user->createToken('auth_token')->plainTextToken;
-    //     $success['name'] = $user->name;
-
-    //     // Kembalikan data pengguna dan token
-    //     return redirect()->route('login')->with([
-    //         'success' => true,
-    //         'message' => 'Sukses Registrasi',
-    //         'data' => $success
-    //     ]);
-    // }
-
     public function register(Request $request)
     {
         // Validasi data yang diterima
@@ -53,29 +25,21 @@ class AuthController extends Controller
             return redirect()->route('registrasi')->withErrors($validator)->withInput();
         }
 
-        // Membuat pengguna baru
-        $user = User::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => bcrypt($request->input('password')),
-        ]);
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
 
-        // Simpan peran pengguna
-        $user->role()->attach(1); // Sesuaikan dengan ID peran yang sesuai
+        $success['token'] = $user->createToken('auth_token')->plainTextToken;
+        $success['name'] = $user->name;
 
-        // Simpan profil pengguna (jika diperlukan)
-        UserProfile::create([
-            'user_id' => $user->id,
-            'role_id' => 1, // Sesuaikan dengan ID peran yang sesuai
-            // Tambahkan kolom lain yang perlu disimpan di sini
-        ]);
-
-        // Redirect ke halaman login dengan pesan sukses
+        // Kembalikan data pengguna dan token
         return redirect()->route('login')->with([
             'success' => true,
             'message' => 'Sukses Registrasi',
+            'data' => $success
         ]);
     }
+
 
 
 
