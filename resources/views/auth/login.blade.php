82a8898820f0e1d520c368ee3a763afa17902b49
Bayu <bayurz1001@gmail.com>
2024-06-12 19:25:53 +0700

    up

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,22 +19,22 @@
     <!-- End fonts -->
 
     <!-- core:css -->
-    <link rel="stylesheet" href="{{ asset('assets/vendors/core/core.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets_admin/vendors/core/core.css') }}">
     <!-- endinject -->
 
     <!-- Plugin css for this page -->
     <!-- End plugin css for this page -->
 
     <!-- inject:css -->
-    <link rel="stylesheet" href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets_admin/fonts/feather-font/css/iconfont.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets_admin/vendors/flag-icon-css/css/flag-icon.min.css') }}">
     <!-- endinject -->
 
     <!-- Layout styles -->
-    <link rel="stylesheet" href="{{ asset('assets/css/demo1/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets_admin/css/demo1/style.css') }}">
     <!-- End layout styles -->
 
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('public/assets_admin/images/favicon.png') }}" />
 
     @if (session('success'))
         <div id="success-message" class="notify alert alert-success" role="alert">
@@ -181,15 +181,15 @@
     </div>
 
     <!-- core:js -->
-    <script src="{{ asset('assets/vendors/core/core.js') }}"></script>
+    <script src="{{ asset('public/assets_admin/vendors/core/core.js') }}"></script>
     <!-- endinject -->
 
     <!-- Plugin js for this page -->
     <!-- End plugin js for this page -->
 
     <!-- inject:js -->
-    <script src="{{ asset('assets/vendors/feather-icons/feather.min.js') }}"></script>
-    <script src="{{ asset('assets/js/template.js') }}"></script>
+    <script src="{{ asset('public/assets_admin/vendors/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ asset('public/assets_admin/js/template.js') }}"></script>
     <!-- endinject -->
 
     <!-- Custom js for this page -->
