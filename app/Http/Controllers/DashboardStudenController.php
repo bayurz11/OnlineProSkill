c28330c264374b8e053fa8e9f9b562482f4de199
Bayu <bayurz1001@gmail.com>
2024-06-12 20:23:27 +0700

    up

--- app/Http/Controllers/DashboardStudenController.php
+++ app/Http/Controllers/DashboardStudenController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Carbon;
 use App\Models\User;
 use App\Models\UserRoles;
 use App\Models\UserProfile;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardStudenController extends Controller
@@ -36,6 +38,7 @@ class DashboardStudenController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
+            'last_login' => Carbon::now(),
         ]);
 
         $userRole = new UserRoles();
