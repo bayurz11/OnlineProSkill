b560ac976b0056d9405c7a71681dda11e37e7d7b
Bayu <bayurz1001@gmail.com>
2024-06-12 21:03:03 +0700

    up

---
+++ resources/views/layout/partials/admin/nav_admin.blade.php
@@ -0,0 +1,211 @@
+<nav class="sidebar">
+    <div class="sidebar-header">
+        <a href="{{ route('dashboard') }}" class="sidebar-brand">
+            <img src="public/assets_admin/images/logo1.svg" alt="Logo" style="height: 40px;">
+        </a>
+        <div class="sidebar-toggler not-active">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <div class="sidebar-body">
+        <ul class="nav">
+            <li class="nav-item nav-category">Main</li>
+            <li class="nav-item">
+                <a href="{{ route('dashboard') }}" class="nav-link">
+                    <i class="link-icon" data-feather="box"></i>
+                    <span class="link-title">Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item nav-category">Pengaturan</li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#emails" role="button" aria-expanded="false"
+                    aria-controls="emails">
+                    <i class="link-icon" data-feather="settings"></i>
+                    <span class="link-title">Halaman Beranda</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="emails">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="{{ route('HeroSectionSetting') }}" class="nav-link">Hero Section Setting</a>
+                        </li>
+                        {{-- <li class="nav-item">
+                            <a href="pages/email/compose.html" class="nav-link">Compose</a>
+                        </li> --}}
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a href="{{ route('artikel_setting') }}" class="nav-link">
+                    <i class="link-icon" data-feather="book"></i>
+                    <span class="link-title">Artikel</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#kelas" role="button" aria-expanded="false"
+                    aria-controls="kelas">
+                    <i class="link-icon" data-feather="home"></i>
+                    <span class="link-title">Kelas</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="kelas">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="{{ route('KelasOfflineSetting') }}" class="nav-link">Kelas Tatap Muka</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('KelasOnlineSetting') }}" class="nav-link">Kelas Online</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#galery" role="button" aria-expanded="false"
+                    aria-controls="galery">
+                    <i class="link-icon" data-feather="calendar"></i>
+                    <span class="link-title">Event dan Galery</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="galery">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="{{ route('event_setting') }}" class="nav-link">Event</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('galery_setting') }}" class="nav-link">Galery</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('link_setting') }}" class="nav-link">Link Youtube</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#aboutUs" role="button" aria-expanded="false"
+                    aria-controls="aboutUs">
+                    <i class="link-icon" data-feather="settings"></i>
+                    <span class="link-title">Tentang Kami</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="aboutUs">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="{{ route('about_us_setting') }}" class="nav-link">Visi & Misi</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('izin_setting') }}" class="nav-link">Izin</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a href="{{ route('ContohSertifikatSetting') }}" class="nav-link">
+                    <i class="link-icon" data-feather="book"></i>
+                    <span class="link-title">Contoh Sertifikat</span>
+                </a>
+            </li>
+
+            <li class="nav-item nav-category">Inventaris</li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#Inventaris" role="button"
+                    aria-expanded="false" aria-controls="Inventaris">
+                    <i class="link-icon" data-feather="box"></i>
+                    <span class="link-title">Barang</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="Inventaris">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="{{ route('master_kategori') }}" class="nav-link">Kategori Barang</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('master_barang') }}" class="nav-link">Master Barang</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </li>
+            <li class="nav-item nav-category">Keuangan</li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#Keuangan" role="button" aria-expanded="false"
+                    aria-controls="Keuangan">
+                    <i class="link-icon" data-feather="dollar-sign"></i>
+                    <span class="link-title">Keuangan</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="Keuangan">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="pages/advanced-ui/cropper.html" class="nav-link">Cropper</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/advanced-ui/owl-carousel.html" class="nav-link">Owl carousel</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/advanced-ui/sortablejs.html" class="nav-link">SortableJs</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/advanced-ui/sweet-alert.html" class="nav-link">Sweet Alert</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item nav-category">Kesiswaan</li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#Kesiswaan" role="button"
+                    aria-expanded="false" aria-controls="Kesiswaan">
+                    <i class="link-icon" data-feather="users"></i>
+                    <span class="link-title">Siswa</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="Kesiswaan">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="{{ route('pendaftaran_siswa') }}" class="nav-link">Data Siswa</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('sertifikat') }}" class="nav-link">Sertifikat</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/advanced-ui/sortablejs.html" class="nav-link">SortableJs</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/advanced-ui/sweet-alert.html" class="nav-link">Sweet Alert</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+{{-- <nav class="settings-sidebar">
+    <div class="sidebar-body">
+        <a href="#" class="settings-sidebar-toggler">
+            <i data-feather="settings"></i>
+        </a>
+        <h6 class="text-muted mb-2">Tema</h6>
+        <div class="mb-3 pb-3 border-bottom">
+            <div class="form-check form-check-inline">
+                <input type="radio" class="form-check-input" name="sidebarThemeSettings" id="sidebarLight"
+                    value="sidebar-light" checked>
+                <label class="form-check-label" for="sidebarLight">
+                    Light
+                </label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input type="radio" class="form-check-input" name="sidebarThemeSettings" id="sidebarDark"
+                    value="sidebar-dark">
+                <label class="form-check-label" for="sidebarDark">
+                    Dark
+                </label>
+            </div>
+        </div>
+
+    </div>
+</nav> --}}
---
+++ resources/views/layout/partials/footer.blade.php
@@ -0,0 +1,152 @@
+<!--<< Footer Section Start >>-->
+<footer class="footer-section section-bg fix">
+
+    <div class="frame-shape-3 float-bob-y">
+        <img src="{{ asset('public/assets/img/frame-3.png') }}" alt="shape-img">
+    </div>
+    <div class="footer-widgets-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay=".3s">
+                    <div class="single-footer-widget">
+                        <div class="widget-head">
+                            <a href="index.html">
+                                <img src="{{ asset('public/assets/img/logo/logo.svg') }}" alt="logo-img">
+                            </a>
+                        </div>
+                        <div class="footer-content style-two">
+                            <p>
+                                "ProSkill Akademia" BERKOMITMEN membantu memudahkan Anda menguasai TEKNOLOGI KOMPUTER
+                                dengan CEPAT dan BIAYA TERJANGKAU.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-4 col-md-6 ps-lg-5 wow fadeInUp" data-wow-delay=".5s">
+                    <div class="single-footer-widget">
+                        <div class="widget-head">
+                            <h3>Menu</h3>
+                        </div>
+                        <ul class="list-area">
+                            <li>
+                                <a href="{{ route('/') }}">
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                    Beranda
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('Kelastatapmuka') }}">
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                    Kelas Tatap Muka
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('kelasOnline') }}">
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                    Kelas Online
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('event') }}">
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                    Event
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('artikel') }}">
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                    Artikel
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-md-6 ps-lg-5 wow fadeInUp" data-wow-delay=".5s">
+                    <div class="single-footer-widget style-margin">
+                        <div class="widget-head">
+                            <h3>Profile</h3>
+                        </div>
+                        <ul class="list-area">
+                            <li>
+                                <a href="{{ route('tentangkami') }}">
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                    Tentang Kami
+                                </a>
+                            </li>
+                            <li>
+                                <a href="program-details.html">
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                    Contoh Sertifikat
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://drive.google.com/file/d/1FiVmF5_hau_8xRKfvoM8Wel1tUezCzA_/view"
+                                    target="_blank">
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                    Brosur
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('hubungikami') }}">
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                    Hubungi Kami
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-md-6 ps-lg-5 wow fadeInUp" data-wow-delay=".5s">
+                    <div class="single-footer-widget style-margin">
+                        <div class="widget-head">
+                            <h3>Media Sosial</h3>
+                        </div>
+                        <div class="footer-content style-two">
+
+                            <div class="social-icon d-flex align-items-center">
+                                <a href="https://www.instagram.com/proskillakademia?igsh=MTJzdjNteGNrMGVpOA== "
+                                    target="_blank"><i class="fab fa-instagram"></i></a>
+                                <a href="https://www.linkedin.com/company/proskill-akademia/;" target="_blank"><i
+                                        class="fa-brands fa-linkedin-in"></i></a>
+                                <a href="https://www.youtube.com/channel/UCNw50VFi4Rdmv1WpKlWGHUQ" target="_blank"><i
+                                        class="fa-brands fa-youtube"></i></a>
+                            </div>
+                            {{-- <a href="https://wa.me/6281266187125?text=Halo,%20saya%20mau%20konsultasi"
+                                data-animation="fadeInUp" data-delay="1.7s" class="theme-btn hover-white"
+                                target="_blank">
+                                Hubungi Admin
+                                <i class="fa-solid fa-arrow-right-long"></i>
+                            </a> --}}
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="f-bottom-shape">
+            <img src="{{ asset('public/assets/img/footer-bottom.png') }}" alt="shape-img">
+        </div>
+        <div class="container">
+            <div class="footer-wrapper d-flex align-items-center justify-content-between">
+                <p class="wow fadeInLeft color-2" data-wow-delay=".3s">
+                    © All Copyright 2024 by <a href="{{ route('tentangkami') }}">Proskill Akademia</a>
+                </p>
+                <ul class="footer-menu wow fadeInRight" data-wow-delay=".5s">
+
+                    <li>
+                        <a href="contact.html">
+                            Privacy Policy
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+        <a href="#" id="scrollUp" class="scroll-icon">
+            <i class="far fa-arrow-up"></i>
+        </a>
+    </div>
+</footer>
