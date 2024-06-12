9ad44910679326519142b9c0bc031ee3246f7dcd
Bayu <bayurz1001@gmail.com>
2024-06-12 23:21:00 +0700

    up

--- resources/views/admin/categories/categories.blade.php
+++ resources/views/admin/categories/categories.blade.php
@@ -25,7 +25,7 @@
                                 data-feather="plus-circle"></i>
                             Kategori
                         </button>
-                        <p class="text-muted mb-3">Jumlah Kategori : </p>
+                        <p class="text-muted mb-3">Jumlah Kategori : {{ $kategori->count() }}</p>
                         <div class="table-responsive">
                             <table id="dataTableExample" class="table">
                                 <thead>
