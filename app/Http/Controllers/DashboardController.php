f5580cb4419aeba0fef610099a0e6eb8f66a7c48
Bayu <bayurz1001@gmail.com>
2024-06-12 21:03:52 +0700

    up

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,6 +14,6 @@ class DashboardController extends Controller
         if (!$user) {
             return redirect()->route('login_admin');
         }
-        return view('admin.dashboard');
+        return view('admin.dashboard', compact('user'));
     }
 }
