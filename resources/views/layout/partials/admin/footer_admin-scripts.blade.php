3745b74a3030682870d8ce096f08b31a985fb7cb
Bayu <bayurz1001@gmail.com>
2024-06-13 19:30:46 +0700

    up

--- resources/views/layout/partials/admin/footer_admin-scripts.blade.php
+++ resources/views/layout/partials/admin/footer_admin-scripts.blade.php
@@ -3,54 +3,52 @@
 <!-- endinject -->
 
 <!-- Plugin js for this page -->
-<script src="public/assets_admin/vendors/flatpickr/flatpickr.min.js"></script>
-<script src="public/assets_admin/vendors/apexcharts/apexcharts.min.js"></script>
+<script src="{{ asset('public/assets_admin/vendors/flatpickr/flatpickr.min.js') }}"></script>
+<script src="{{ asset('public/assets_admin/vendors/apexcharts/apexcharts.min.js') }}"></script>
 <!-- End plugin js for this page -->
 
 <!-- Plugin js for this page -->
-<script src="public/assets_admin/vendors/datatables.net/jquery.dataTables.js"></script>
-<script src="public/assets_admin/vendors/datatables.net-bs5/dataTables.bootstrap5.js"></script>
+<script src="{{ asset('public/assets_admin/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+<script src="{{ asset('public/assets_admin/vendors/datatables.net-bs5/dataTables.bootstrap5.js') }}"></script>
 <!-- End plugin js for this page -->
 
 <!-- Custom js for this page -->
-<script src="public/assets_admin/js/data-table.js"></script>
+<script src="{{ asset('public/assets_admin/js/data-table.js') }}"></script>
 <!-- End custom js for this page -->
 
-
 <!-- inject:js -->
-<script src="public/assets_admin/vendors/feather-icons/feather.min.js"></script>
-<script src="public/assets_admin/js/template.js"></script>
+<script src="{{ asset('public/assets_admin/vendors/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('public/assets_admin/js/template.js') }}"></script>
 <!-- endinject -->
 
 <!-- Custom js for this page -->
-<script src="public/assets_admin/js/dashboard-light.js"></script>
-<script src="public/assets_admin/js/form-validation.js"></script>
-<script src="public/assets_admin/js/bootstrap-maxlength.js"></script>
-<script src="public/assets_admin/js/inputmask.js"></script>
+<script src="{{ asset('public/assets_admin/js/dashboard-light.js') }}"></script>
+<script src="{{ asset('public/assets_admin/js/form-validation.js') }}"></script>
+<script src="{{ asset('public/assets_admin/js/bootstrap-maxlength.js') }}"></script>
+<script src="{{ asset('public/assets_admin/js/inputmask.js') }}"></script>
 <script src="{{ asset('public/assets_admin/js/select2.js') }}"></script>
-<script src="public/assets_admin/js/typeahead.js"></script>
-<script src="public/assets_admin/js/tags-input.js"></script>
-<script src="public/assets_admin/js/dropzone.js"></script>
-<script src="public/assets_admin/js/dropify.js"></script>
-<script src="public/assets_admin/js/pickr.js"></script>
-<script src="public/assets_admin/js/flatpickr.js"></script>
-<!-- End custom js for this page -->
+<script src="{{ asset('public/assets_admin/js/typeahead.js') }}"></script>
+<script src="{{ asset('public/assets_admin/js/tags-input.js') }}"></script>
+<script src="{{ asset('public/assets_admin/js/dropzone.js') }}"></script>
+<script src="{{ asset('public/assets_admin/js/dropify.js') }}"></script>
+<script src="{{ asset('public/assets_admin/js/pickr.js') }}"></script>
+<script src="{{ asset('public/assets_admin/js/flatpickr.js') }}"></script>
 <!-- End custom js for this page -->
 
 <!-- Plugin js for this page -->
-<script src="public/assets_admin/vendors/easymde/easymde.min.js"></script>
-<script src="public/assets_admin/js/easymde.js"></script>
+<script src="{{ asset('public/assets_admin/vendors/easymde/easymde.min.js') }}"></script>
+<script src="{{ asset('public/assets_admin/js/easymde.js') }}"></script>
 
-<script src="public/assets_admin/vendors/jquery-validation/jquery.validate.min.js"></script>
-<script src="public/assets_admin/vendors/bootstrap-maxlength/bootstrap-maxlength.min.js"></script>
-{{-- <script src="public/assets_admin/vendors/inputmask/jquery.inputmask.min.js"></script> --}}
+<script src="{{ asset('public/assets_admin/vendors/jquery-validation/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('public/assets_admin/vendors/bootstrap-maxlength/bootstrap-maxlength.min.js') }}"></script>
+{{-- <script src="{{ asset('public/assets_admin/vendors/inputmask/jquery.inputmask.min.js') }}"></script> --}}
 <script src="{{ asset('public/assets_admin/vendors/select2/select2.min.js') }}"></script>
-<script src="public/assets_admin/vendors/typeahead.js/typeahead.bundle.min.js"></script>
-<script src="public/assets_admin/vendors/jquery-tags-input/jquery.tagsinput.min.js"></script>
-<script src="public/assets_admin/vendors/dropzone/dropzone.min.js"></script>
-<script src="public/assets_admin/vendors/dropify/dist/dropify.min.js"></script>
+<script src="{{ asset('public/assets_admin/vendors/typeahead.js/typeahead.bundle.min.js') }}"></script>
+<script src="{{ asset('public/assets_admin/vendors/jquery-tags-input/jquery.tagsinput.min.js') }}"></script>
+<script src="{{ asset('public/assets_admin/vendors/dropzone/dropzone.min.js') }}"></script>
+<script src="{{ asset('public/assets_admin/vendors/dropify/dist/dropify.min.js') }}"></script>
 <script src="{{ asset('public/assets_admin/vendors/pickr/pickr.min.js') }}"></script>
-<script src="public/assets_admin/vendors/moment/moment.min.js"></script>
-<script src="public/assets_admin/vendors/flatpickr/flatpickr.min.js"></script>
+<script src="{{ asset('public/assets_admin/vendors/moment/moment.min.js') }}"></script>
+<script src="{{ asset('public/assets_admin/vendors/flatpickr/flatpickr.min.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/@yaireo/tagify/dist/tagify.min.js"></script>
 {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@yaireo/tagify/dist/tagify.css" /> --}}
