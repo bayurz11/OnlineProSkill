a62b0219894effc5768118e80ab3882ccb1c7e89
Bayu <bayurz1001@gmail.com>
2024-06-13 15:40:50 +0700

    uup

--- database/migrations/2024_06_12_141935_create_categories_table.php
+++ database/migrations/2024_06_12_141935_create_categories_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->string('name');
+            $table->string('gambar');
             $table->timestamps();
         });
     }
