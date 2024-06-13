366bf68b69c5b4f8a726cf6baa153a08bffefb7f
Bayu <bayurz1001@gmail.com>
2024-06-13 20:10:55 +0700

    up

--- resources/views/admin/categories/subcategories.blade.php
+++ resources/views/admin/categories/subcategories.blade.php
@@ -45,7 +45,7 @@
 
                                             <td><img src="{{ asset('public/uploads/' . $kategori->gambar) }}" alt="Banner"
                                                     class="wd-100 wd-sm-150 me-3"></td>
-                                            <td>{{ $subcategory->category?->name_category ?? 'Kategori tidak ditemukan' }}
+                                            <td>{{ $subcategory->kategori?->name_category ?? 'Kategori tidak ditemukan' }}
                                             </td>
                                             <td>{{ $kategori->name }}</td>
                                             <td><a href="#" id="badgeLink"
