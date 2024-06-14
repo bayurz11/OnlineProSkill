2947e135d4365f38dfb999dcdb2c52d9844b456f
Bayu <bayurz1001@gmail.com>
2024-06-14 10:26:34 +0700

    up

--- resources/views/admin/CourseMaster/orderhistory.blade.php
+++ resources/views/admin/CourseMaster/orderhistory.blade.php
@@ -8,7 +8,7 @@
         <nav class="page-breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Course</li>
+                <li class="breadcrumb-item active" aria-current="page">Order History</li>
             </ol>
         </nav>
 
