72befe885511d7b087e6a93f2b0bb6fa0767e88c
Bayu <bayurz1001@gmail.com>
2024-06-14 10:48:07 +0700

    up

--- resources/views/admin/modal/add_course.blade.php
+++ resources/views/admin/modal/add_course.blade.php
@@ -104,8 +104,6 @@
     }
 </script>
 
-<!-- Include jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- Include Select2 JS -->
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0/dist/js/select2.min.js"></script>
 
