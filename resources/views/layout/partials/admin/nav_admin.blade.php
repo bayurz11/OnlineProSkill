4db1e9b71075e70e6452902ac6b0079e0b7600b2
Bayu <bayurz1001@gmail.com>
2024-06-14 09:42:43 +0700

    up

--- resources/views/layout/partials/admin/nav_admin.blade.php
+++ resources/views/layout/partials/admin/nav_admin.blade.php
@@ -20,6 +20,25 @@
             </li>
             <li class="nav-item nav-category">Online Course Setting</li>
             <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#charts" role="button" aria-expanded="false"
+                    aria-controls="charts">
+                    <i class="link-icon" data-feather="folder"></i>
+                    <span class="link-title">Kategori</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="charts">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="{{ route('categories') }}" class="nav-link">Categories</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('subcategories') }}" class="nav-link">Subcategories</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
                 <a class="nav-link" data-bs-toggle="collapse" href="#emails" role="button" aria-expanded="false"
                     aria-controls="emails">
                     <i class="link-icon" data-feather="folder"></i>
@@ -30,10 +49,10 @@
                     <ul class="nav sub-menu">
 
                         <li class="nav-item">
-                            <a href="{{ route('categories') }}" class="nav-link">Categories</a>
+
                         </li>
                         <li class="nav-item">
-                            <a href="{{ route('subcategories') }}" class="nav-link">Subcategories</a>
+
                         </li>
 
                     </ul>
