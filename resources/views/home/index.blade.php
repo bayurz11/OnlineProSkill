3e4a26db81b70f83b38ec81a730fecebc1b2a273
Bayu <bayurz1001@gmail.com>
2024-06-14 09:20:06 +0700

    up

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -23,7 +23,7 @@
     <!-- Login Modal -->
     <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel"
         aria-hidden="true">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="loginModalLabel">Login</h5>
