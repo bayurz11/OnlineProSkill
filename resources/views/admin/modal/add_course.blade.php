811050410c7e75d80b7f6f87e16df968c2a3a150
Bayu <bayurz1001@gmail.com>
2024-06-14 12:05:30 +0700

    up

--- resources/views/admin/modal/add_course.blade.php
+++ resources/views/admin/modal/add_course.blade.php
@@ -74,7 +74,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="price" class="form-label">Harga<span class="text-danger">*</span></label>
-                        <input type="text" class="form-control" id="price" name="price">
+                        <input type="number" class="form-control" id="price" name="price">
                     </div>
                     <div class="mb-3">
                         <label for="discount" class="form-label">Diskon %</label>
