384dea076016a42070e76d4d077a8d6f6c28ac70
Bayu <bayurz1001@gmail.com>
2024-06-20 16:11:28 +0700

    up

--- resources/views/admin/categories/categories.blade.php
+++ resources/views/admin/categories/categories.blade.php
@@ -45,16 +45,11 @@
                                                     class="wd-100 wd-sm-150 me-3"></td>
                                             <td>{{ $kategori->name_category }}</td>
                                             <td>
-                                                <div class="form-check form-switch mb-2">
-                                                    <input type="checkbox" class="form-check-input" id="formSwitch1"
-                                                        {{ $kategori->status ? 'checked' : '' }}>
-                                                    <a href="#"
-                                                        class="badge badgeLink {{ $kategori->status ? 'bg-success' : 'bg-danger' }}"
-                                                        data-id="{{ $kategori->id }}" data-status="{{ $kategori->status }}">
-                                                        {{ $kategori->status ? 'Aktif' : 'Tidak Aktif' }}
-                                                    </a>
-                                                </div>
-
+                                                <a href="#"
+                                                    class="badge badgeLink {{ $kategori->status ? 'bg-success' : 'bg-danger' }}"
+                                                    data-id="{{ $kategori->id }}" data-status="{{ $kategori->status }}">
+                                                    {{ $kategori->status ? 'Active' : 'Inactive' }}
+                                                </a>
                                             </td>
                                             <td>
                                                 <button type="button" class="btn btn-primary btn-icon edit-button"
