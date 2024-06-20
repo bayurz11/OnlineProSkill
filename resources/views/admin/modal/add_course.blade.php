8143d2c00e951eccce687e21049c7bdae1246b16
Bayu <bayurz1001@gmail.com>
2024-06-20 09:23:19 +0700

    up

--- resources/views/admin/modal/add_course.blade.php
+++ resources/views/admin/modal/add_course.blade.php
@@ -47,7 +47,6 @@
                     </div>
                     <div class="mb-3">
                         <label for="pimpinan" class="form-label">Deskripsi<span class="text-danger">*</span></label>
-                        <script src="https://cdn.ckeditor.com/ckeditor5/36.0.1/classic/ckeditor.js"></script>
                         <textarea id="content" style="height: 800px; width: 200px; font-size: 18px;"></textarea>
                         <!-- Menggunakan <textarea> untuk CKEditor -->
                         <input type="hidden" id="content_input" name="content">
@@ -122,6 +121,7 @@
 <script src="https://cdn.jsdelivr.net/npm/@yaireo/tagify/dist/tagify.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@yaireo/tagify/dist/tagify.css" />
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.ckeditor.com/ckeditor5/36.0.1/classic/ckeditor.js"></script>
 <script>
     //tags
     document.addEventListener("DOMContentLoaded", function() {
