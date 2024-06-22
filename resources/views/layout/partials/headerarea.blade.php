c398fa1aa4cda821f42a889b0b9cd4fed43538fb
Bayu <bayurz1001@gmail.com>
2024-06-22 10:36:03 +0700

    up

--- resources/views/layout/partials/headerarea.blade.php
+++ resources/views/layout/partials/headerarea.blade.php
@@ -191,8 +191,32 @@
                                              style="background-color: white; color: black; border: 1px solid black;"
                                              data-toggle="modal" data-target="#exampleModal">Masuk</button>
                                      </li>
+                                     {{-- login modal --}}
 
-                                     @include('home.modal.login')
+                                     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                                         aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                         <div class="modal-dialog" role="document">
+                                             <div class="modal-content">
+                                                 <div class="modal-header">
+                                                     <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                     <button type="button" class="close" data-dismiss="modal"
+                                                         aria-label="Close">
+                                                         <span aria-hidden="true">&times;</span>
+                                                     </button>
+                                                 </div>
+                                                 <div class="modal-body">
+                                                     <!-- Konten modal di sini -->
+                                                     Konten modal di sini.
+                                                 </div>
+                                                 <div class="modal-footer">
+                                                     <button type="button" class="btn btn-secondary"
+                                                         data-dismiss="modal">Close</button>
+                                                     <button type="button" class="btn btn-primary">Save
+                                                         changes</button>
+                                                 </div>
+                                             </div>
+                                         </div>
+                                     </div>
 
                                      <li class="header-btn login-btn">
                                          <a href="contact.html" class="btn">Daftar</a>
