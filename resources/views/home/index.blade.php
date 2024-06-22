540ab3686946c09ef80c157f7d800e152a7bdfd7
Bayu <bayurz1001@gmail.com>
2024-06-22 13:39:10 +0700

    up

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -40,7 +40,7 @@
         </div>
     </div>
     <!-- banner-area -->
-    <section class="banner-area-two banner-bg-two tg-motion-effects"
+    {{-- <section class="banner-area-two banner-bg-two tg-motion-effects"
         data-background="public/assets/img/banner/banner_bg02.png">
         <div class="container">
             <div class="row justify-content-center">
@@ -90,6 +90,44 @@
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+        <img src="public/assets/img/banner/h2_banner_shape03.svg" alt="shape" class="line-shape-two"
+            data-aos="fade-right" data-aos-delay="1600">
+    </section> --}}
+    <section class="banner-area-two banner-bg-two tg-motion-effects"
+        data-background="public/assets/img/banner/banner_bg02.png">
+        <div class="container">
+            <div class="instructor-slider-dot">
+                <div class="swiper instructor-nav">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <button><img src="public/assets/img/instructor/instructor_nav01.png" alt="img"></button>
+                        </div>
+                        <div class="swiper-slide">
+                            <button><img src="public/assets/img/instructor/instructor_nav02.png" alt="img"></button>
+                        </div>
+                        <div class="swiper-slide">
+                            <button><img src="public/assets/img/instructor/instructor_nav03.png" alt="img"></button>
+                        </div>
+                        <div class="swiper-slide">
+                            <button><img src="public/assets/img/instructor/instructor_nav04.png" alt="img"></button>
+                        </div>
+                        <div class="swiper-slide">
+                            <button><img src="public/assets/img/instructor/h2_instructor05.png" alt="img"></button>
+                        </div>
+                        <div class="swiper-slide">
+                            <button><img src="public/assets/img/instructor/instructor_nav04.png" alt="img"></button>
+                        </div>
+                        <div class="swiper-slide">
+                            <button><img src="public/assets/img/instructor/instructor_nav03.png" alt="img"></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="instructor__nav-two">
+                    <div class="instructor-button-prev"><i class="flaticon-arrow-right"></i></div>
+                    <div class="instructor-button-next"><i class="flaticon-arrow-right"></i></div>
                 </div>
             </div>
         </div>
