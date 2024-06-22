c4910b4cb9077b2c26ca53aef4cda820b0c88cf4
Bayu <bayurz1001@gmail.com>
2024-06-22 13:03:26 +0700

    up

--- resources/views/home/modal/register.blade.php
+++ resources/views/home/modal/register.blade.php
@@ -1,6 +1,10 @@
 <div class="modal fade" id="exampleModalDaftar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
             <!-- Modal Body -->
             <div class="modal-body">
                 <div class="singUp-wrap">
