c56330932dcf4c7664de45d39ee44edb8c7ef008
Bayu <bayurz1001@gmail.com>
2024-06-22 11:37:07 +0700

    up

--- resources/views/home/modal/login.blade.php
+++ resources/views/home/modal/login.blade.php
@@ -3,9 +3,8 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Welcome back!</h5>
-                <button type="button" class="close rounded-circle p-0" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true" class="p-2"
-                        style="background-color: rgba(0,0,0,.1); border-radius: 50%;">&times;</span>
+                <button type="button" class="close rounded-circle" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
@@ -47,3 +46,17 @@
         </div>
     </div>
 </div>
+<!-- jQuery (diperlukan untuk Bootstrap JavaScript plugins) -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha384-FqpYut+0ITe8Hv0OcQ6mGXJ3d1iEtAVIEbYB2yUwh1vX00aWKj//SK28/n+ThjiR" crossorigin="anonymous">
+</script>
+
+<!-- Bootstrap JavaScript -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"
+    integrity="sha384-TM+pzfSqMqpXJ2oiFvDU8rxWoRJ/G0sN6EISOs6RP/HsGJ/JA1JQ5im6U6Z6wu89" crossorigin="anonymous">
+</script>
+
+<!-- Optional: FontAwesome (jika Anda ingin menggunakan ikon tambahan, tidak diperlukan jika sudah ada di proyek Anda) -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"
+    integrity="sha512-+QmNnmH5S+yIw8xQgg/+uXpC+V9HjBOBwR2+I65dK4S3mfnYzC3HmeW5IHefkGOyZ5uHdKXHvP8rGgd3yB11ew=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
