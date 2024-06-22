4e0d19c6e2b8ae16b3156b96dc9f96653b631265
Bayu <bayurz1001@gmail.com>
2024-06-22 10:55:47 +0700

    up

--- resources/views/layout/partials/headerarea.blade.php
+++ resources/views/layout/partials/headerarea.blade.php
@@ -222,7 +222,7 @@
                              <div class="tgmobile__menu-outer">
                                  <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
                              </div>
-                             <div class="button_login">
+                             <div class="tgmobile__search">
                                  <ul class="list-wrap">
                                      <li class="header-btn login-btn">
                                          <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal"
