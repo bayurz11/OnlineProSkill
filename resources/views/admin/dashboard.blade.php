2505edb092af6bdb8f8cd2d6c268755532c8513d
Bayu <bayurz1001@gmail.com>
2024-06-24 16:22:53 +0700

    up

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -32,7 +32,7 @@
         <div class="row">
             <div class="col-12 col-xl-12 stretch-card">
                 <div class="row flex-grow-1">
-                    <div class="col-md-4 grid-margin stretch-card">
+                    <div class="col-md-2 grid-margin stretch-card">
                         <div class="card">
                             <div class="card-body">
                                 <div class="d-flex justify-content-between align-items-baseline">
