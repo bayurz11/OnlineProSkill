920bb2d8c3752a00ef87239c6b8bb56a27d93c05
Bayu <bayurz1001@gmail.com>
2024-06-24 19:56:46 +0700

    up

--- resources/views/admin/modal/edit_course.blade.php
+++ resources/views/admin/modal/edit_course.blade.php
@@ -250,12 +250,11 @@
     });
 
     document.addEventListener("DOMContentLoaded", function() {
-        // Inisialisasi Tagify pada input tag setelah modal ditampilkan
         $('#editModal').on('shown.bs.modal', function() {
             var input = document.querySelector('input[name=tag]');
             if (input) {
                 new Tagify(input, {
-                    whitelist: [], // Tambahkan daftar tag yang diizinkan jika diperlukan
+                    whitelist: [],
                     dropdown: {
                         enabled: 1,
                         maxItems: 100
