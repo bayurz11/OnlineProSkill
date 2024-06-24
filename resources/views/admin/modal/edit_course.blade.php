a568c488ce7e54982f6628382302cc974d081570
Bayu <bayurz1001@gmail.com>
2024-06-24 19:55:17 +0700

    up

--- resources/views/admin/modal/edit_course.blade.php
+++ resources/views/admin/modal/edit_course.blade.php
@@ -128,21 +128,6 @@
 </div>
 
 <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        // Inisialisasi Tagify pada input tag setelah modal ditampilkan
-        $('#editModal').on('shown.bs.modal', function() {
-            var input = document.querySelector('input[name=tag]');
-            if (input) {
-                new Tagify(input, {
-                    whitelist: [], // Tambahkan daftar tag yang diizinkan jika diperlukan
-                    dropdown: {
-                        enabled: 1,
-                        maxItems: 100
-                    }
-                });
-            }
-        });
-    });
     $(document).ready(function() {
         // Fetch data when the edit button is clicked
         $('.edit-button').on('click', function() {
@@ -262,5 +247,21 @@
         }
 
         $('#edit_free').on('change', toggleEditPriceAndDiscount);
+    });
+
+    document.addEventListener("DOMContentLoaded", function() {
+        // Inisialisasi Tagify pada input tag setelah modal ditampilkan
+        $('#editModal').on('shown.bs.modal', function() {
+            var input = document.querySelector('input[name=tag]');
+            if (input) {
+                new Tagify(input, {
+                    whitelist: [], // Tambahkan daftar tag yang diizinkan jika diperlukan
+                    dropdown: {
+                        enabled: 1,
+                        maxItems: 100
+                    }
+                });
+            }
+        });
     });
 </script>
