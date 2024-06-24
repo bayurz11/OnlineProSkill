f94784a5fce01b2b772c237274111d1339ab0243
Bayu <bayurz1001@gmail.com>
2024-06-24 19:15:09 +0700

    up

--- resources/views/layout/partials/admin/head_admin.blade.php
+++ resources/views/layout/partials/admin/head_admin.blade.php
@@ -44,3 +44,4 @@
 {{-- <script src="https://cdn.jsdelivr.net/npm/@yaireo/tagify/dist/tagify.min.js"></script> --}}
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@yaireo/tagify/dist/tagify.css" />
 <script src="https://cdn.ckeditor.com/ckeditor5/41.4.2/classic/ckeditor.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
