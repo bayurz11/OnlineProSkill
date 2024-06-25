3aca5546b6ccd085e44e1c41d57413a3e3775eb2
Bayu <bayurz1001@gmail.com>
2024-06-25 14:14:29 +0700

    up

--- resources/views/home/classroomdetail.blade.php
+++ resources/views/home/classroomdetail.blade.php
@@ -74,11 +74,7 @@
                                     data-bs-target="#curriculum-tab-pane" type="button" role="tab"
                                     aria-controls="curriculum-tab-pane" aria-selected="false">Curriculum</button>
                             </li>
-                            <li class="nav-item" role="presentation">
-                                <button class="nav-link" id="instructors-tab" data-bs-toggle="tab"
-                                    data-bs-target="#instructors-tab-pane" type="button" role="tab"
-                                    aria-controls="instructors-tab-pane" aria-selected="false">Instructors</button>
-                            </li>
+
                             <li class="nav-item" role="presentation">
                                 <button class="nav-link" id="reviews-tab" data-bs-toggle="tab"
                                     data-bs-target="#reviews-tab-pane" type="button" role="tab"
@@ -288,30 +284,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="tab-pane fade" id="instructors-tab-pane" role="tabpanel"
-                                aria-labelledby="instructors-tab" tabindex="0">
-                                <div class="courses__instructors-wrap">
-                                    <div class="courses__instructors-thumb">
-                                        <img src="public/assets/img/courses/course_instructors.png" alt="img">
-                                    </div>
-                                    <div class="courses__instructors-content">
-                                        <h2 class="title">Mark Jukarberg</h2>
-                                        <span class="designation">UX Design Lead</span>
-                                        <p class="avg-rating"><i class="fas fa-star"></i>(4.8 Ratings)</p>
-                                        <p>Dorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                            incididunt ut labore et dolore magna aliqua Quis ipsum suspendisse ultrices
-                                            gravida. Risus commodo viverra maecenas accumsan.</p>
-                                        <div class="instructor__social">
-                                            <ul class="list-wrap justify-content-start">
-                                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-whatsapp"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+
                             <div class="tab-pane fade" id="reviews-tab-pane" role="tabpanel"
                                 aria-labelledby="reviews-tab" tabindex="0">
                                 <div class="courses__rating-wrap">
