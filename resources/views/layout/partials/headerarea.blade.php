192cabe616df5fc98927de8ecc12ba76885e4f02
Bayu <bayurz1001@gmail.com>
2024-06-25 16:29:30 +0700

    up

--- resources/views/layout/partials/headerarea.blade.php
+++ resources/views/layout/partials/headerarea.blade.php
@@ -138,7 +138,7 @@
                                             class="injectable">
                                     </a>
                                     <a href="{{ route('cart') }}" class="cart-count-two">
-                                        <i class="flaticon-shopping-cart"></i>
+                                        <i class="flaticon-shopping-cart"></i><span class="red-circle"></span>
                                     </a>
                                 @endauth
                             </div>
