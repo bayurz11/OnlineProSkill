acd3fddca7bd28034c2e5632084544321b4e1680
Bayu <bayurz1001@gmail.com>
2024-06-26 16:42:07 +0700

    up

--- resources/views/layout/partials/headerarea.blade.php
+++ resources/views/layout/partials/headerarea.blade.php
@@ -84,7 +84,7 @@
                                 <ul class="list-wrap">
                                     <li class="mini-cart-icon">
                                         <a href="#" class="cart-count-two">
-                                            <i class="fas fa-bell"></i>
+                                            <i class="far fa-bell"></i>
                                             <span class="mini-cart-count">0</span>
                                         </a>
                                     </li>
