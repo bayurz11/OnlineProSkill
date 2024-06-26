f46decb1acd839d3769ede3c619ddb7e85ed5a5d
Bayu <bayurz1001@gmail.com>
2024-06-26 20:30:58 +0700

    up

--- resources/views/layout/partials/headerarea.blade.php
+++ resources/views/layout/partials/headerarea.blade.php
@@ -145,34 +145,10 @@
 
                             <div class="mobile-login-btn">
                                 @auth
-                                    <ul class="navigation">
-                                        <li class="menu-item-has-children">
-                                            <a href="{{ route('dashboard_studen') }}">
-                                                <img src="{{ asset('public/assets/img/courses/course_author001.png') }}"
-                                                    alt="img" style="border-radius: 50%;">
-                                            </a>
-                                            <ul class="sub-menu">
-                                                <li class="{{ Request::is('dashboard_studen') ? 'active' : '' }}">
-                                                    <a href="{{ route('dashboard_studen') }}">Dashboard</a>
-                                                </li>
-                                                <li class="{{ Request::is('Profil') ? 'active' : '' }}">
-                                                    <a href="about-us.html">Profil</a>
-                                                </li>
-                                                <li>
-                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                                        style="display: none;">
-                                                        @csrf
-                                                    </form>
-                                                    <a href="{{ route('logout') }}"
-                                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                                        Logout
-                                                    </a>
-                                                </li>
-
-                                            </ul>
-                                        </li>
-
-                                    </ul>
+                                    <a href="{{ route('dashboard_studen') }}" style="margin-right: 10px;">
+                                        <img src="{{ asset('public/assets/img/icons/user.svg') }}" alt=""
+                                            class="injectable">
+                                    </a>
                                     <a href="#" class="cart-count-two">
                                         <i class="far fa-bell"></i><span class="red-circle">0</span>
                                     </a>
