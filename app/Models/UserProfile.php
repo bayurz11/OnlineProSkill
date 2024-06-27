b20e2e2eaa97b208d1f8052ffbe64bc0cf113281
Bayu <bayurz1001@gmail.com>
2024-06-27 13:16:01 +0700

    up

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\UserRoles;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserProfile extends Model
 {
@@ -16,5 +17,9 @@ class UserProfile extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    public function userRole()
+    {
+        return $this->hasOne(UserRoles::class);
     }
 }
