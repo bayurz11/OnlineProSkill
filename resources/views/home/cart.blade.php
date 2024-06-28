6ee76a29de6a97df6b7203d53124f0d21f2a047e
Bayu <bayurz1001@gmail.com>
2024-06-28 22:18:09 +0700

    up

--- resources/views/home/cart.blade.php
+++ resources/views/home/cart.blade.php
@@ -4,41 +4,42 @@
 <?php $page = 'Cart'; ?>
 
 @section('content')
-    @auth
-        @isset($courses)
-            <section class="breadcrumb__area breadcrumb__bg" data-background="{{ asset('public/assets/img/bg/breadcrumb_bg.jpg') }}">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="breadcrumb__content">
-                                <h3 class="title">Cart</h3>
-                                <nav class="breadcrumb">
-                                    <span property="itemListElement" typeof="ListItem">
-                                        <a href="{{ route('/') }}">Beranda</a>
-                                    </span>
-                                    <span class="breadcrumb-separator"><i class="fas fa-angle-right"></i></span>
-                                    <span property="itemListElement" typeof="ListItem">Cart</span>
-                                </nav>
-                            </div>
-                        </div>
+
+    <section class="breadcrumb__area breadcrumb__bg" data-background="{{ asset('public/assets/img/bg/breadcrumb_bg.jpg') }}">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="breadcrumb__content">
+                        <h3 class="title">Cart</h3>
+                        <nav class="breadcrumb">
+                            <span property="itemListElement" typeof="ListItem">
+                                <a href="{{ route('/') }}">Beranda</a>
+                            </span>
+                            <span class="breadcrumb-separator"><i class="fas fa-angle-right"></i></span>
+                            <span property="itemListElement" typeof="ListItem">Cart</span>
+                        </nav>
                     </div>
                 </div>
-                <div class="breadcrumb__shape-wrap">
-                    <img src="{{ asset('public/assets/img/others/breadcrumb_shape01.svg') }}" alt="img" class="alltuchtopdown">
-                    <img src="{{ asset('public/assets/img/others/breadcrumb_shape02.svg') }}" alt="img" data-aos="fade-right"
-                        data-aos-delay="300">
-                    <img src="{{ asset('public/assets/img/others/breadcrumb_shape03.svg') }}" alt="img" data-aos="fade-up"
-                        data-aos-delay="400">
-                    <img src="{{ asset('public/assets/img/others/breadcrumb_shape04.svg') }}" alt="img"
-                        data-aos="fade-down-left" data-aos-delay="400">
-                    <img src="{{ asset('public/assets/img/others/breadcrumb_shape05.svg') }}" alt="img" data-aos="fade-left"
-                        data-aos-delay="400">
-                </div>
-            </section>
+            </div>
+        </div>
+        <div class="breadcrumb__shape-wrap">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape01.svg') }}" alt="img" class="alltuchtopdown">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape02.svg') }}" alt="img" data-aos="fade-right"
+                data-aos-delay="300">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape03.svg') }}" alt="img" data-aos="fade-up"
+                data-aos-delay="400">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape04.svg') }}" alt="img"
+                data-aos="fade-down-left" data-aos-delay="400">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape05.svg') }}" alt="img" data-aos="fade-left"
+                data-aos-delay="400">
+        </div>
+    </section>
 
-            <div class="cart__area section-py-120">
-                <div class="container">
-                    <div class="row">
+    <div class="cart__area section-py-120">
+        <div class="container">
+            <div class="row">
+                @auth
+                    @isset($courses)
                         @auth
                             <div class="col-12">
                                 <div class="coupon__code-wrap">
@@ -131,47 +132,49 @@
                                 </div>
                             </div>
                         @endguest
-                        <div class="col-lg-5">
-                            <div class="cart__collaterals-wrap">
-                                <h2 class="title">Total Keranjang</h2>
-                                <ul class="list-wrap">
-                                    @foreach ($cart as $item)
-                                        <li class="cart-item" style="display: flex; align-items: center; margin-bottom: 10px;">
-                                            <a href="{{ route('cart.remove', $item['id']) }}"
-                                                style="margin-right: 10px; color: red; text-decoration: none;">×</a>
-                                            <a href="{{ route('classroomdetail', $item['id']) }}" style="margin-right: 10px;">
-                                                <img src="{{ asset('public/uploads/' . $item['gambar']) }}" alt="img"
-                                                    style="width: 50px; height: 50px;">
-                                            </a>
-                                            <div class="cart-details" style="display: flex; flex-direction: column;">
-                                                <a href="{{ route('classroomdetail', $item['id']) }}">{{ $item['name'] }}</a>
-                                                <span
-                                                    class="cart-price">Rp.{{ number_format($item['price'], 0, ',', '.') }}</span>
-                                            </div>
-                                        </li>
-                                    @endforeach
-                                </ul>
-                                <div class="cart-summary" style="margin-top: 20px;">
-                                    <div class="subtotal"
-                                        style="display: flex; justify-content: space-between; margin-bottom: 10px;">
-                                        <span>Subtotal</span>
-                                        <span>Rp.{{ number_format(array_sum(array_column($cart, 'price')), 0, ',', '.') }}</span>
-                                    </div>
-                                    <div class="total"
-                                        style="display: flex; justify-content: space-between; margin-bottom: 10px;">
-                                        <span>Total</span>
+                    @endisset
+                @endauth
+                <div class="col-lg-5">
+                    <div class="cart__collaterals-wrap">
+                        <h2 class="title">Total Keranjang</h2>
+                        <ul class="list-wrap">
+                            @foreach ($cart as $item)
+                                <li class="cart-item" style="display: flex; align-items: center; margin-bottom: 10px;">
+                                    <a href="{{ route('cart.remove', $item['id']) }}"
+                                        style="margin-right: 10px; color: red; text-decoration: none;">×</a>
+                                    <a href="{{ route('classroomdetail', $item['id']) }}" style="margin-right: 10px;">
+                                        <img src="{{ asset('public/uploads/' . $item['gambar']) }}" alt="img"
+                                            style="width: 50px; height: 50px;">
+                                    </a>
+                                    <div class="cart-details" style="display: flex; flex-direction: column;">
+                                        <a href="{{ route('classroomdetail', $item['id']) }}">{{ $item['name'] }}</a>
                                         <span
-                                            class="amount">Rp.{{ number_format(array_sum(array_column($cart, 'price')), 0, ',', '.') }}</span>
+                                            class="cart-price">Rp.{{ number_format($item['price'], 0, ',', '.') }}</span>
                                     </div>
-                                    <a href="" class="btn">Bayar & Gabung Kelas</a>
-                                </div>
+                                </li>
+                            @endforeach
+                        </ul>
+                        <div class="cart-summary" style="margin-top: 20px;">
+                            <div class="subtotal"
+                                style="display: flex; justify-content: space-between; margin-bottom: 10px;">
+                                <span>Subtotal</span>
+                                <span>Rp.{{ number_format(array_sum(array_column($cart, 'price')), 0, ',', '.') }}</span>
                             </div>
+                            <div class="total"
+                                style="display: flex; justify-content: space-between; margin-bottom: 10px;">
+                                <span>Total</span>
+                                <span
+                                    class="amount">Rp.{{ number_format(array_sum(array_column($cart, 'price')), 0, ',', '.') }}</span>
+                            </div>
+                            <a href="" class="btn">Bayar & Gabung Kelas</a>
                         </div>
-
-
                     </div>
                 </div>
+
+
             </div>
-        @endisset
-    @endauth
+        </div>
+    </div>
+@endisset
+@endauth
 @endsection
