ff4b32a2494f7357951f5726cde6ac44d8c3673b
Bayu <bayurz1001@gmail.com>
2024-06-29 01:14:31 +0700

    up

--- resources/views/home/cart.blade.php
+++ resources/views/home/cart.blade.php
@@ -52,11 +52,11 @@
                         <h2 class="title">Total keranjang</h2>
                         <ul class="list-wrap">
                             @foreach ($cart as $item)
-                                <tr>
+                                <li>
                                     <td class="product__name">
                                         <a href="{{ route('classroomdetail', $item['id']) }}">{{ $item['name'] }}</a>
                                     </td>
-                                </tr>
+                                </li>
                             @endforeach
                             <li>Subtotal <span>Rp.{{ array_sum(array_column($cart, 'price')) }}</span></li>
                             <li>Total <span class="amount">Rp.{{ array_sum(array_column($cart, 'price')) }}</span></li>
