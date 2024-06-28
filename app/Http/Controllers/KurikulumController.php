140aeb740a1f37e574db9886b783aa56a31545a6
Bayu <bayurz1001@gmail.com>
2024-06-28 13:20:22 +0700

    up

--- app/Http/Controllers/KurikulumController.php
+++ app/Http/Controllers/KurikulumController.php
@@ -19,7 +19,7 @@ class KurikulumController extends Controller
     {
         $user = Auth::user();
         $categori = Categories::all();
-        $course = KelasTatapMuka::with('user')->get();
+        $course = Kurikulum::with('user')->get();
         $cours = KelasTatapMuka::all();
         $count = $course->count();
         if (!$user) {
