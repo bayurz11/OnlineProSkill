805bdc056874620be6153b0a06693433f476fa45
Bayu <bayurz1001@gmail.com>
2024-06-28 14:34:03 +0700

    up

--- resources/views/home/checkout.blade.php
+++ resources/views/home/checkout.blade.php
@@ -22,7 +22,8 @@
                             </span>
                             <span class="breadcrumb-separator"><i class="fas fa-angle-right"></i></span>
                             <span property="itemListElement" typeof="ListItem">
-                                <a href="{{ route('classroom') }}">{{ $courses->nama_kursus }}</a>
+                                <a
+                                    href="{{ route('classroomdetail', ['id' => $cours->id]) }}">{{ $courses->nama_kursus }}</a>
                             </span>
                             <span class="breadcrumb-separator"><i class="fas fa-angle-right"></i></span>
                             <span property="itemListElement" typeof="ListItem">Checkout</span>
