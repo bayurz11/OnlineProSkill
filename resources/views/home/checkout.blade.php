ca0f4d6914ccfc134c922ec0f0bc34de01f9758e
Bayu <bayurz1001@gmail.com>
2024-06-28 14:10:47 +0700

    up

--- resources/views/home/checkout.blade.php
+++ resources/views/home/checkout.blade.php
@@ -88,6 +88,15 @@
                         </form>
                     </div>
                 @endauth
+                @guest
+                    <li class="header-btn login-btn">
+                        <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn"
+                            style="background-color: white; color: black; border: 1px solid black;">Masuk</a>
+                    </li>
+                    <li class="header-btn login-btn">
+                        <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModalDaftar" class="btn">Daftar</a>
+                    </li>
+                @endguest
                 <div class="col-lg-5">
                     <div class="order__info-wrap">
                         <h2 class="title">PESANAN ANDA</h2>
