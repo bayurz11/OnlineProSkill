2b51d90d96d7028a31e9600accf126c0d0478fa4
Bayu <bayurz1001@gmail.com>
2024-06-28 23:21:34 +0700

    up

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -6,20 +6,21 @@ use App\Models\UserProfile;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class SettingController extends Controller
 {
     public function index()
     {
+        $cart = Session::get('cart', []);
         $user = Auth::user();
         if (!$user) {
             return redirect()->route('/');
         }
-
         // Mengambil profil pengguna yang sedang login
         $profile = UserProfile::where('user_id', $user->id)->first();
 
-        return view('studen.setting', compact('user', 'profile'));
+        return view('studen.setting', compact('user', 'profile', 'cart'));
     }
 
     public function updateprofil(Request $request, $id)
