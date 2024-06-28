ccf70ff76775110c45d5de336482a89e57e5f7b4
Bayu <bayurz1001@gmail.com>
2024-06-29 01:02:43 +0700

    up

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -84,7 +84,7 @@ class CartController extends Controller
 
         Session::put('cart', $cart);
 
-        return redirect()->route('cart.view');
+        return redirect()->route('classroom');
     }
     public function removeFromCart($id)
     {
