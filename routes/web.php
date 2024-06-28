aab1949b14efb392272586a5992242309f154095
Bayu <bayurz1001@gmail.com>
2024-06-28 15:07:47 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::post('/regisInstruktur', [DashboardInstrukturController::class, 'register'])->name('regisInstruktur');
 Route::post('/regisStuden', [AuthController::class, 'register'])->name('regisStuden');
-Route::post('/guestregister', [AuthController::class, 'register'])->name('guestregister');
+Route::post('/guestregister', [AuthController::class, 'guestregister'])->name('guestregister');
 
 Route::middleware('isAdmin')->group(function () {
 
