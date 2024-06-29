ab4aaa5a9b2601f45641f03fefca86a3f18fff60
Bayu <bayurz1001@gmail.com>
2024-06-29 13:30:43 +0700

    up

--- app/Http/Controllers/RiwayatTransaksiController.php
+++ app/Http/Controllers/RiwayatTransaksiController.php
@@ -6,18 +6,19 @@ use App\Models\UserProfile;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class RiwayatTransaksiController extends Controller
 {
     public function index()
     {
-
+        $cart = Session::get('cart', []);
         $user = Auth::user();
         if (!$user) {
             return redirect()->route('/');
         }
         $profile = UserProfile::where('user_id', $user->id)->first();
 
-        return view('studen.history', compact('user', 'profile',));
+        return view('studen.history', compact('user', 'profile', 'cart'));
     }
 }
