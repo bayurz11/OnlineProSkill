5b8eae6d35c35e258354081868de80dff3ac30e7
Bayu <bayurz1001@gmail.com>
2024-06-29 13:56:20 +0700

    up

--- app/Http/Controllers/RiwayatTransaksiController.php
+++ app/Http/Controllers/RiwayatTransaksiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\UserProfile;
 use Illuminate\Http\Request;
+use App\Models\NotifikasiUser;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
@@ -12,13 +13,19 @@ class RiwayatTransaksiController extends Controller
 {
     public function index()
     {
+
         $cart = Session::get('cart', []);
         $user = Auth::user();
         if (!$user) {
             return redirect()->route('/');
         }
         $profile = UserProfile::where('user_id', $user->id)->first();
+        if ($user) {
+            $profile = UserProfile::where('user_id', $user->id)->first();
+        }
 
-        return view('studen.history', compact('user', 'profile', 'cart'));
+        // Ambil notifikasi untuk pengguna yang sedang login
+        $notifikasi = NotifikasiUser::all();
+        return view('studen.history', compact('user', 'profile', 'cart', 'notifikasi'));
     }
 }
