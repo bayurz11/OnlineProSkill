2b5d008afa4b25776fef9cb9da2b51dc3639e4da
Bayu <bayurz1001@gmail.com>
2024-07-01 19:35:13 +0700

    up

--- resources/views/layout/partials/headerarea.blade.php
+++ resources/views/layout/partials/headerarea.blade.php
@@ -135,7 +135,7 @@
                                             .notification-dropdown {
                                                 position: absolute;
                                                 background: #fff;
-                                                border: 1px solid #ddd;
+                                                border: 0px solid #ddd;
                                                 box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
                                                 width: 300px;
                                                 max-height: 400px;
