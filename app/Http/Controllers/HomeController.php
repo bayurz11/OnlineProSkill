3bb1611c9cba23addcdd5c768f1a15119d5061d2
Bayu <bayurz1001@gmail.com>
2024-07-01 17:50:34 +0700

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
 
         // Hitung jumlah notifikasi
         $notifikasiCount = $notifikasi->count();
-        return view('home.index', compact('user', 'profile', 'cart', 'notifikasiCount'));
+        return view('home.index', compact('user', 'profile', 'cart', 'notifikasiCount', 'notifikasi'));
     }
 
     public function classroom()
