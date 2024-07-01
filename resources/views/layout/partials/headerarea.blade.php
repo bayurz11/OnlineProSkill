a51204a20438cafb470f123fc491ffa420b8da83
Bayu <bayurz1001@gmail.com>
2024-07-01 17:34:48 +0700

    up

--- resources/views/layout/partials/headerarea.blade.php
+++ resources/views/layout/partials/headerarea.blade.php
@@ -110,27 +110,7 @@
                                                 <span class="mini-cart-count">0</span>
                                             </a>
                                         </li> --}}
-                                        <li class="nav-item dropdown mini-cart-icon">
-                                            <a class="nav-link dropdown-toggle cart-count-two" href="#"
-                                                id="navbarDropdown" role="button" data-bs-toggle="dropdown"
-                                                aria-expanded="false">
-                                                <i class="far fa-bell" style="color: #007F73;"></i>
-                                                <span class="mini-cart-count">0</span>
-                                            </a>
-                                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                                <li>
-                                                    <hr class="dropdown-divider">
-                                                </li>
-                                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                            </ul>
-                                        </li>
-                                        <style>
-                                            .no-caret::after {
-                                                display: none !important;
-                                            }
-                                        </style>
+
                                     @endauth
                                     @php
                                         use Illuminate\Support\Str;
