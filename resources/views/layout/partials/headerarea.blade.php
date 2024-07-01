1c4bd77badcc173c4c7b11a28f69047862e5e277
Bayu <bayurz1001@gmail.com>
2024-07-01 17:32:39 +0700

    up

--- resources/views/layout/partials/headerarea.blade.php
+++ resources/views/layout/partials/headerarea.blade.php
@@ -104,11 +104,27 @@
                                             </ul>
                                         </li> --}}
 
-                                        <li class="mini-cart-icon" style="margin-left: 20px;">
+                                        {{-- <li class="mini-cart-icon" style="margin-left: 20px;">
                                             <a href="#" class="cart-count-two">
                                                 <i class="far fa-bell" style="color: #007F73;"></i>
                                                 <span class="mini-cart-count">0</span>
                                             </a>
+                                        </li> --}}
+                                        <li class="nav-item dropdown mini-cart-icon">
+                                            <a class="nav-link dropdown-toggle cart-count-two" href="#"
+                                                id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+                                                aria-expanded="false">
+                                                <i class="far fa-bell" style="color: #007F73;"></i>
+                                                <span class="mini-cart-count">0</span>
+                                            </a>
+                                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                <li>
+                                                    <hr class="dropdown-divider">
+                                                </li>
+                                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                            </ul>
                                         </li>
 
                                     @endauth
