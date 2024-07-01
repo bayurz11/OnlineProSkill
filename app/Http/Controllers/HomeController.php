6b661434e14d2b36cf4a6bb969303173d78f73b0
Bayu <bayurz1001@gmail.com>
2024-07-01 18:22:15 +0700

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,21 +15,26 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $user = Auth::user(); // Mengambil pengguna yang sedang login
+        $user = Auth::user();
         $profile = null;
         $cart = Session::get('cart', []);
+
         if ($user) {
-            $profile = UserProfile::where('user_id', $user->id)->first(); // Mengambil profil pengguna yang terkait
+            $profile = UserProfile::where('user_id', $user->id)->first();
         }
+
         // Ambil notifikasi untuk pengguna yang sedang login
-        $notifikasi = NotifikasiUser::where('user_id', $user->id)
+        $notifikasi = $user ? NotifikasiUser::where('user_id', $user->id)
             ->orderBy('created_at', 'desc')
-            ->get();
+            ->get()
+            : collect(); // Menggunakan collect() untuk membuat koleksi kosong jika pengguna belum login
 
         // Hitung jumlah notifikasi dengan status = 1
         $notifikasiCount = $notifikasi->where('status', 1)->count();
+
         return view('home.index', compact('user', 'profile', 'cart', 'notifikasiCount', 'notifikasi'));
     }
+
 
     public function classroom()
     {
