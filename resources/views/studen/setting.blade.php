8f75b570e94f6ff366098a550dd5f3ef7525e840
Bayu <bayurz1001@gmail.com>
2024-07-01 20:46:08 +0700

    up

--- resources/views/studen/setting.blade.php
+++ resources/views/studen/setting.blade.php
@@ -145,7 +145,8 @@
                                                             <label for="phonenumber">No.HP<span
                                                                     style="color: red">*</span> </label>
                                                             <input id="phonenumber" type="number" min="0"
-                                                                name="phonenumber" value="{{ $profile->phone_number }}"
+                                                                max="12" name="phonenumber"
+                                                                value="{{ $profile->phone_number }}"
                                                                 placeholder="masukan no telepon">
                                                         </div>
                                                     </div>
