80d3d67407644a3060ae1e5e0f497bdf2ca20604
Bayu <bayurz1001@gmail.com>
2024-07-01 17:49:01 +0700

    up

--- resources/views/layout/partials/headerarea.blade.php
+++ resources/views/layout/partials/headerarea.blade.php
@@ -104,12 +104,78 @@
                                             </ul>
                                         </li> --}}
 
-                                        <li class="mini-cart-icon" style="margin-left: 20px;">
+                                        {{-- <li class="mini-cart-icon" style="margin-left: 20px;">
                                             <a href="#" class="cart-count-two">
                                                 <i class="far fa-bell" style="color: #007F73;"></i>
                                                 <span class="mini-cart-count">{{ $notifikasiCount }}</span>
                                             </a>
+                                        </li> --}}
+                                        <li class="mini-cart-icon" style="margin-left: 20px;">
+                                            <a href="#" class="cart-count-two" id="notification-icon">
+                                                <i class="far fa-bell" style="color: #007F73;"></i>
+                                                <span class="mini-cart-count">{{ $notifikasiCount }}</span>
+                                            </a>
+                                            <div id="notification-dropdown" class="notification-dropdown"
+                                                style="display: none;">
+                                                @if ($notifikasiCount > 0)
+                                                    <ul>
+                                                        @foreach ($notifikasi as $notif)
+                                                            <li>{{ $notif->message }} -
+                                                                <small>{{ $notif->created_at }}</small></li>
+                                                        @endforeach
+                                                    </ul>
+                                                @else
+                                                    <p>Tidak ada notifikasi.</p>
+                                                @endif
+                                            </div>
                                         </li>
+
+                                        <style>
+                                            .notification-dropdown {
+                                                position: absolute;
+                                                background: #fff;
+                                                border: 1px solid #ddd;
+                                                box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+                                                width: 300px;
+                                                max-height: 400px;
+                                                overflow-y: auto;
+                                                z-index: 1000;
+                                                top: 40px;
+                                                right: 0;
+                                            }
+
+                                            .notification-dropdown ul {
+                                                list-style: none;
+                                                padding: 0;
+                                                margin: 0;
+                                            }
+
+                                            .notification-dropdown ul li {
+                                                padding: 10px;
+                                                border-bottom: 1px solid #eee;
+                                            }
+
+                                            .notification-dropdown ul li:last-child {
+                                                border-bottom: none;
+                                            }
+
+                                            .notification-dropdown p {
+                                                padding: 10px;
+                                            }
+                                        </style>
+
+                                        <script>
+                                            document.getElementById('notification-icon').addEventListener('click', function(event) {
+                                                event.preventDefault();
+                                                var dropdown = document.getElementById('notification-dropdown');
+                                                if (dropdown.style.display === 'none' || dropdown.style.display === '') {
+                                                    dropdown.style.display = 'block';
+                                                } else {
+                                                    dropdown.style.display = 'none';
+                                                }
+                                            });
+                                        </script>
+
 
                                     @endauth
                                     @php
