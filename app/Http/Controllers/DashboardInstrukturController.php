637a0f02c42b9ff028fdaa268a0fbce7d6a7e690
Bayu <bayurz1001@gmail.com>
2024-07-04 09:46:21 +0700

    up

--- app/Http/Controllers/DashboardInstrukturController.php
+++ app/Http/Controllers/DashboardInstrukturController.php
@@ -29,6 +29,7 @@ class DashboardInstrukturController extends Controller
     public function register(Request $request)
     {
         dd($request);
+
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
