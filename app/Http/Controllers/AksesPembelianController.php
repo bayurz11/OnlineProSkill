db671f31ff3d97158784f049b140810f916b84a2
Bayu <bayurz1001@gmail.com>
2024-07-05 09:34:43 +0700

    up

--- app/Http/Controllers/AksesPembelianController.php
+++ app/Http/Controllers/AksesPembelianController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use App\Models\UserProfile;
 use Illuminate\Http\Request;
 use App\Models\NotifikasiUser;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
@@ -27,6 +29,20 @@ class AksesPembelianController extends Controller
             : collect();
 
         $notifikasiCount = $notifikasi->where('status', 1)->count();
-        return view('studen.aksespembelian', compact('user', 'profile', 'cart', 'notifikasi', 'notifikasiCount'));
+
+        // Fetching orders related to the user
+        $orders = Order::where('user_id', $user->id)->with('KelasTatapMuka')->get();
+
+        // Debugging data
+        foreach ($orders as $order) {
+            Log::info('Order ID: ' . $order->id);
+            if ($order->KelasTatapMuka) {
+                Log::info('Kelas Tatap Muka ID: ' . $order->KelasTatapMuka->id);
+                Log::info('Kelas Tatap Muka Name: ' . $order->KelasTatapMuka->nama_kelas);
+            } else {
+                Log::info('Kelas Tatap Muka: Not Found');
+            }
+        }
+        return view('studen.aksespembelian', compact('user', 'profile', 'cart', 'notifikasi', 'notifikasiCount', 'orders'));
     }
 }
--- resources/views/studen/aksespembelian.blade.php
+++ resources/views/studen/aksespembelian.blade.php
@@ -92,51 +92,53 @@
                                         aria-labelledby="all-tab" tabindex="0">
                                         <div class="swiper dashboard-courses-active">
                                             <div class="swiper-wrapper">
-                                                <div class="swiper-slide">
-                                                    <div class="courses__item courses__item-two shine__animate-item">
-                                                        <div class="courses__item-thumb courses__item-thumb-two">
-                                                            <a href="course-details.html" class="shine__animate-link">
-                                                                <img src="{{ asset('public/assets/img/courses/course_thumb01.jpg') }}"
-                                                                    alt="img">
-                                                            </a>
-                                                        </div>
-                                                        <div class="courses__item-content courses__item-content-two">
-                                                            <ul class="courses__item-meta list-wrap">
-                                                                <li class="courses__item-tag">
-                                                                    <a href="course.html">Development</a>
-                                                                </li>
-                                                            </ul>
-                                                            <h5 class="title"><a href="course-details.html">Learning
-                                                                    JavaScript With Imagination</a></h5>
-                                                            <div class="courses__item-content-bottom">
-                                                                <div class="author-two">
-                                                                    <a href="instructor-details.html"><img
-                                                                            src="{{ asset('public/assets/img/courses/course_author001.png') }}"
-                                                                            alt="img">David Millar</a>
+                                                @foreach ($orders as $order)
+                                                    <div class="swiper-slide">
+                                                        <div class="courses__item courses__item-two shine__animate-item">
+                                                            <div class="courses__item-thumb courses__item-thumb-two">
+                                                                <a href="course-details.html" class="shine__animate-link">
+                                                                    <img src="{{ asset('public/assets/img/courses/course_thumb01.jpg') }}"
+                                                                        alt="img">
+                                                                </a>
+                                                            </div>
+                                                            <div class="courses__item-content courses__item-content-two">
+                                                                <ul class="courses__item-meta list-wrap">
+                                                                    <li class="courses__item-tag">
+                                                                        <a href="course.html">Development</a>
+                                                                    </li>
+                                                                </ul>
+                                                                <h5 class="title"><a href="course-details.html">Learning
+                                                                        JavaScript With Imagination</a></h5>
+                                                                <div class="courses__item-content-bottom">
+                                                                    <div class="author-two">
+                                                                        <a href="instructor-details.html"><img
+                                                                                src="{{ asset('public/assets/img/courses/course_author001.png') }}"
+                                                                                alt="img">David Millar</a>
+                                                                    </div>
+                                                                    <div class="avg-rating">
+                                                                        <i class="fas fa-star"></i> (4.8 Reviews)
+                                                                    </div>
                                                                 </div>
-                                                                <div class="avg-rating">
-                                                                    <i class="fas fa-star"></i> (4.8 Reviews)
+                                                                <div class="progress-item progress-item-two">
+                                                                    <h6 class="title">COMPLETE <span>88%</span></h6>
+                                                                    <div class="progress" role="progressbar"
+                                                                        aria-label="Example with label" aria-valuenow="25"
+                                                                        aria-valuemin="0" aria-valuemax="100">
+                                                                        <div class="progress-bar" style="width: 88%">
+                                                                        </div>
+                                                                    </div>
                                                                 </div>
                                                             </div>
-                                                            <div class="progress-item progress-item-two">
-                                                                <h6 class="title">COMPLETE <span>88%</span></h6>
-                                                                <div class="progress" role="progressbar"
-                                                                    aria-label="Example with label" aria-valuenow="25"
-                                                                    aria-valuemin="0" aria-valuemax="100">
-                                                                    <div class="progress-bar" style="width: 88%"></div>
-                                                                </div>
+                                                            <div class="courses__item-bottom-two">
+                                                                <ul class="list-wrap">
+                                                                    <li><i class="flaticon-book"></i>05</li>
+                                                                    <li><i class="flaticon-clock"></i>11h 20m</li>
+                                                                    <li><i class="flaticon-mortarboard"></i>22</li>
+                                                                </ul>
                                                             </div>
-                                                        </div>
-                                                        <div class="courses__item-bottom-two">
-                                                            <ul class="list-wrap">
-                                                                <li><i class="flaticon-book"></i>05</li>
-                                                                <li><i class="flaticon-clock"></i>11h 20m</li>
-                                                                <li><i class="flaticon-mortarboard"></i>22</li>
-                                                            </ul>
                                                         </div>
                                                     </div>
-                                                </div>
-
+                                                @endforeach
                                             </div>
                                         </div>
                                     </div>
