fd99152750b03a6558963fb7cb7124f3a971be33
Bayu <bayurz1001@gmail.com>
2024-07-06 09:11:48 +0700

    up

--- database/migrations/2024_07_06_020759_create_kurikulum_table.php
+++ database/migrations/2024_07_06_020759_create_kurikulum_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('course_id');
             $table->string('title');
+            $table->string('pos');
             $table->foreign('course_id')->references('id')->on('classroom_master')->onDelete('cascade');
             $table->timestamps();
         });
