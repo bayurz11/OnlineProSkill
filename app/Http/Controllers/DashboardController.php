a2ae1c42ddab9aab21d30f2c3f63f816fa6b6f77
Bayu <bayurz1001@gmail.com>
2024-07-06 08:48:05 +0700

    up

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\KelasTatapMuka;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -10,10 +11,11 @@ class DashboardController extends Controller
     public function index()
     {
         $user = Auth::user();
-
+        $course = KelasTatapMuka::with('user')->get();
+        $count = $course->count();
         if (!$user) {
             return redirect()->route('login_admin');
         }
-        return view('admin.dashboard', compact('user'));
+        return view('admin.dashboard', compact('user', 'course', 'count'));
     }
 }
