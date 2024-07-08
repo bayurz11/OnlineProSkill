3a60c7084ad034c478a28303ab5654d0d92c48e2
Bayu <bayurz1001@gmail.com>
2024-07-08 18:56:07 +0700

    up

--- resources/views/layout/partials/headerarea.blade.php
+++ resources/views/layout/partials/headerarea.blade.php
@@ -155,20 +155,11 @@
                                             aria-labelledby="alertModalLabel" aria-hidden="true">
                                             <div class="modal-dialog" role="document">
                                                 <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <h5 class="modal-title" id="alertModalLabel">Notifikasi</h5>
-                                                        <button type="button" class="close" data-dismiss="modal"
-                                                            aria-label="Close">
-                                                            <span aria-hidden="true">&times;</span>
-                                                        </button>
-                                                    </div>
+
                                                     <div class="modal-body">
                                                         <p id="alertMessage"></p>
                                                     </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-primary"
-                                                            data-dismiss="modal">OK</button>
-                                                    </div>
+
                                                 </div>
                                             </div>
                                         </div>
