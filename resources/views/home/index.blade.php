654a0dae851cfb315f8bb50d1c080b8be7d023b1
Bayu <bayurz1001@gmail.com>
2024-07-09 15:11:42 +0700

    up

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -92,8 +92,8 @@
 
 
     <!-- Add Swiper.js CDN -->
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    {{-- <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script> --}}
 
     <!-- Initialize Swiper -->
     <script>
