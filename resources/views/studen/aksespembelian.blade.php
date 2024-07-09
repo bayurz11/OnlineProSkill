c59ed75da2206cd18fac655ebb71b60de3906aec
Bayu <bayurz1001@gmail.com>
2024-07-09 15:19:09 +0700

    up

--- resources/views/studen/aksespembelian.blade.php
+++ resources/views/studen/aksespembelian.blade.php
@@ -131,60 +131,116 @@
                                                     </div>
                                                 @endforeach
                                             </div>
-                                        </div>
-                                    </div>
-
-
-                                    <div class="tab-pane fade" id="business-tab-pane" role="tabpanel"
-                                        aria-labelledby="business-tab" tabindex="0">
-                                        <div class="swiper dashboard-courses-active">
-                                            <div class="swiper-wrapper">
+                                            @if ($orders->count() < 3)
                                                 <div class="swiper-slide">
+                                                    <!-- Duplicate slide to meet minimum requirement -->
                                                     <div class="courses__item courses__item-two shine__animate-item">
                                                         <div class="courses__item-thumb courses__item-thumb-two">
-                                                            <a href="course-details.html" class="shine__animate-link">
+                                                            <a href="javascript:void(0);" class="shine__animate-link">
                                                                 <img src="{{ asset('public/assets/img/courses/course_thumb01.jpg') }}"
                                                                     alt="img">
                                                             </a>
                                                         </div>
                                                         <div class="courses__item-content courses__item-content-two">
-                                                            <ul class="courses__item-meta list-wrap">
-                                                                <li class="courses__item-tag">
-                                                                    <a href="course.html">Development</a>
-                                                                </li>
-                                                            </ul>
-                                                            <h5 class="title"><a href="course-details.html">Learning
-                                                                    JavaScript With Imagination</a></h5>
-                                                            <div class="courses__item-content-bottom">
-                                                                <div class="author-two">
-                                                                    <a href="instructor-details.html"><img
-                                                                            src="{{ asset('public/assets/img/courses/course_author001.png') }}"
-                                                                            alt="img">David Millar</a>
-                                                                </div>
-                                                                <div class="avg-rating">
-                                                                    <i class="fas fa-star"></i> (4.8 Reviews)
-                                                                </div>
-                                                            </div>
-                                                            <div class="progress-item progress-item-two">
-                                                                <h6 class="title">COMPLETE <span>100%</span></h6>
-                                                                <div class="progress" role="progressbar"
-                                                                    aria-label="Example with label" aria-valuenow="25"
-                                                                    aria-valuemin="0" aria-valuemax="100">
-                                                                    <div class="progress-bar" style="width: 100%"></div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="courses__item-bottom-two">
-                                                            <ul class="list-wrap">
-                                                                <li><i class="flaticon-book"></i>05</li>
-                                                                <li><i class="flaticon-clock"></i>11h 20m</li>
-                                                                <li><i class="flaticon-mortarboard"></i>22</li>
-                                                            </ul>
+                                                            <h5 class="title"><a href="javascript:void(0);">Tambahkan
+                                                                    Kursus Lagi</a></h5>
                                                         </div>
                                                     </div>
                                                 </div>
+                                            @endif
+                                        </div>
+                                    </div>
 
+                                    <div class="tab-pane fade" id="business-tab-pane" role="tabpanel"
+                                        aria-labelledby="business-tab" tabindex="0">
+                                        <div class="swiper dashboard-courses-active">
+                                            <div class="swiper-wrapper">
+                                                <!-- Slide untuk kursus selesai -->
+                                                @if ($completedCourses->count() < 3)
+                                                    @for ($i = $completedCourses->count(); $i < 3; $i++)
+                                                        <div class="swiper-slide">
+                                                            <div
+                                                                class="courses__item courses__item-two shine__animate-item">
+                                                                <div class="courses__item-thumb courses__item-thumb-two">
+                                                                    <a href="javascript:void(0);"
+                                                                        class="shine__animate-link">
+                                                                        <img src="{{ asset('public/assets/img/courses/course_thumb01.jpg') }}"
+                                                                            alt="img">
+                                                                    </a>
+                                                                </div>
+                                                                <div
+                                                                    class="courses__item-content courses__item-content-two">
+                                                                    <h5 class="title"><a
+                                                                            href="javascript:void(0);">Kursus Selesai
+                                                                            Dummy</a></h5>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    @endfor
+                                                @endif
+
+                                                @foreach ($completedCourses as $course)
+                                                    <div class="swiper-slide">
+                                                        <div class="courses__item courses__item-two shine__animate-item">
+                                                            <div class="courses__item-thumb courses__item-thumb-two">
+                                                                <a href="course-details.html" class="shine__animate-link">
+                                                                    <img src="{{ $course->gambar ? asset('public/uploads/' . $course->gambar) : asset('public/assets/img/courses/course_thumb01.jpg') }}"
+                                                                        alt="img">
+                                                                </a>
+                                                            </div>
+                                                            <div class="courses__item-content courses__item-content-two">
+                                                                <h5 class="title"><a
+                                                                        href="course-details.html">{{ $course->nama_kursus }}</a>
+                                                                </h5>
+                                                                <div class="courses__item-content-bottom">
+                                                                    <div class="author-two">
+                                                                        <a href="instructor-details.html"><img
+                                                                                src="{{ asset('public/assets/img/courses/course_author001.png') }}"
+                                                                                alt="img">{{ $course->instructor->name }}</a>
+                                                                    </div>
+                                                                    <div class="avg-rating">
+                                                                        <i class="fas fa-star"></i> (4.8 Reviews)
+                                                                    </div>
+                                                                </div>
+                                                                <div class="progress-item progress-item-two">
+                                                                    <h6 class="title">COMPLETE <span>100%</span></h6>
+                                                                    <div class="progress" role="progressbar"
+                                                                        aria-label="Example with label"
+                                                                        aria-valuenow="100" aria-valuemin="0"
+                                                                        aria-valuemax="100">
+                                                                        <div class="progress-bar" style="width: 100%">
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="courses__item-bottom-two">
+                                                                <ul class="list-wrap">
+                                                                    <li><i class="flaticon-book"></i>05</li>
+                                                                    <li><i class="flaticon-clock"></i>11h 20m</li>
+                                                                    <li><i class="flaticon-mortarboard"></i>22</li>
+                                                                </ul>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                @endforeach
                                             </div>
+                                            @if ($completedCourses->count() < 3)
+                                                <div class="swiper-slide">
+                                                    <!-- Duplicate slide to meet minimum requirement -->
+                                                    <div class="courses__item courses__item-two shine__animate-item">
+                                                        <div class="courses__item-thumb courses__item-thumb-two">
+                                                            <a href="javascript:void(0);" class="shine__animate-link">
+                                                                <img src="{{ asset('public/assets/img/courses/course_thumb01.jpg') }}"
+                                                                    alt="img">
+                                                            </a>
+                                                        </div>
+                                                        <div class="courses__item-content courses__item-content-two">
+                                                            <h5 class="title"><a href="javascript:void(0);">Tambahkan
+                                                                    Kursus Lagi</a></h5>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            @endif
                                         </div>
                                     </div>
                                 </div>
@@ -213,6 +269,16 @@
                 };
                 reader.readAsDataURL(file);
             }
+        });
+
+        const swiper = new Swiper('.dashboard-courses-active', {
+            slidesPerView: 1, // Sesuaikan nilai ini jika perlu
+            slidesPerGroup: 1, // Sesuaikan nilai ini jika perlu
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
         });
     </script>
 @endsection
