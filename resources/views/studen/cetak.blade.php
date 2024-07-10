d75e6a3859a3162b0db0c9eaf45193c647adb929
Bayu <bayurz1001@gmail.com>
2024-07-10 19:20:10 +0700

    up

--- resources/views/studen/cetak.blade.php
+++ resources/views/studen/cetak.blade.php
@@ -185,8 +185,8 @@
                     <table>
                         <tr>
                             <td>
-                                Nama :{{ $user->name }}<br>
-                                Email :{{ $user->email }}
+                                Nama : {{ $user->name }}<br>
+                                Email : {{ $user->email }}
                             </td>
                         </tr>
                     </table>
