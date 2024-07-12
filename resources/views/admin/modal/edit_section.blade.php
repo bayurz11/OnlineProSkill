b5d07e048df80da95563694ef334e9ab250e1a70
Bayu <bayurz1001@gmail.com>
2024-07-12 12:16:06 +0700

    up

--- resources/views/admin/modal/edit_section.blade.php
+++ resources/views/admin/modal/edit_section.blade.php
@@ -5,16 +5,17 @@
             <form id="editSectionForm" method="POST" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
-                <input type="hidden" name="course_id" id="course_id">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="sectionModalEditLabel">Edit Kurikulum</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
                 <div class="modal-body">
+                    <input type="hidden" class="form-control" id="kurikulum_id" name="kurikulum_id">
                     <div class="mb-3">
                         <label for="edittitle" class="form-label">Judul<span class="text-danger">*</span></label>
                         <input type="text" class="form-control" id="edittitle" name="title"
                             placeholder="Masukkan judul Kurikulum Anda">
+                    </div>
+                    <div class="mb-3">
+                        <label for="linkedit" class="form-label">Link Materi<span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" id="linkedit" name="link"
+                            placeholder="Masukkan link materi Anda">
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -43,6 +44,8 @@
                     $('#course_id').val(response.id); // Set nilai course_id di dalam modal
                     $('#edittitle').val(response
                         .title); // Set nilai judul section di dalam modal
+                    $('#linkedit').val(response
+                        .link); // Set nilai judul section di dalam modal
 
                     // Set action form dengan id yang benar
                     $('#editSectionForm').attr('action', '/sectionupdate/' +
