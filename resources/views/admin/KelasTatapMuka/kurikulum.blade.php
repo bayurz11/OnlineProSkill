b7b03fe814a77b027b6d4ed86d178aef72948870
Bayu <bayurz1001@gmail.com>
2024-07-12 10:31:19 +0700

    up

--- resources/views/admin/KelasTatapMuka/kurikulum.blade.php
+++ resources/views/admin/KelasTatapMuka/kurikulum.blade.php
@@ -55,7 +55,7 @@
                                         </div>
                                         <div class="card">
                                             <div class="card-body d-flex justify-content-between align-items-center">
-                                                <div>aaa</div>
+                                                Pelajaran {{ $kurikulum->no_urut }}. {{ $kurikulum->title }}
                                                 <div class="d-flex gap-2">
                                                     <button type="button" class="btn btn-outline-primary"
                                                         data-bs-toggle="modal" data-bs-target="#exampleModalEdit"
