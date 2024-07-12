fea8f969023b7db12e20404baaa7d6b01acff8cc
Bayu <bayurz1001@gmail.com>
2024-07-12 09:06:35 +0700

    up

--- resources/views/admin/modal/edit_kurikulum.blade.php
+++ resources/views/admin/modal/edit_kurikulum.blade.php
@@ -1,3 +1,4 @@
+<!-- Modal untuk Edit Kurikulum -->
 <div class="modal fade" id="exampleModalEdit" tabindex="-1" aria-labelledby="exampleModalEditLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
@@ -11,7 +12,7 @@
                 </div>
                 <div class="modal-body">
                     <div class="mb-3">
-                        <label for="title" class="form-label">Judul<span class="text-danger">*</span></label>
+                        <label for="edittitle" class="form-label">Judul<span class="text-danger">*</span></label>
                         <input type="text" class="form-control" id="edittitle" name="title"
                             placeholder="Masukkan judul Kurikulum Anda">
                     </div>
@@ -24,12 +25,13 @@
         </div>
     </div>
 </div>
+
 <script>
     $(document).ready(function() {
-        // Set ID to hidden input when showing edit modal
         $('#exampleModalEdit').on('show.bs.modal', function(event) {
             var button = $(event.relatedTarget);
             var kurikulumId = button.data('id');
+            console.log('Kurikulum ID:', kurikulumId); // Debugging line
             $('#course_id').val(kurikulumId); // Pastikan ada elemen dengan id 'course_id'
 
             $.ajax({
