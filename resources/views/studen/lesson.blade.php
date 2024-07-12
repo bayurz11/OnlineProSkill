82516a262753ccddc6d403e420a48a57a86da6be
Bayu <bayurz1001@gmail.com>
2024-07-12 16:10:21 +0700

    up

--- resources/views/studen/lesson.blade.php
+++ resources/views/studen/lesson.blade.php
@@ -64,54 +64,7 @@
                                                         </div>
                                                     </a>
                                                 </li>
-                                                <li class="course-item">
-                                                    <a href="#" class="course-item-link">
-                                                        <span class="item-name">Create a Simple React App</span>
-                                                        <div class="course-item-meta">
-                                                            <span class="item-meta duration">07:48</span>
-                                                            <span class="item-meta course-item-status">
-                                                                <img src="{{ asset('public/assets/img/icons/lock.svg') }}"
-                                                                    alt="icon">
-                                                            </span>
-                                                        </div>
-                                                    </a>
-                                                </li>
-                                                <li class="course-item">
-                                                    <a href="#" class="course-item-link">
-                                                        <span class="item-name">React for the Rest of us</span>
-                                                        <div class="course-item-meta">
-                                                            <span class="item-meta duration">10:48</span>
-                                                            <span class="item-meta course-item-status">
-                                                                <img src="{{ asset('public/assets/img/icons/lock.svg') }}"
-                                                                    alt="icon">
-                                                            </span>
-                                                        </div>
-                                                    </a>
-                                                </li>
-                                                <li class="course-item">
-                                                    <a href="#" class="course-item-link">
-                                                        <span class="item-name">Create a Simple React App</span>
-                                                        <div class="course-item-meta">
-                                                            <span class="item-meta duration">07:48</span>
-                                                            <span class="item-meta course-item-status">
-                                                                <img src="{{ asset('public/assets/img/icons/lock.svg') }}"
-                                                                    alt="icon">
-                                                            </span>
-                                                        </div>
-                                                    </a>
-                                                </li>
-                                                <li class="course-item">
-                                                    <a href="#" class="course-item-link">
-                                                        <span class="item-name">React for the Rest of us</span>
-                                                        <div class="course-item-meta">
-                                                            <span class="item-meta duration">10:48</span>
-                                                            <span class="item-meta course-item-status">
-                                                                <img src="{{ asset('public/assets/img/icons/lock.svg') }}"
-                                                                    alt="icon">
-                                                            </span>
-                                                        </div>
-                                                    </a>
-                                                </li>
+
                                             </ul>
                                         </div>
                                     </div>
