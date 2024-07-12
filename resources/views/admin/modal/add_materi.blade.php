43caea4de9579d1abb16b45aa588c88267a8e07b
Bayu <bayurz1001@gmail.com>
2024-07-12 11:01:04 +0700

    up

--- resources/views/admin/modal/add_materi.blade.php
+++ resources/views/admin/modal/add_materi.blade.php
@@ -1,4 +1,3 @@
-<!-- Modal Tambah Kurikulum -->
 <div class="modal fade" id="materiModal" tabindex="-1" aria-labelledby="materiModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
@@ -35,15 +34,28 @@
         </div>
     </div>
 </div>
+
 <script>
     $(document).ready(function() {
         $('#materiModal').on('show.bs.modal', function(event) {
             var button = $(event.relatedTarget); // Tombol yang membuka modal
             var kurikulumId = button.data('id'); // Ambil data-id dari tombol
-            console.log('Kurikulum ID:', kurikulumId); // Debugging line
 
-            var modal = $(this);
-            modal.find('.modal-body #kurikulum_id').val(kurikulumId);
+            // Permintaan AJAX ke backend untuk mendapatkan data kurikulum
+            $.ajax({
+                url: '/getKurikulum/' + kurikulumId,
+                type: 'GET',
+                dataType: 'json',
+                success: function(response) {
+                    // Mengisi nilai-nilai modal dengan data yang diterima dari backend
+                    $('#kurikulum_id').val(response.kurikulum_id);
+                    $('#title').val(response.title);
+                    $('#link_materi').val(response.link_materi);
+                },
+                error: function(xhr, status, error) {
+                    console.error('Terjadi kesalahan dalam permintaan AJAX:', error);
+                }
+            });
         });
     });
 </script>
