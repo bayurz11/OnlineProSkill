3de6e25228edafdb53f018f5bdf1bcf432392fee
Bayu <bayurz1001@gmail.com>
2024-07-12 10:19:09 +0700

    up

--- resources/views/admin/KelasTatapMuka/kurikulum.blade.php
+++ resources/views/admin/KelasTatapMuka/kurikulum.blade.php
@@ -37,9 +37,6 @@
                                             <div>
                                                 {{ $kurikulum->no_urut }}. {{ $kurikulum->title }}
                                             </div>
-                                            <div>
-                                                Materi
-                                            </div>
                                             <div class="d-flex gap-2">
                                                 <button type="button" class="btn btn-outline-primary"
                                                     data-bs-toggle="modal" data-bs-target="#exampleModalEdit"
@@ -54,6 +51,31 @@
                                                     data-bs-toggle="modal" data-bs-target="#materiModal">
                                                     <i class="btn-icon-prepend" data-feather="plus-circle"></i> Tambah Sub
                                                 </button>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12 grid-margin stretch-card">
+                                            <div class="card">
+                                                <div class="card-body d-flex justify-content-between align-items-center">
+                                                    <div>
+                                                        {{ $kurikulum->no_urut }}. {{ $kurikulum->title }}
+                                                    </div>
+                                                    <div class="d-flex gap-2">
+                                                        <button type="button" class="btn btn-outline-primary"
+                                                            data-bs-toggle="modal" data-bs-target="#exampleModalEdit"
+                                                            title="Edit Kurikulum" data-id="{{ $kurikulum->id }}">
+                                                            <i class="btn-icon-prepend" data-feather="edit"></i>
+                                                        </button>
+                                                        <button onclick="hapus('{{ $kurikulum->id }}')"
+                                                            class="btn btn-outline-danger btn-icon" title="Hapus">
+                                                            <i data-feather="trash-2"></i>
+                                                        </button>
+                                                        <button type="button" class="btn btn-outline-primary"
+                                                            data-bs-toggle="modal" data-bs-target="#materiModal">
+                                                            <i class="btn-icon-prepend" data-feather="plus-circle"></i>
+                                                            Tambah Sub
+                                                        </button>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
