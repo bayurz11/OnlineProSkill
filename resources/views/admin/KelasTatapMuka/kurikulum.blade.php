62ad48dca3c3a71c0220ccd66b1a6168d7b9b688
Bayu <bayurz1001@gmail.com>
2024-07-12 10:20:33 +0700

    up

--- resources/views/admin/KelasTatapMuka/kurikulum.blade.php
+++ resources/views/admin/KelasTatapMuka/kurikulum.blade.php
@@ -57,7 +57,7 @@
                                         <div class="card">
                                             <div class="card-body d-flex justify-content-between align-items-center">
                                                 <div>
-                                                    {{ $kurikulum->no_urut }}. Materi/ link materi
+                                                    Materi/ link materi
                                                 </div>
                                                 <div class="d-flex gap-2">
                                                     <button type="button" class="btn btn-outline-primary"
