955eebc7fdf3c2fd9b95bd7ab6276f04ccd3ed7e
Bayu <bayurz1001@gmail.com>
2024-07-13 09:52:14 +0700

    up

--- resources/views/studen/lesson.blade.php
+++ resources/views/studen/lesson.blade.php
@@ -134,7 +134,7 @@
                     iframe.setAttribute('frameborder', '0');
                     iframe.setAttribute('allow',
                         'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-                    );
+                        );
                     iframe.setAttribute('allowfullscreen', '');
 
                     contentDisplay.appendChild(iframe);
