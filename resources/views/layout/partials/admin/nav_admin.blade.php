1c456262e02e17eb43717ab92159c4e89cfb218e
Bayu <bayurz1001@gmail.com>
2024-07-15 13:20:31 +0700

    up

--- resources/views/layout/partials/admin/nav_admin.blade.php
+++ resources/views/layout/partials/admin/nav_admin.blade.php
@@ -80,6 +80,73 @@
                 </div>
             </li>
 
+            <li class="nav-item nav-category">Pages</li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#general-pages" role="button" aria-expanded="false"
+                    aria-controls="general-pages">
+                    <i class="link-icon" data-feather="book"></i>
+                    <span class="link-title">Special pages</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="general-pages">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="pages/general/blank-page.html" class="nav-link">Blank page</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/general/faq.html" class="nav-link">Faq</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/general/invoice.html" class="nav-link">Invoice</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/general/profile.html" class="nav-link">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/general/pricing.html" class="nav-link">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/general/timeline.html" class="nav-link">Timeline</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#authPages" role="button"
+                    aria-expanded="false" aria-controls="authPages">
+                    <i class="link-icon" data-feather="unlock"></i>
+                    <span class="link-title">Authentication</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="authPages">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="pages/auth/login.html" class="nav-link">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/auth/register.html" class="nav-link">Register</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#errorPages" role="button"
+                    aria-expanded="false" aria-controls="errorPages">
+                    <i class="link-icon" data-feather="cloud-off"></i>
+                    <span class="link-title">Error</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="errorPages">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="pages/error/404.html" class="nav-link">404</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/error/500.html" class="nav-link">500</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
             <li class="nav-item">
                 <a class="nav-link" data-bs-toggle="collapse" href="#forms" role="button" aria-expanded="false"
                     aria-controls="forms">
@@ -166,73 +233,6 @@
                         </li>
                         <li class="nav-item">
                             <a href="pages/icons/mdi-icons.html" class="nav-link">Mdi Icons</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item nav-category">Pages</li>
-            <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#general-pages" role="button"
-                    aria-expanded="false" aria-controls="general-pages">
-                    <i class="link-icon" data-feather="book"></i>
-                    <span class="link-title">Special pages</span>
-                    <i class="link-arrow" data-feather="chevron-down"></i>
-                </a>
-                <div class="collapse" id="general-pages">
-                    <ul class="nav sub-menu">
-                        <li class="nav-item">
-                            <a href="pages/general/blank-page.html" class="nav-link">Blank page</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/general/faq.html" class="nav-link">Faq</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/general/invoice.html" class="nav-link">Invoice</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/general/profile.html" class="nav-link">Profile</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/general/pricing.html" class="nav-link">Pricing</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/general/timeline.html" class="nav-link">Timeline</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#authPages" role="button"
-                    aria-expanded="false" aria-controls="authPages">
-                    <i class="link-icon" data-feather="unlock"></i>
-                    <span class="link-title">Authentication</span>
-                    <i class="link-arrow" data-feather="chevron-down"></i>
-                </a>
-                <div class="collapse" id="authPages">
-                    <ul class="nav sub-menu">
-                        <li class="nav-item">
-                            <a href="pages/auth/login.html" class="nav-link">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/auth/register.html" class="nav-link">Register</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#errorPages" role="button"
-                    aria-expanded="false" aria-controls="errorPages">
-                    <i class="link-icon" data-feather="cloud-off"></i>
-                    <span class="link-title">Error</span>
-                    <i class="link-arrow" data-feather="chevron-down"></i>
-                </a>
-                <div class="collapse" id="errorPages">
-                    <ul class="nav sub-menu">
-                        <li class="nav-item">
-                            <a href="pages/error/404.html" class="nav-link">404</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/error/500.html" class="nav-link">500</a>
                         </li>
                     </ul>
                 </div>
