f8be32161547a362ab933acbc65e28030670bd02
Bayu <bayurz1001@gmail.com>
2024-07-15 14:22:22 +0700

    up

--- resources/views/admin/modal/edit_course.blade.php
+++ resources/views/admin/modal/edit_course.blade.php
@@ -10,16 +10,32 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="btn-close"></button>
                 </div>
                 <div class="modal-body">
+
                     <div class="mb-3">
-                        <label for="edit_nama_kursus" class="form-label">Nama Kursus<span
+                        <label for="nama_kursus" class="form-label">Nama Kursus<span
                                 class="text-danger">*</span></label>
                         <input type="text" class="form-control" id="edit_nama_kursus" name="nama_kursus"
                             placeholder="Masukkan Nama Kursus Anda">
                     </div>
-
+                    <div class="mb-3">
+                        <label for="durasi" class="form-label">Durasi Kursus<span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" id="edit_durasi" name="durasi"
+                            placeholder="Durasi kursus">
+                    </div>
+                    <div class="mb-3">
+                        <label for="sertifikat" class="form-label">Sertifikat<span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" id="edit_sertifikat" name="sertifikat"
+                            placeholder="Apakah mendapatkan sertifikat">
+                    </div>
+                    <div class="mb-3">
+                        <label for="kuota" class="form-label">Kuota Perkelas<span
+                                class="text-danger">*</span></label>
+                        <input type="text" class="form-control" id="edit_kuota" name="kuota"
+                            placeholder="Masukkan Jumlah Kuota yang Disediakan">
+                    </div>
                     <div class="mb-3">
                         <label class="form-label">Kategori<span class="text-danger">*</span></label>
-                        <select id="edit_category" class="form-select" name="kategori_id" data-width="100%">
+                        <select id="edit_category" class="form-select" name="kategori_id">
                             <option value="">Pilih Kategori</option>
                             @foreach ($categori as $category)
                                 @if ($category->status == 1)
@@ -28,15 +44,13 @@
                             @endforeach
                         </select>
                     </div>
-
                     <div class="mb-3">
                         <label for="edit_subcategory" class="form-label">Subkategori<span
                                 class="text-danger">*</span></label>
-                        <select id="edit_subcategory" class="form-control" name="subkategori_id" disabled>
+                        <select id="edit_subcategory" class="form-select" name="subkategori_id" disabled>
                             <option value="">Pilih Subkategori</option>
                         </select>
                     </div>
-
                     <div class="mb-3">
                         <label for="edit_tingkat" class="form-label">Tingkat <span class="text-danger">*</span></label>
                         <select id="edit_tingkat" class="form-select" name="tingkat" required>
@@ -51,11 +65,10 @@
                         <label for="edit_content" class="form-label">Deskripsi<span class="text-danger">*</span></label>
                         <textarea id="edit_content" name="content" style="height: 400px; width: 100%; font-size: 18px;"></textarea>
                         <input type="hidden" id="edit_content_input" name="content">
-
                     </div>
 
                     <div class="mb-3">
-                        <label for="edit_include" class="form-label">Yang akan di pelajari <span
+                        <label for="edit_include" class="form-label">Yang akan dipelajari <span
                                 class="text-danger">*</span></label>
                         <div id="edit-include-container">
                             <div class="input-group mb-2">
@@ -66,30 +79,20 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="edit_price" class="form-label">Harga (Rp)<span class="text-danger">*</span></label>
+                        <label for="edit_price" class="form-label">Harga (Rp)<span
+                                class="text-danger">*</span></label>
                         <input type="number" class="form-control" id="edit_price" name="price">
                     </div>
-
                     <div class="mb-3">
                         <label for="edit_discount" class="form-label">Diskon %</label>
                         <input type="text" class="form-control" id="edit_discount" name="discount"
                             oninput="calculateEditDiscountedPrice()">
                     </div>
-
                     <div class="mb-3">
-                        <label for="edit_discountedPrice">Harga Setelah Diskon (Rp)</label>
+                        <label for="edit_discountedPrice" class="form-label">Harga Setelah Diskon (Rp)</label>
                         <input type="text" class="form-control" id="edit_discountedPrice" name="discountedPrice"
                             readonly>
                     </div>
-
-                    <div class="mb-3">
-                        <div>
-                            <input type="checkbox" id="edit_free" name="free" value="1"
-                                onchange="toggleEditPriceAndDiscount()">
-                            <label for="edit_free">Free</label>
-                        </div>
-                    </div>
-
                     <div class="mb-3">
                         <label class="form-label" for="edit_gambar">Gambar Kursus<span
                                 class="text-danger">*</span></label>
@@ -98,14 +101,12 @@
                     </div>
                     <img id="edit_preview" src="#" alt="Preview banner"
                         style="max-width: 100%; max-height: 200px; display: none;">
-
                     <div class="mb-3">
                         <label for="edit_tag" class="form-label">Tag</label>
                         <input type="text" class="form-control" id="edit_tag" name="tag">
-                        <small class="text-secondary">Note : Isi Dengan Tags kursus yang relevan</small>
+                        <small class="text-secondary">Note: Isi Dengan Tags kursus yang relevan</small>
                     </div>
                 </div>
-
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Keluar</button>
                     <button type="submit" class="btn btn-primary">Simpan</button>
@@ -117,21 +118,28 @@
 
 <script>
     $(document).ready(function() {
-        // Initialize an empty object to store editors
         const editors = {};
 
-        // Pengambilan data saat tombol edit diklik
         $('.edit-button').on('click', function() {
             const id = $(this).data('id');
-            fetch(`/Course/${id}/edit`)
+            fetch(`/class/${id}/edit`)
                 .then(response => response.json())
                 .then(data => {
-                    // Pengaturan nilai form edit
-                    $('#editCourseForm').attr('action', `/Course/${id}`);
+                    console.log(data);
+
+                    $('#editCourseForm').attr('action', `/class/${id}`);
                     $('#edit_nama_kursus').val(data.nama_kursus);
+                    $('#edit_durasi').val(data.durasi);
+                    $('#edit_sertifikat').val(data.sertifikat);
+                    $('#edit_kuota').val(data.kuota);
                     $('#edit_category').val(data.kategori_id);
 
-                    // Pengaturan subkategori berdasarkan kategori
+                    if (data.course_type === 'online') {
+                        $('#edit_online').prop('checked', true);
+                    } else if (data.course_type === 'offline') {
+                        $('#edit_offline').prop('checked', true);
+                    }
+
                     const categoryId = data.kategori_id;
                     const subcategorySelect = $('#edit_subcategory');
                     subcategorySelect.prop('disabled', !categoryId);
@@ -158,82 +166,87 @@
                         subcategorySelect.html('<option value="">Pilih Subkategori</option>');
                     }
 
-                    // Pengaturan nilai tingkat
                     $('#edit_tingkat').val(data.tingkat);
 
-                    // Pengaturan konten deskripsi dengan CKEditor
                     if (editors[id]) {
                         editors[id].destroy().then(() => {
+                            delete editors[id];
                             createEditor(id, data.content);
                         });
                     } else {
                         createEditor(id, data.content);
                     }
 
-                    function createEditor(id, content) {
-                        ClassicEditor.create(document.querySelector('#edit_content'))
-                            .then(editor => {
-                                editors[id] = editor; // Store the editor instance
-                                editor.setData(
-                                content); // Mengatur nilai konten dari data yang diterima
-                                editor.model.document.on('change:data', () => {
-                                    const content_input = document.querySelector(
-                                        '#edit_content_input');
-                                    content_input.value = editor.getData();
-                                });
-                            })
-                            .catch(error => {
-                                console.error(error);
-                            });
-                    }
-
-                    // Pengaturan harga dan diskon
                     $('#edit_price').val(data.price);
                     $('#edit_discount').val(data.discount);
                     $('#edit_discountedPrice').val(data.discountedPrice);
-                    $('#edit_free').prop('checked', data.free);
 
-                    // Pengaturan pratinjau gambar
                     if (data.gambar) {
                         $('#edit_preview').attr('src', `/public/uploads/${data.gambar}`).show();
                     } else {
                         $('#edit_preview').hide();
                     }
 
-                    // Pengaturan nilai tag
                     let tagValue = '';
+                    try {
+                        const parsedTag = JSON.parse(data.tag);
 
-                    if (Array.isArray(data.tag) && data.tag.length > 0) {
-                        tagValue = data.tag[0]
-                        .value; // Mengambil nilai dari item pertama dalam array
-                    } else if (typeof data.tag === 'object' && data.tag !== null) {
-                        tagValue = data.tag.value; // Jika data.tag adalah objek tunggal
-                    } else if (typeof data.tag === 'string') {
-                        tagValue = data.tag; // Jika data.tag adalah string tunggal
+                        if (Array.isArray(parsedTag) && parsedTag.length > 0) {
+                            tagValue = parsedTag[0].value;
+                        } else if (typeof parsedTag === 'object' && parsedTag !== null) {
+                            tagValue = parsedTag.value;
+                        } else if (typeof parsedTag === 'string') {
+                            tagValue = parsedTag;
+                        }
+                    } catch (e) {
+                        if (typeof data.tag === 'string') {
+                            tagValue = data.tag;
+                        }
                     }
 
-                    $('#edit_tag').val(tagValue); // Menetapkan nilai tag ke dalam elemen input
+                    $('#edit_tag').val(tagValue);
 
-                    // Pengaturan field include
                     const includeContainer = $('#edit-include-container');
                     includeContainer.html('');
-                    data.include.forEach(item => {
-                        const inputGroup = $(`
-                        <div class="input-group mb-2">
-                            <input type="text" class="form-control" name="include[]" value="${item}">
-                            <button class="btn btn-danger remove-edit-include" type="button">-</button>
-                        </div>
-                    `);
-                        includeContainer.append(inputGroup);
-                    });
 
-                    // Mengatur disable/enable harga dan diskon berdasarkan status free
+                    try {
+                        const includes = JSON.parse(data.include);
+
+                        if (Array.isArray(includes)) {
+                            includes.forEach(item => {
+                                const inputGroup = $(`
+                                <div class="input-group mb-2">
+                                    <input type="text" class="form-control" name="include[]" value="${item}">
+                                    <button class="btn btn-danger remove-edit-include" type="button">-</button>
+                                </div>
+                            `);
+                                includeContainer.append(inputGroup);
+                            });
+                        } else {
+                            console.error('Parsed include is not an array:', includes);
+                        }
+                    } catch (e) {
+                        console.error('Error parsing include:', e, data.include);
+                    }
+
                     toggleEditPriceAndDiscount();
                 })
-                .catch(error => console.error('Error fetching course data:', error));
+                .catch(error => console.error('Error fetching class data:', error));
         });
 
-        // Menampilkan pratinjau gambar yang diunggah
+        function createEditor(id, content) {
+            ClassicEditor.create(document.querySelector('#edit_content'))
+                .then(editor => {
+                    editors[id] = editor;
+                    editor.setData(content);
+                    editor.model.document.on('change:data', () => {
+                        const content_input = document.querySelector('#edit_content_input');
+                        content_input.value = editor.getData();
+                    });
+                })
+                .catch(error => console.error(error));
+        }
+
         $('#edit_gambar').change(function() {
             readURL(this);
         });
@@ -248,7 +261,6 @@
             }
         }
 
-        // Menambahkan dan menghapus field include pada form edit
         $('#add-edit-include').on('click', function() {
             const inputGroup = $(`
             <div class="input-group mb-2">
@@ -263,7 +275,6 @@
             $(this).closest('.input-group').remove();
         });
 
-        // Menghitung harga setelah diskon pada form edit
         function calculateEditDiscountedPrice() {
             const price = parseFloat($('#edit_price').val());
             const discount = parseFloat($('#edit_discount').val());
@@ -275,7 +286,6 @@
 
         $('#edit_discount').on('input', calculateEditDiscountedPrice);
 
-        // Menyembunyikan/menampilkan field harga dan diskon berdasarkan status free
         function toggleEditPriceAndDiscount() {
             const isFree = $('#edit_free').is(':checked');
             $('#edit_price, #edit_discount, #edit_discountedPrice').prop('disabled', isFree);
