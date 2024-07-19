329f3d61ffa93c25a3eeee2d91e8b30db355c517
Bayu <bayurz1001@gmail.com>
2024-07-19 15:33:01 +0700

    up

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -25,6 +25,6 @@ class SearchController extends Controller
             ->where('nama_kursus', 'like', '%' . $search_term . '%')
             ->get();
 
-        return view('search_results', compact('results', 'categori', ' cart'));
+        return view('search_results', compact('results', 'categori', 'cart'));
     }
 }
