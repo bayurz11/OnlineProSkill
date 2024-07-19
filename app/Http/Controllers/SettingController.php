fa1e18d65c2e7818fd89532362246ff723c00f1d
Bayu <bayurz1001@gmail.com>
2024-07-19 12:12:16 +0700

    up

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -83,7 +83,7 @@ class SettingController extends Controller
             'phonenumber' => 'required|string|max:15',
             'alamat' => 'required|string|max:255',
             'bio' => 'nullable|string',
-            'foto' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:5000'
+            'foto' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:10000'
         ]);
 
         // Menangani upload gambar
