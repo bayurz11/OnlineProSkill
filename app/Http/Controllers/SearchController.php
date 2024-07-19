08b7586bae5f8217b8fd7d8d4e5d4c62bd364fad
Bayu <bayurz1001@gmail.com>
2024-07-19 15:32:36 +0700

    up

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -15,6 +15,7 @@ class SearchController extends Controller
 {
     public function search(Request $request)
     {
+        $cart = Session::get('cart', []);
         $categori = Categories::all();
         $category_id = $request->input('category_id');
         $search_term = $request->input('search_term');
@@ -24,6 +25,6 @@ class SearchController extends Controller
             ->where('nama_kursus', 'like', '%' . $search_term . '%')
             ->get();
 
-        return view('search_results', compact('results', 'categori'));
+        return view('search_results', compact('results', 'categori', ' cart'));
     }
 }
