db3c381c1370951e15fdf45621ded864d297ab4b
Bayu <bayurz1001@gmail.com>
2024-07-22 15:41:20 +0700

    up

--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -76,8 +76,6 @@
                                             @endif
                                         @endforeach
                                     </ul>
-
-
                                     <div class="show-more">
                                         <a href="#">Show More +</a>
                                     </div>
@@ -165,7 +163,6 @@
                                         <div class="courses-top-right m-0 ms-md-auto">
                                             <span class="sort-by">Sort By:</span>
                                             <div class="courses-top-right m-0 ms-md-auto">
-                                                <span class="sort-by">Sort By:</span>
                                                 <div class="courses-top-right-select">
                                                     <form method="GET" action="{{ route('search') }}">
                                                         <select name="orderby" class="orderby"
@@ -189,45 +186,7 @@
 
                                         </div>
 
-                                        <ul class="nav nav-tabs courses__nav-tabs" id="myTab" role="tablist">
-                                            <li class="nav-item" role="presentation">
-                                                <button class="nav-link active" id="grid-tab" data-bs-toggle="tab"
-                                                    data-bs-target="#grid" type="button" role="tab"
-                                                    aria-controls="grid" aria-selected="true">
-                                                    <svg width="18" height="18" viewBox="0 0 18 18"
-                                                        fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path
-                                                            d="M6 1H2C1.44772 1 1 1.44772 1 2V6C1 6.55228 1.44772 7 2 7H6C6.55228 7 7 6.55228 7 6V2C7 1.44772 6.55228 1 6 1Z"
-                                                            stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                            stroke-linejoin="round" />
-                                                        <path
-                                                            d="M16 1H12C11.4477 1 11 1.44772 11 2V6C11 6.55228 11.4477 7 12 7H16C16.5523 7 17 6.55228 17 6V2C17 1.44772 16.5523 1 16 1Z"
-                                                            stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                            stroke-linejoin="round" />
-                                                        <path
-                                                            d="M6 11H2C1.44772 11 1 11.4477 1 12V16C1 16.5523 1.44772 17 2 17H6C6.55228 17 7 16.5523 7 16V12C7 11.4477 6.55228 11 6 11Z"
-                                                            stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                            stroke-linejoin="round" />
-                                                        <path
-                                                            d="M16 11H12C11.4477 11 11 11.4477 11 12V16C11 16.5523 11.4477 17 12 17H16C16.5523 17 17 16.5523 17 16V12C17 11.4477 16.5523 11 16 11Z"
-                                                            stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                            stroke-linejoin="round" />
-                                                    </svg>
-                                                </button>
-                                            </li>
-                                            <li class="nav-item" role="presentation">
-                                                <button class="nav-link" id="list-tab" data-bs-toggle="tab"
-                                                    data-bs-target="#list" type="button" role="tab"
-                                                    aria-controls="list" aria-selected="false">
-                                                    <svg width="19" height="15" viewBox="0 0 19 15"
-                                                        fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path
-                                                            d="M1.5 6C0.67 6 0 6.67 0 7.5C0 8.33 0.67 9 1.5 9C2.33 9 3 8.33 3 7.5C3 6.67 2.33 6 1.5 6ZM1.5 0C0.67 0 0 0.67 0 1.5C0 2.33 0.67 3 1.5 3C2.33 3 3 2.33 3 1.5C3 0.67 2.33 0 1.5 0ZM1.5 12C0.67 12 0 12.68 0 13.5C0 14.32 0.68 15 1.5 15C2.32 15 3 14.32 3 13.5C3 12.68 2.33 12 1.5 12ZM5.5 14.5H17.5C18.05 14.5 18.5 14.05 18.5 13.5C18.5 12.95 18.05 12.5 17.5 12.5H5.5C4.95 12.5 4.5 12.95 4.5 13.5C4.5 14.05 4.95 14.5 5.5 14.5ZM5.5 8.5H17.5C18.05 8.5 18.5 8.05 18.5 7.5C18.5 6.95 18.05 6.5 17.5 6.5H5.5C4.95 6.5 4.5 6.95 4.5 7.5C4.5 8.05 4.95 8.5 5.5 8.5ZM4.5 1.5C4.5 2.05 4.95 2.5 5.5 2.5H17.5C18.05 2.5 18.5 2.05 18.5 1.5C18.5 0.95 18.05 0.5 17.5 0.5H5.5C4.95 0.5 4.5 0.95 4.5 1.5Z"
-                                                            fill="currentColor" />
-                                                    </svg>
-                                                </button>
-                                            </li>
-                                        </ul>
+
                                     </div>
                                 </div>
                             </div>
@@ -247,86 +206,80 @@
                                         <div class="courses__item-content">
 
                                             <h5 class="title"><a
-                                                    href="{{ route('classroomdetail', ['id' => $cours->id]) }}">{{ $cours->nama_kursus }}</a>
+                                                    href="{{ route('classroomdetail', ['id' => $cours->id]) }}">{{ $cours->nama_kelas }}</a>
                                             </h5>
-                                            <p class="author">By <a href="#">{{ $cours->user->name }}</a></p>
-                                            <div class="courses__item-bottom">
-                                                <div class="button">
-                                                    <a href="{{ route('classroomdetail', ['id' => $cours->id]) }}">
-                                                        <span class="text">Detail</span>
-                                                        <i class="flaticon-arrow-right"></i>
-                                                    </a>
-                                                </div>
-                                                <h5 class="price">Rp {{ number_format($cours->price, 0, ',', ',') }}</h5>
-                                                @if ($cours->course_type == 'online')
-                                                    <span class="badge bg-primary">Online</span>
-                                                @else
-                                                    <span class="badge bg-secondary">Offline</span>
-                                                @endif
+                                            <div class="courses__item-info">
+                                                <ul class="list-wrap">
+                                                    <li><span> <i class="fas fa-file"></i>
+                                                            {{ $cours->Kategori->name_category }}</span></li>
+                                                    <li><span> <i class="fas fa-tv"></i>
+                                                            {{ $cours->Tipe->nama_tipe }}</span></li>
+                                                </ul>
                                             </div>
+                                            <div class="courses__item-bottom">
+                                                <ul class="list-wrap">
+                                                    <li class="courses__item-price">
+                                                        <span>IDR</span>{{ number_format($cours->harga, 0, ',', '.') }}
+                                                    </li>
+                                                    <li class="courses__item-rating">
+                                                        <ul class="list-wrap">
+                                                            <li><a href="#">4.7</a></li>
+                                                            <li class="active"><i class="fas fa-star"></i></li>
+                                                        </ul>
+                                                    </li>
+                                                </ul>
+                                            </div>
+
                                         </div>
                                     </div>
                                 </div>
                             @endforeach
                         </div>
-
+                        {{ $results->appends(request()->except('page'))->links('pagination::bootstrap-4') }}
                     </div>
                 </div>
+            @endif
         </div>
     </section>
     <!-- all-courses-end -->
-    @endif
+@endsection
 
-
+@push('scripts')
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
+        function toggleAllCategories(checkbox) {
             const checkboxes = document.querySelectorAll('.category-checkbox');
-            const allCategoriesCheckbox = document.getElementById('all_categories');
+            checkboxes.forEach(cb => cb.checked = checkbox.checked);
+            updateURL();
+        }
 
-            function updateUrl(selectedCategories) {
-                const url = new URL(window.location.href);
-                url.searchParams.set('categories', selectedCategories.join(','));
-                window.location.href = url.toString();
+        function updateURL() {
+            const selectedCategories = Array.from(document.querySelectorAll('.category-checkbox:checked'))
+                .map(cb => cb.getAttribute('data-category-id'));
+
+            const url = new URL(window.location.href);
+            if (selectedCategories.length > 0) {
+                url.searchParams.set('category_ids', selectedCategories.join(','));
+            } else {
+                url.searchParams.delete('category_ids');
             }
+            window.location.href = url.toString();
+        }
 
-            function toggleAllCategories(source) {
-                if (source.checked) {
-                    // Uncheck all category checkboxes
-                    checkboxes.forEach(checkbox => {
-                        checkbox.checked = false;
-                    });
-                    // Update URL to show all data
-                    updateUrl([]);
+        document.querySelectorAll('.category-checkbox').forEach(checkbox => {
+            checkbox.addEventListener('change', function() {
+                if (!this.checked) {
+                    document.getElementById('all_categories').checked = false;
+                } else {
+                    const allChecked = Array.from(document.querySelectorAll('.category-checkbox')).every(
+                        cb => cb.checked);
+                    document.getElementById('all_categories').checked = allChecked;
                 }
-            }
-
-            checkboxes.forEach(checkbox => {
-                checkbox.addEventListener('change', function() {
-                    if (this.checked) {
-                        allCategoriesCheckbox.checked = false;
-                    }
-
-                    // Get selected categories
-                    const selectedCategories = Array.from(checkboxes)
-                        .filter(checkbox => checkbox.checked)
-                        .map(checkbox => checkbox.value);
-
-                    // Update URL with selected categories
-                    updateUrl(selectedCategories);
-                });
-            });
-
-            allCategoriesCheckbox.addEventListener('change', function() {
-                if (this.checked) {
-                    // Uncheck all category checkboxes
-                    checkboxes.forEach(checkbox => {
-                        checkbox.checked = false;
-                    });
-                    // Update URL to show all data
-                    updateUrl([]);
-                }
+                updateURL();
             });
         });
-    </script>
 
-@endsection
+        document.getElementById('all_categories').addEventListener('change', function() {
+            toggleAllCategories(this);
+        });
+    </script>
+@endpush
