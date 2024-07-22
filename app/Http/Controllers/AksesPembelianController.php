0c6e63c04a2dfe59cc6b07ed72e3bd19b79ab0b9
Bayu <bayurz1001@gmail.com>
2024-07-22 13:08:22 +0700

    up

--- app/Http/Controllers/AksesPembelianController.php
+++ app/Http/Controllers/AksesPembelianController.php
@@ -16,6 +16,40 @@ use Illuminate\Support\Facades\Session;
 
 class AksesPembelianController extends Controller
 {
+    // public function index()
+    // {
+    //     $categori = Categories::all();
+    //     $cart = Session::get('cart', []);
+    //     $user = Auth::user();
+    //     if (!$user) {
+    //         return redirect()->route('home');
+    //     }
+
+    //     $profile = UserProfile::where('user_id', $user->id)->first();
+
+    //     $notifikasi = $user ? NotifikasiUser::where('user_id', $user->id)
+    //         ->orderBy('created_at', 'desc')
+    //         ->get()
+    //         : collect();
+
+    //     $notifikasiCount = $notifikasi->where('status', 1)->count();
+
+    //     // Fetching orders related to the user
+    //     $orders = Order::where('user_id', $user->id)->with('KelasTatapMuka')->get();
+    //     $kurikulum = Kurikulum::all();
+    //     // Debugging data
+    //     foreach ($orders as $order) {
+    //         Log::info('Order ID: ' . $order->id);
+    //         if ($order->KelasTatapMuka) {
+    //             Log::info('Kelas Tatap Muka ID: ' . $order->KelasTatapMuka->id);
+    //             Log::info('Kelas Tatap Muka Name: ' . $order->KelasTatapMuka->nama_kelas);
+    //         } else {
+    //             Log::info('Kelas Tatap Muka: Not Found');
+    //         }
+    //     }
+
+    //     return view('studen.aksespembelian', compact('user', 'categori', 'profile', 'cart', 'notifikasi', 'notifikasiCount', 'orders', 'kurikulum'));
+    // }220724
     public function index()
     {
         $categori = Categories::all();
@@ -34,9 +68,14 @@ class AksesPembelianController extends Controller
 
         $notifikasiCount = $notifikasi->where('status', 1)->count();
 
-        // Fetching orders related to the user
-        $orders = Order::where('user_id', $user->id)->with('KelasTatapMuka')->get();
+        // Fetching orders related to the user with status PAID and SETTLED
+        $orders = Order::where('user_id', $user->id)
+            ->whereIn('status', ['PAID', 'SETTLED'])
+            ->with('KelasTatapMuka')
+            ->get();
+
         $kurikulum = Kurikulum::all();
+
         // Debugging data
         foreach ($orders as $order) {
             Log::info('Order ID: ' . $order->id);
@@ -50,6 +89,7 @@ class AksesPembelianController extends Controller
 
         return view('studen.aksespembelian', compact('user', 'categori', 'profile', 'cart', 'notifikasi', 'notifikasiCount', 'orders', 'kurikulum'));
     }
+
     public function lesson($id)
     {
         $categori = Categories::all();
