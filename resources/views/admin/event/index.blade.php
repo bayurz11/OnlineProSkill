6863dfb0e8ee6161c45e2c50570fdcaeca55d6b9
Bayu <bayurz1001@gmail.com>
2024-07-26 14:12:33 +0700

    up

--- resources/views/admin/event/index.blade.php
+++ resources/views/admin/event/index.blade.php
@@ -23,7 +23,7 @@
                         <button type="button" class="btn btn-outline-primary position-absolute top-0 end-0 mt-3 me-3"
                             data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="btn-icon-prepend"
                                 data-feather="plus-circle"></i>
-                            Kategori
+                            Event
                         </button>
                         <p class="text-muted mb-3">Jumlah Event : </p>
                         <div class="table-responsive">
