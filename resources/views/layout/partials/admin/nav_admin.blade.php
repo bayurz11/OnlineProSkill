1200b7b8175a47ae68583e144618f8c871919218
Bayu <bayurz1001@gmail.com>
2024-07-27 10:01:36 +0700

    up

--- resources/views/layout/partials/admin/nav_admin.blade.php
+++ resources/views/layout/partials/admin/nav_admin.blade.php
@@ -108,24 +108,7 @@
                     </ul>
                 </div>
             </li>
-            {{-- <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#errorPages" role="button"
-                    aria-expanded="false" aria-controls="errorPages">
-                    <i class="link-icon" data-feather="cloud-off"></i>
-                    <span class="link-title">Error</span>
-                    <i class="link-arrow" data-feather="chevron-down"></i>
-                </a>
-                <div class="collapse" id="errorPages">
-                    <ul class="nav sub-menu">
-                        <li class="nav-item">
-                            <a href="pages/error/404.html" class="nav-link">404</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/error/500.html" class="nav-link">500</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
+            {{-- 
             <li class="nav-item">
                 <a class="nav-link" data-bs-toggle="collapse" href="#forms" role="button" aria-expanded="false"
                     aria-controls="forms">
@@ -240,15 +223,26 @@
                         <li class="nav-item">
                             <a href="pages/charts/peity.html" class="nav-link">Faq</a>
                         </li>
-
                     </ul>
                 </div>
             </li>
             <li class="nav-item">
-                <a href="{{ route('kelola_event') }}" class="nav-link">
-                    <i class="link-icon" data-feather="hash"></i>
-                    <span class="link-title">Event</span>
+                <a class="nav-link" data-bs-toggle="collapse" href="#errorPages" role="button"
+                    aria-expanded="false" aria-controls="errorPages">
+                    <i class="link-icon" data-feather="cloud-off"></i>
+                    <span class="link-title">Event & Artikel</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
                 </a>
+                <div class="collapse" id="errorPages">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="{{ route('kelola_event') }}" class="nav-link">Event</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/error/500.html" class="nav-link">Artikel</a>
+                        </li>
+                    </ul>
+                </div>
             </li>
         </ul>
     </div>
