1a8f1bd79b9fdc86df9d99a89d3c0b8147e8e391
Bayu <bayurz1001@gmail.com>
2024-07-29 12:02:21 +0700

    up

--- resources/views/admin/sertifikat/index.blade.php
+++ resources/views/admin/sertifikat/index.blade.php
@@ -59,7 +59,7 @@
                                                     <i data-feather="trash-2"></i>
                                                 </button>
 
-                                                <a href="{{ route('cetak_sertifikat', ['id' => $sertifikate->id]) }}"
+                                                <a href="{{ route('cetak_sertifikat', ['id' => $sertifikat->id]) }}"
                                                     class="btn btn-warning btn-icon" title="print" target="_blank">
                                                     <i data-feather="printer"></i>
                                                 </a>
