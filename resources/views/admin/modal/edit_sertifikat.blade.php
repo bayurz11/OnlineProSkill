a13d4804a26798191c25c57155660c7af297057a
Bayu <bayurz1001@gmail.com>
2024-07-29 11:08:04 +0700

    up

---
+++ resources/views/admin/modal/edit_sertifikat.blade.php
@@ -0,0 +1,65 @@
+<div class="modal fade" id="editSertifikatModal" tabindex="-1" aria-labelledby="editSertifikatModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <form action="{{ route('sertifikat.update', $sertifikat->id) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editSertifikatModalLabel">Edit Sertifikat</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="btn-close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nama<span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" id="name" name="name"
+                            value="{{ $sertifikat->name }}" placeholder="Masukkan Nama ">
+                    </div>
+                    <div class="mb-3">
+                        <label for="sertifikat_id" class="form-label">ID Sertifikat<span
+                                class="text-danger">*</span></label>
+                        <input type="text" class="form-control" id="sertifikat_id" name="sertifikat_id"
+                            value="{{ $sertifikat->sertifikat_id }}" placeholder="Masukkan ID Sertifikat">
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label" for="gambar">Sertifikat<span class="text-danger">*</span></label>
+                        <input type="file" accept="image/*" class="form-control" id="gambar" name="gambar">
+                    </div>
+                    <img id="preview" src="{{ asset('uploads/' . $sertifikat->gambar) }}" alt="Preview Sertifikat"
+                        style="max-width: 100%; max-height: 200px; display: {{ $sertifikat->gambar ? 'block' : 'none' }};">
+                    <div class="mb-3">
+                        <label for="keterangan" class="form-label">Keterangan<span class="text-danger">*</span></label>
+                        <input type="text" class="form-control" id="keterangan" name="keterangan"
+                            value="{{ $sertifikat->keterangan }}" placeholder="Masukkan Keterangan">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Keluar</button>
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $("#gambar").change(function() {
+            readURL(this);
+        });
+    });
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                $('#preview').attr('src', e.target.result).show();
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
