bd88acebda9e8a0ae6de0a113093dfd02135eaf7
Bayu <bayurz1001@gmail.com>
2024-08-03 10:25:19 +0700

    up

--- app/Http/Controllers/AksesPembelianController.php
+++ app/Http/Controllers/AksesPembelianController.php
@@ -316,13 +316,14 @@ class AksesPembelianController extends Controller
         })->values(); // Tambahkan values() untuk mereset kunci array
 
         $certificateId = sprintf("%03d", Order::where('user_id', $user->id)->count()) . " / PSA / " . strtoupper($completedCourses->first()->KelasTatapMuka->nama_kursus) . " / " . now()->format('m.Y');
-
+        $coursename = strtoupper($completedCourses->first()->KelasTatapMuka->nama_kursus);
         $pdf = $this->pdf->loadView('home.sertifikat.index', [
             'user' => $user,
             'profile' => $profile,
             'completedCourses' => $completedCourses,
             'date' => now()->format('d F Y'),
             'certificateId' => $certificateId,
+            'coursename' => $coursename,
         ])->setPaper('a4', 'landscape');
 
         return $pdf->stream('sertifikat_penyelesaian.pdf');
