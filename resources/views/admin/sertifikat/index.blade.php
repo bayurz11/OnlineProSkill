6aa8e29a91053f28e9ad641a8aa87fb2e77001a6
Bayu <bayurz1001@gmail.com>
2024-08-06 10:07:54 +0700

    up

--- resources/views/admin/sertifikat/index.blade.php
+++ resources/views/admin/sertifikat/index.blade.php
@@ -46,7 +46,7 @@
                                             <td>{{ $sertifikat->sertifikat_id }}</td>
                                             <td>{{ $sertifikat->name }}</td>
                                             <td>{{ $sertifikat->keterangan }}</td>
-                                            <td>{{ $sertifikat->kategori->name_category }}</td>
+                                            <td>{{ $sertifikat->kategori }}</td>
                                             <td><img src="{{ asset('public/uploads/' . $sertifikat->gambar) }}"
                                                     alt="Banner" class="wd-100 wd-sm-150 me-3"></td>
                                             <td>
