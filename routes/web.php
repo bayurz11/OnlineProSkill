a33849d3245720291b63f696468efe58da7435dd
Bayu <bayurz1001@gmail.com>
2024-08-06 14:20:23 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -150,8 +150,9 @@ Route::middleware('isStuden')->group(function () {
     //lesson
     Route::get('/lesson/{id}', [AksesPembelianController::class, 'lesson'])->name('lesson');
     Route::put('/sectionupdatestatus/{id}', [AksesPembelianController::class, 'updatestatus'])->name('sectionstatus');
-    Route::post('/print-certificate', [AksesPembelianController::class, 'printCertificate'])->name('print_certificate');
-    Route::get('/certificate/preview', [AksesPembelianController::class, 'previewCertificate'])->name('certificate.preview');
+    Route::get('/print-certificate/{course_id}', [AksesPembelianController::class, 'printCertificate'])->name('printCertificate');
+    Route::get('/preview-certificate/{course_id}', [AksesPembelianController::class, 'previewCertificate'])->name('previewCertificate');
+
 
 
     //sertifikat
