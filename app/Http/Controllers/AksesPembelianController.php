0ffe53776ce92b5f23c57a519bcf369374779678
Bayu <bayurz1001@gmail.com>
2024-08-06 14:37:27 +0700

    up

--- app/Http/Controllers/AksesPembelianController.php
+++ app/Http/Controllers/AksesPembelianController.php
@@ -295,7 +295,7 @@ class AksesPembelianController extends Controller
             ])->setPaper('a4', 'landscape');
 
 
-            return $pdf->merge('download', 'sertifikat_penyelesaian.pdf');
+            return $pdf->merge('sertifikat_penyelesaian.pdf');
         }
     }
 
