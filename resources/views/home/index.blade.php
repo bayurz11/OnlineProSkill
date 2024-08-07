acdb156bb17f365b2debcfac3f4abe10d08e02d4
Bayu <bayurz1001@gmail.com>
2024-08-07 16:29:24 +0700

    up

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -374,16 +374,18 @@
                                     <div class="col-lg-4 col-md-6">
                                         <div class="event__item shine__animate-item">
                                             <div class="event__item-thumb">
-                                                <a href="events-details.html" class="shine__animate-link"><img
-                                                        src="public/assets/img/events/event_thumb01.jpg"
+                                                <a href="{{ route('event_detail', ['id' => $event->id]) }}"
+                                                    class="shine__animate-link"><img
+                                                        src="{{ asset('public/uploads/events/' . $event->gambar) }}"
                                                         alt="img"></a>
                                             </div>
                                             <div class="event__item-content">
                                                 <span class="date">25 June, 2024</span>
-                                                <h2 class="title"><a href="events-details.html">The Accessible
-                                                        Target Sizes Cheatsheet</a></h2>
+                                                <h2 class="title"><a
+                                                        href="{{ route('event_detail', ['id' => $event->id]) }}">{{ $event->name }}</a>
+                                                </h2>
                                                 <a href="https://maps.google.com/maps" class="location"
-                                                    target="_blank"><i class="flaticon-map"></i>United Kingdom</a>
+                                                    target="_blank"><i class="flaticon-map"></i>{{ $event->lokasi }}</a>
                                             </div>
                                         </div>
                                     </div>
