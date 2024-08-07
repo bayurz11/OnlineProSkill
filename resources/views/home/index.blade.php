7fe87e77a78c3417b7914554a81120ff18ed31ec
Bayu <bayurz1001@gmail.com>
2024-08-07 17:20:07 +0700

    up

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -72,7 +72,7 @@
         <!-- banner-area-end -->
 
         <!-- brand-area -->
-        <div class="brand-area brand-area-two" style="color: wheat">
+        <div class="brand-area brand-area-two" style="background-color: wheat">
             <div class="container-fluid">
                 <div class="marquee_mode">
                     <div class="brand__item">
