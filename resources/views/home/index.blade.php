7fcae3d5d35042fbdd20860fd33e6e7ef30eeeea
Bayu <bayurz1001@gmail.com>
2024-08-07 16:19:23 +0700

    up

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -221,51 +221,52 @@
                     <div class="swiper courses-swiper-active">
                         <div class="swiper-wrapper">
                             @foreach ($KelasTatapMuka as $kelas)
-                                <div class="swiper-slide">
+                                <div class="swiper-slide h-100">
                                     <div
                                         class="courses__item courses__item-two shine__animate-item d-flex flex-column h-100">
                                         <div class="courses__item-thumb courses__item-thumb-two">
                                             <a href="{{ route('classroomdetail', ['id' => $kelas->id]) }}"
                                                 class="shine__animate-link">
-                                                <img src="{{ asset('public/uploads/' . $kelas->gambar) }}" alt="img">
+                                                <img src="{{ asset('public/uploads/' . $kelas->gambar) }}" alt="img"
+                                                    class="img-fluid">
                                             </a>
                                         </div>
-                                        <div class="courses__item-content courses__item-content-two">
+                                        <div
+                                            class="courses__item-content courses__item-content-two d-flex flex-column flex-grow-1">
                                             <ul class="courses__item-meta list-wrap">
                                                 <li class="courses__item-tag">
-
                                                     @if ($kelas->course_type == 'online')
                                                         <span class="badge bg-primary">Online</span>
                                                     @else
                                                         <span class="badge bg-secondary">Offline</span>
                                                     @endif
                                                 </li>
-                                                <li class="price">Rp {{ number_format($kelas->price, 0, '.', '.') }}
-                                                </li>
+                                                <li class="price">Rp {{ number_format($kelas->price, 0, '.', '.') }}</li>
                                             </ul>
-                                            <h5 class="title"><a
+                                            <h5 class="title flex-grow-1">
+                                                <a
                                                     href="{{ route('classroomdetail', ['id' => $kelas->id]) }}">{{ $kelas->nama_kursus }}</a>
                                             </h5>
-                                            <div class="courses__item-content-bottom">
+                                            <div class="courses__item-content-bottom mt-auto">
                                                 <div class="author-two">
-                                                    <a href="instructor-details.html"><img
-                                                            src="public/assets/img/courses/course_author001.png"
-                                                            alt="img">{{ $kelas->user->name }}</a>
+                                                    <a href="instructor-details.html">
+                                                        <img src="public/assets/img/courses/course_author001.png"
+                                                            alt="img">{{ $kelas->user->name }}
+                                                    </a>
                                                 </div>
                                                 <div class="avg-rating">
                                                     <i class="fas fa-star"></i> (4.8 Reviews)
                                                 </div>
                                             </div>
                                         </div>
-
                                     </div>
                                 </div>
                             @endforeach
                         </div>
                     </div>
                 </div>
-
             </div>
+
             <div class="all-courses-btn mt-30">
                 <div class="tg-button-wrap justify-content-center">
                     <a href="{{ route('search') }}" class="btn arrow-btn">Lihat Semua Kursus <img
