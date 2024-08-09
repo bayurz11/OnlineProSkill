cd2fa39cc7768c8aa13e88fb89530b5901398dd7
Bayu <bayurz1001@gmail.com>
2024-08-09 10:35:57 +0700

    up

--- resources/views/home/blog/index.blade.php
+++ resources/views/home/blog/index.blade.php
@@ -58,7 +58,7 @@
                                         <div class="blog__post-meta">
                                             <ul class="list-wrap">
                                                 <li><i
-                                                        class="flaticon-calendar"></i>{{ Carbon::parse($blog->date)->format('d - M - Y') }}
+                                                        class="flaticon-calendar"></i>{{ Carbon::parse($blog->date)->format('d M , Y') }}
                                                 </li>
                                                 <li><i class="flaticon-user-1"></i>by <a
                                                         href="blog-details.html">{{ $blog->user->name }}</a>
