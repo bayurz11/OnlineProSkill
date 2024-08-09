142857d56fd75cfe64ed11294809dec6c937566e
Bayu <bayurz1001@gmail.com>
2024-08-09 15:59:22 +0700

    up

--- resources/views/home/blog/detail.blade.php
+++ resources/views/home/blog/detail.blade.php
@@ -26,14 +26,15 @@
             </div>
         </div>
         <div class="breadcrumb__shape-wrap">
-            <img src="public/assets/img/others/breadcrumb_shape01.svg" alt="img" class="alltuchtopdown" loading="lazy">
-            <img src="public/assets/img/others/breadcrumb_shape02.svg" alt="img" data-aos="fade-right"
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape01.svg') }}" alt="img" class="alltuchtopdown"
+                loading="lazy">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape02.svg') }}" alt="img" data-aos="fade-right"
                 data-aos-delay="300" loading="lazy">
-            <img src="public/assets/img/others/breadcrumb_shape03.svg" alt="img" data-aos="fade-up"
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape03.svg') }}" alt="img" data-aos="fade-up"
                 data-aos-delay="400" loading="lazy">
-            <img src="public/assets/img/others/breadcrumb_shape04.svg" alt="img" data-aos="fade-down-left"
-                data-aos-delay="400" loading="lazy">
-            <img src="public/assets/img/others/breadcrumb_shape05.svg" alt="img" data-aos="fade-left"
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape04.svg') }}" alt="img"
+                data-aos="fade-down-left" data-aos-delay="400" loading="lazy">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape05.svg') }}" alt="img" data-aos="fade-left"
                 data-aos-delay="400" loading="lazy">
         </div>
     </section>
