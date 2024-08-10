b86bd68751bdbf335181efb47c8c6dda7254800d
Bayu <bayurz1001@gmail.com>
2024-08-10 10:15:04 +0700

    up

--- resources/views/home/event/detail.blade.php
+++ resources/views/home/event/detail.blade.php
@@ -169,14 +169,14 @@
                                                     </li>
                                                     <!-- Twitter Share -->
                                                     <li>
-                                                        <a href="https://twitter.com/intent/tweet?url={{ urlencode(url()->current()) }}&text={{ urlencode($courseTitle) }}"
+                                                        <a href="https://twitter.com/intent/tweet?url={{ urlencode(url()->current()) }}&text={{ urlencode($event->name) }}"
                                                             target="_blank">
                                                             <i class="fab fa-twitter"></i>
                                                         </a>
                                                     </li>
                                                     <!-- WhatsApp Share -->
                                                     <li>
-                                                        <a href="https://api.whatsapp.com/send?text={{ urlencode($courseTitle) }}%20{{ urlencode(url()->current()) }}"
+                                                        <a href="https://api.whatsapp.com/send?text={{ urlencode($event->name) }}%20{{ urlencode(url()->current()) }}"
                                                             target="_blank">
                                                             <i class="fab fa-whatsapp"></i>
                                                         </a>
