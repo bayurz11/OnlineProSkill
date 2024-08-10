79d80cec4815ef6ea48d06ff3c569ffee40a3714
Bayu <bayurz1001@gmail.com>
2024-08-10 12:29:26 +0700

    up

--- resources/views/home/hubungikami/index.blade.php
+++ resources/views/home/hubungikami/index.blade.php
@@ -39,6 +39,87 @@
         </div>
     </section>
     <!-- breadcrumb-area-end -->
-
+    <!-- contact-area -->
+    <section class="contact-area section-py-120">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="contact-info-wrap">
+                        <ul class="list-wrap">
+                            <li>
+                                <div class="icon">
+                                    <img src="public/assets/img/icons/map.svg" alt="img" class="injectable">
+                                </div>
+                                <div class="content">
+                                    <h4 class="title">Address</h4>
+                                    <p>Awamileaug Drive, Kensington <br> London, UK</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <img src="public/assets/img/icons/contact_phone.svg" alt="img" class="injectable">
+                                </div>
+                                <div class="content">
+                                    <h4 class="title">Phone</h4>
+                                    <a href="tel:0123456789">+1 (800) 123 456 789</a>
+                                    <a href="tel:0123456789">+1 (800) 123 456 789</a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <img src="public/assets/img/icons/emial.svg" alt="img" class="injectable">
+                                </div>
+                                <div class="content">
+                                    <h4 class="title">E-mail Address</h4>
+                                    <a href="mailto:info@gmail.com">info@gmail.com</a>
+                                    <a href="mailto:info@gmail.com">info@gmail.com</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="contact-form-wrap">
+                        <h4 class="title">Send Us Message</h4>
+                        <p>Your email address will not be published. Required fields are marked *</p>
+                        <form id="contact-form" action="public/assets/mail.php" method="POST">
+                            <div class="form-grp">
+                                <textarea name="message" placeholder="Comment" required></textarea>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="form-grp">
+                                        <input name="name" type="text" placeholder="Name *" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-grp">
+                                        <input name="email" type="email" placeholder="E-mail *" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-grp">
+                                        <input name="website" type="url" placeholder="Website *" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-two arrow-btn">Submit Now <img
+                                    src="assets/img/icons/right_arrow.svg" alt="img" class="injectable"></button>
+                        </form>
+                        <p class="ajax-response mb-0"></p>
+                    </div>
+                </div>
+            </div>
+            <!-- contact-map -->
+            <div class="contact-map">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1923.7919637366656!2d104.46155006362552!3d0.9007406620588777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d9729a2556642d%3A0xaa63e717cb0e87b9!2sProSkill%20Akademia!5e1!3m2!1sid!2sid!4v17232635"
+                    style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <!-- contact-map-end -->
+        </div>
+    </section>
+    <!-- contact-area-end -->
 
 @endsection
