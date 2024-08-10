f065959dd4bdeda1c696a10179538f5a3b0c55a4
Bayu <bayurz1001@gmail.com>
2024-08-10 10:30:37 +0700

    up

--- resources/views/home/event/detail.blade.php
+++ resources/views/home/event/detail.blade.php
@@ -82,7 +82,8 @@
                                             dolore magna aliqua.</p>
                                     </div>
                                     <div class="event__map">
-                                        <h4 class="title">Map</h4>
+                                        <h4 class="title">Lokasi</h4>
+                                        {{ $event->link_maps }}
                                         <div class="map">
                                             <iframe
                                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48409.69813174607!2d-74.05163325136718!3d40.68264649999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25bae694479a3%3A0xb9949385da52e69e!2sBarclays%20Center!5e0!3m2!1sen!2sbd!4v1684309529719!5m2!1sen!2sbd"
