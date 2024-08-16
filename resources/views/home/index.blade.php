650cef74f4251a05f669e869e9917d91f041e328
Bayu <bayurz1001@gmail.com>
2024-08-16 08:42:22 +0700

    up

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -27,8 +27,9 @@
                                     </svg>
                                     tentang apa
                                 </span>
-                                yang Anda hasilkan. Jadikan itu milik Anda di ProSkill.
+                                yang Anda ciptakan. Jadikan itu bagian dari kesuksesan Anda bersama ProSkill.
                             </h3>
+
                             <div class="banner__btn-two" data-aos="fade-right" data-aos-delay="600">
                                 <a href="{{ route('search') }}" class="btn arrow-btn">Bergabung Sekarang<img
                                         src="{{ asset('public/assets/img/icons/right_arrow.svg') }}" alt="img"
