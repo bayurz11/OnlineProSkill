d6862c80c33f1f593ff51ed41bb200b41552bb88
Bayu <bayurz1001@gmail.com>
2024-08-26 17:40:58 +0700

    up

--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -277,9 +277,7 @@
                                         </div>
                                     @endforeach
                                 </div>
-                                <nav class="pagination__wrap mt-30">
-                                    {{ $results->links($paginationView) }}
-                                </nav>
+
                             </div>
 
                             <div class="tab-pane fade" id="list" role="tabpanel" aria-labelledby="list-tab">
@@ -338,12 +336,12 @@
                                             </div>
                                         </div>
                                     @endforeach
-                                    <nav class="pagination__wrap mt-30">
-                                        {{ $results->links($paginationView) }}
-                                    </nav>
+
                                 </div>
                             </div>
-
+                            <nav class="pagination__wrap mt-30">
+                                {{ $results->links($paginationView) }}
+                            </nav>
                         </div>
                 </div>
     </section>
