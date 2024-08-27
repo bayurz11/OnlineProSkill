e7e9927ded04e113f02930410c62c992668bd269
Bayu <bayurz1001@gmail.com>
2024-08-27 09:56:08 +0700

    up

--- resources/views/admin/sertifikat/cetak.blade.php
+++ resources/views/admin/sertifikat/cetak.blade.php
@@ -124,7 +124,7 @@
         <div class="content">
             <h1>SERTIFIKAT</h1>
             <div class="underline"></div>
-            <p>{{ $sertifikat->sertifikat_id }}</p>
+            <p>ID : {{ $sertifikat->sertifikat_id }}</p>
             {{-- @if ($profile && $profile->gambar)
                 @if (strpos($profile->gambar, 'googleusercontent') !== false)
                     <img class="photo" src="{{ $profile->gambar }}" alt="Foto Peserta">
