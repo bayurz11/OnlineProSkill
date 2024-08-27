2d23f8f4ab72ff6bb772b9357b144b3a5d6505d5
Bayu <bayurz1001@gmail.com>
2024-08-27 12:16:25 +0700

    up

--- resources/views/admin/sertifikat/cetak.blade.php
+++ resources/views/admin/sertifikat/cetak.blade.php
@@ -95,7 +95,7 @@
         .qr {
             position: absolute;
             top: 400px;
-            left: 30px;
+            left: 26px;
             width: 150px;
             height: 150px;
             object-fit: cover;
