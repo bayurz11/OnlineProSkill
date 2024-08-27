c937b00bf13102e633edde05d3b4e06bef9d1be6
Bayu <bayurz1001@gmail.com>
2024-08-27 14:39:44 +0700

    up

--- resources/views/home/sertifikat/cetak.blade.php
+++ resources/views/home/sertifikat/cetak.blade.php
@@ -6,11 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sertifikat</title>
     <style>
-        @page {
-            size: A4 landscape;
-            margin: 0;
-        }
-
         body,
         html {
             margin: 0;
