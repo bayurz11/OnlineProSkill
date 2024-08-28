c34bd5f51b78a132bdb083ef4c0e67fe51783070
Bayu <bayurz1001@gmail.com>
2024-08-28 12:52:16 +0700

    up

--- database/migrations/2024_08_01_033140_create_user_section_statuses_table.php
+++ database/migrations/2024_08_01_033140_create_user_section_statuses_table.php
@@ -11,10 +11,22 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('section', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('kurikulum_id');
+            $table->string('title');
+            $table->string('no_urut');
+            $table->string('link');
+            $table->string('duration');
+            $table->string('file_path');
+            $table->string('status');
+            $table->foreign('kurikulum_id')->references('id')->on('section')->onDelete('cascade');
+            $table->timestamps();
+        });
         Schema::create('user_section_statuses', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('section_id')->constrained()->onDelete('cascade');
+            $table->foreignId('section_id')->constrained('section')->onDelete('cascade'); // Pastikan nama tabel 'sections' disertakan
             $table->boolean('status')->default(false);
             $table->timestamps();
         });
@@ -25,6 +37,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('section');
         Schema::dropIfExists('user_section_statuses');
     }
 };
