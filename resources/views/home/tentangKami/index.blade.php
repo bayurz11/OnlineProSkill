f9cdce368d3555e547b20418c59973b8bd01fd7a
Bayu <bayurz1001@gmail.com>
2024-09-17 16:28:55 +0700

    up

--- resources/views/home/tentangKami/index.blade.php
+++ resources/views/home/tentangKami/index.blade.php
@@ -56,7 +56,7 @@
                 <div class="col-xl-3 col-lg-4 col-md-6">
                     <div class="features__item">
                         <div class="features__icon">
-                            <img src="public/assets/img/icons/features_icon01.svg" class="injectable" alt="img">
+                            <img src="public/assets/img/icons/features_icon03.svg" class="injectable" alt="img">
                         </div>
                         <div class="features__content">
                             <h4 class="title">Learn with Experts</h4>
@@ -67,7 +67,7 @@
                 <div class="col-xl-3 col-lg-4 col-md-6">
                     <div class="features__item">
                         <div class="features__icon">
-                            <img src="public/assets/img/icons/features_icon02.svg" class="injectable" alt="img">
+                            <img src="public/assets/img/icons/features_icon03.svg" class="injectable" alt="img">
                         </div>
                         <div class="features__content">
                             <h4 class="title">Learn Anything</h4>
