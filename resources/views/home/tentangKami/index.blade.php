28a68c4e2d31849ba8bac3dfa50138818082a6b8
Bayu <bayurz1001@gmail.com>
2024-09-17 16:35:02 +0700

    up

--- resources/views/home/tentangKami/index.blade.php
+++ resources/views/home/tentangKami/index.blade.php
@@ -45,18 +45,12 @@
             <div class="row justify-content-center">
                 <div class="col-xl-6">
                     <div class="section__title white-title text-center mb-50">
-
-                        <h2 class="title">Tentang Kami</h2>
-                        <p>Proskill Akademia adalah lembaga kursus yang dikelola oleh:
-
-                            PT Bahagia Sukses Digimedia
-
-                            Nomor Induk Berusaha PT. Bahagia Sukses Digimedia: 1910220049534
-
-                            SK Depkumham: AHU-043447 .AH.01.30.Tahun 2022
-
-                            Jl.DI Panjaitan Km 7 Komplek Taman Mekar Sari 2 Blok C No.22, Melayu Kota Piring, Tanjung Pinang
-                            Timur, Tanjung Pinang, Kepulauan Riau, 29123</p>
+                        <span class="sub-title">Tentang Kami</span>
+                        <h2 class="title">Proskill Akademia adalah lembaga kursus yang dikelola oleh</h2>
+                        <p>PT Bahagia Sukses Digimedia <br> Nomor Induk Berusaha PT. Bahagia Sukses Digimedia: 1910220049534
+                            <br>SK Depkumham: AHU-043447 .AH.01.30.Tahun 2022<br>Jl.DI Panjaitan Km 7 Komplek Taman Mekar
+                            Sari 2 Blok C No.22, Melayu Kota Piring, Tanjung Pinang Timur, Tanjung Pinang, Kepulauan Riau,
+                            29123</p>
                     </div>
                 </div>
             </div>
