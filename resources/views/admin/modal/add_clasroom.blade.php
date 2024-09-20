2b9cba9569fe0aeca6a51543b594cc8f02d01b29
Bayu <bayurz1001@gmail.com>
2024-09-20 09:05:32 +0700

    up

--- resources/views/admin/modal/add_clasroom.blade.php
+++ resources/views/admin/modal/add_clasroom.blade.php
@@ -99,12 +99,22 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="include" class="form-label">yang akan di pelajari <span
+                        <label for="include" class="form-label">Apa yang akan di pelajari <span
                                 class="text-danger">*</span></label>
                         <div id="include-container">
                             <div class="input-group mb-2">
                                 <input type="text" class="form-control" id="include" name="include[]">
                                 <button class="btn btn-success" type="button" id="add-include">+</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="perstaratan" class="form-label">Persyaratan Kelas <span
+                                class="text-danger">*</span></label>
+                        <div id="perstaratan-container">
+                            <div class="input-group mb-2">
+                                <input type="text" class="form-control" id="perstaratan" name="perstaratan[]">
+                                <button class="btn btn-success" type="button" id="add-perstaratan">+</button>
                             </div>
                         </div>
                     </div>
@@ -201,6 +211,30 @@
 
         includeContainer.addEventListener('click', function(event) {
             if (event.target && event.target.classList.contains('remove-include')) {
+                event.target.closest('.input-group').remove();
+            }
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        const addperstaratanButton = document.getElementById('add-perstaratan');
+        const perstaratanContainer = document.getElementById('perstaratan-container');
+
+        addperstaratanButton.addEventListener('click', function() {
+            const newInputGroup = document.createElement('div');
+            newInputGroup.classList.add('input-group', 'mb-2');
+            newInputGroup.innerHTML = `
+                <input type="text" class="form-control" name="perstaratan[]" >
+                <button class="btn btn-danger remove-perstaratan" type="button">-</button>
+            `;
+            perstaratanContainer.appendChild(newInputGroup);
+
+            newInputGroup.querySelector('.remove-perstaratan').addEventListener('click', function() {
+                newInputGroup.remove();
+            });
+        });
+
+        perstaratanContainer.addEventListener('click', function(event) {
+            if (event.target && event.target.classList.contains('remove-perstaratan')) {
                 event.target.closest('.input-group').remove();
             }
         });
