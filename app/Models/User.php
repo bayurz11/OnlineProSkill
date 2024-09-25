b407c8ac30b9b434c773bd9d7756ed2971a5deb9
Bayu <bayurz1001@gmail.com>
2024-09-25 14:19:48 +0700

    up

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use App\Models\UserSectionStatus;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
