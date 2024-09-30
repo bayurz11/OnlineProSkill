784e97da859ade5ecac1d5ced733602e0566ff07
Bayu <bayurz1001@gmail.com>
2024-09-30 20:54:22 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Proyek Laravel dengan React dan Framer Motion</title>
+    <title>Bootcamp Power BI</title>
 </head>
 
 <body>
