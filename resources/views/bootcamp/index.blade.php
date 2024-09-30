874fae17cb7b224e982c5f153efeb0b226697cd8
Bayu <bayurz1001@gmail.com>
2024-09-30 20:43:08 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet"> <!-- Tailwind CSS -->
+    <link href="{{ assets('css/app.css') }}" rel="stylesheet"> <!-- Tailwind CSS -->
 </head>
 
 <body>
