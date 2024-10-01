9b5c8b711db387c8c18cae1d32ec8c7fe18d4450
Bayu <bayurz1001@gmail.com>
2024-10-01 18:31:15 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -52,18 +52,18 @@
                         <img src="public/assets/img/banner/banner_shape02.png" alt="shape"
                             class="shape small-shape tg-motion-effects3">
 
-                        <!-- Overlay div with opacity/gradien -->
-                        <div
-                            style="
-                            position: absolute; 
-                            bottom: 0; 
-                            left: 0; 
-                            right: 0; 
-                            height: 200px; 
-                            background: linear-gradient(to bottom, transparent, white); /* Sesuaikan dengan warna background */
-                            z-index: 1;">
-                        </div>
                     </div>
+                </div>
+                <!-- Overlay div with opacity/gradien -->
+                <div
+                    style="
+                        position: absolute; 
+                        bottom: 0; 
+                        left: 0; 
+                        right: 0; 
+                        height: 200px; 
+                        background: linear-gradient(to bottom, transparent, white); /* Sesuaikan dengan warna background */
+                        z-index: 1;">
                 </div>
 
 
