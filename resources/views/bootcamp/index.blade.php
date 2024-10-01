ed7b37abcede5966966f79607e9bd9313fec2b28
Bayu <bayurz1001@gmail.com>
2024-10-01 14:22:24 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -22,12 +22,14 @@
                                         d="M4.74438 7.70565C69.7006 -1.18799 136.097 -2.38304 203.934 4.1205C207.178 4.48495 209.422 7.14626 208.933 10.0534C206.793 23.6481 205.415 36.5704 204.801 48.8204C204.756 51.3291 202.246 53.5582 199.213 53.7955C136.093 59.7623 74.1922 60.5985 13.5091 56.3043C10.5653 56.0924 7.84371 53.7277 7.42158 51.0325C5.20725 38.2627 2.76333 25.6511 0.0898448 13.1978C-0.465589 10.5873 1.61173 8.1379 4.73327 7.70565"
                                         fill="currentcolor" />
                                 </svg>
-                                Power BI
+                                Power BI.
                             </span>
                             <br>Life <b>Never Stop</b> Teaching
                         </h3>
-                        <p data-aos="fade-right" data-aos-delay="600">Every teaching and learning journey is unique
-                            Following We'll help guide your way.</p>
+                        <p data-aos="fade-right" data-aos-delay="600">📊 Jadilah ahli dalam visualisasi data dan ambil
+                            langkah pertama menuju karier yang lebih cerah! Daftar sekarang , dan wujudkan impianmu menjadi
+                            data-driven professional!
+                        </p>
                         <div class="banner__btn-wrap" data-aos="fade-right" data-aos-delay="800">
                             <a href="contact.html" class="btn arrow-btn">Daftar Sekarang.. <img
                                     src="public/assets/img/icons/right_arrow.svg" alt="img" class="injectable"></a>
