f697d7d9c574223a3bf6ac8138161cc3c2c85971
Bayu <bayurz1001@gmail.com>
2024-10-02 09:47:24 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -44,19 +44,22 @@
                     </div>
                     <div class="banner__content"
                         style="padding: 0; margin-top: 40px; display: flex; justify-content: space-between; align-items: center;">
-                        <div style="flex: 1; color: white; padding: 8px;">
+                        <div style="flex: 1; color: white; padding: 8px; text-align: center;">
                             <p style="margin-bottom: 10px;">Date</p>
                             <p style="font-weight: bold;">Sept 21</p>
                         </div>
-                        <div style="flex: 1; color: white; padding: 8px;">
+                        <div style="border-left: 1px solid white; height: 50px; margin: 0 20px;"></div>
+                        <div style="flex: 1; color: white; padding: 8px; text-align: center;">
                             <p style="margin-bottom: 10px;">Duration</p>
                             <p style="font-weight: bold;">5 Weeks</p>
                         </div>
-                        <div style="flex: 1; color: white; padding: 8px;">
+                        <div style="border-left: 1px solid white; height: 50px; margin: 0 20px;"></div>
+                        <div style="flex: 1; color: white; padding: 8px; text-align: center;">
                             <p style="margin-bottom: 10px;">Time</p>
                             <p style="font-weight: bold;">4 hrs/week</p>
                         </div>
                     </div>
+
 
                 </div>
 
