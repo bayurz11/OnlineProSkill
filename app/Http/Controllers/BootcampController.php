16958740451293284f8b283a9ffe7a8836b58de7
Bayu <bayurz1001@gmail.com>
2024-10-02 13:14:06 +0700

    up

--- app/Http/Controllers/BootcampController.php
+++ app/Http/Controllers/BootcampController.php
@@ -109,6 +109,6 @@ class BootcampController extends Controller
 
         Session::put('cart', $cart);
 
-        return redirect()->route('bootcamp_cart.view');
+        return redirect()->route('cart_bootcamp.view');
     }
 }
