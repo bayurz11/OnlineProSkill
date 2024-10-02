b23bc291391a10032e595ed9603289065201a978
Bayu <bayurz1001@gmail.com>
2024-10-02 18:38:42 +0700

    up

--- resources/views/home/bootcamp_cart.blade.php
+++ resources/views/home/bootcamp_cart.blade.php
@@ -8,135 +8,127 @@
         <div class="container">
             <div class="row">
 
-
-                @if (count($cart) > 0)
+                @auth
                     <div class="col-lg-7">
-                        <table class="table cart__table">
-                            <thead>
-                                <tr>
-                                    <th class="product__thumb">&nbsp;</th>
-                                    <th class="product__name">Kelas</th>
-                                    <th class="product__price">Harga</th>
-                                    <th class="product__remove">&nbsp;</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                        <form action="{{ route('payment') }}" class="customer__form-wrap" method="POST">
+                            @csrf
+                            <span class="title">RINCIAN PENAGIHAN</span>
+
+                            @foreach ($cart as $item)
+                                <input type="hidden" name="cart_items[]" value="{{ $item['id'] }}">
+                            @endforeach
+
+                            <div class="form-grp">
+                                <label for="name">Nama *</label>
+                                <input type="text" id="name" name="name" value="{{ $user->name }}">
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-grp">
+                                        <label for="phone">Telepon *</label>
+                                        <input type="number" id="phone" name="phone" min="0" required
+                                            value="{{ $profile->phone_number }}" maxlength="12" placeholder="08**********">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-grp">
+                                        <label for="email">Alamat Email *</label>
+                                        <input type="email" id="email" name="email" value="{{ $user->email }}">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button type="submit" class="btn">Bayar & gabung kelas</button>
+                        </form>
+                    </div>
+
+                @endauth
+                @guest
+                    <div class="col-12">
+                        <div class="coupon__code-wrap">
+                            <div class="coupon__code-info">
+                                <span>Sudah memiliki akun?</span>
+                                <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModalchart">Klik di sini untuk
+                                    Masuk</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="singUp-wrap">
+                            <h2 class="title">Buat Akun ProSkill</h2>
+                            <p>Silahkan isi form berikut untuk melanjutkan.</p>
+
+                            <form action="{{ route('guestregister') }}" class="account__form" method="POST" id="guestregister">
+                                @csrf
+                                <div class="form-grp">
+                                    <input type="text" id="name" name="name"
+                                        placeholder="Masukkan Nama Lengkap Anda">
+                                </div>
+                                <div class="form-grp">
+                                    <input type="email" id="email" placeholder="Email" name="email">
+                                </div>
+                                <div class="form-grp">
+                                    <input type="phone" id="phone_number" placeholder="08**********" name="phone_number"
+                                        maxlength="12">
+                                </div>
+                                <div class="form-grp">
+                                    <input type="password" id="password" placeholder="Password" name="password">
+                                </div>
+                                <div class="form-grp">
+                                    <input type="password" id="password_confirmation" placeholder="Konfirmasi Password"
+                                        name="password_confirmation">
+                                </div>
+                                <button class="g-recaptcha btn btn-two arrow-btn"
+                                    data-sitekey="{{ config('services.recaptcha_v3.siteKey') }}"
+                                    data-callback="onSubmitguestregister" data-action='submit'>
+                                    Daftar
+                                    <img src="{{ asset('public/assets/img/icons/right_arrow.svg') }}" alt="img"
+                                        class="injectable">
+                                </button>
+                            </form><br>
+                            <div class="account__social">
+                                <a href="{{ route('oauth.google') }}" class="account__social-btn">
+                                    <img src="{{ asset('public/assets/img/icons/google.svg') }}" alt="img">
+                                    Daftar Dengan Google
+                                </a>
+                            </div>
+
+                        </div>
+                    </div>
+                @endguest
+
+                <div class="col-lg-5">
+                    <div class="cart__collaterals-wrap">
+                        <h2 class="title">Total keranjang</h2>
+                        @if (count($cart) > 0)
+                            <ul class="list-wrap">
                                 @foreach ($cart as $item)
-                                    <tr>
-                                        <td class="product__thumb">
-                                            <a href="{{ route('classroomdetail', $item['id']) }}">
-                                                <img src="{{ $item['gambar'] ? asset('public/uploads/' . $item['gambar']) : asset('public/assets/img/shop/shop_img01.jpg') }}"
-                                                    alt="">
-                                            </a>
-                                        </td>
+                                    <li>
                                         <td class="product__name">
                                             <a href="{{ route('classroomdetail', $item['id']) }}">{{ $item['name'] }}</a>
-                                        </td>
-                                        <td class="product__price">Rp {{ number_format($item['price'], 0, ',', ',') }}</td>
-
-                                        <td class="product__remove">
-                                            <form action="{{ route('cart_bootcamp.remove', $item['id']) }}" method="POST"
+                                            <form action="{{ route('cart.remove', $item['id']) }}" method="POST"
                                                 style="display:inline;">
                                                 @csrf
                                                 <button type="submit" class="btn-remove"
-                                                    style="background:none; border:none; color:rgb(20, 20, 20); cursor:pointer;">x</button>
+                                                    style="background:none; border:none; color:red; cursor:pointer;">x</button>
                                             </form>
                                         </td>
-                                    </tr>
+                                    </li>
                                 @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                @else
-                    <div class="col-lg-7">
-                        <p>Keranjang Anda kosong. <a href="{{ route('search') }}">Lihat kelas yang
-                                tersedia.</a></p>
-                    </div>
-                @endif
-
-                @if (count($cart) > 0)
-                    <div class="col-lg-5">
-                        <div class="cart__collaterals-wrap">
-                            <h2 class="title">Total keranjang</h2>
-
-                            @php
-                                // Cek apakah user sudah pernah melakukan order
-                                $biayaPendaftaran = 20000; // Biaya pendaftaran default
-                                if (Auth::check()) {
-                                    // Cari apakah user sudah pernah melakukan order
-                                    $userOrders = \App\Models\Order::where('user_id', Auth::id())
-                                        ->where('status', '!=', 'canceled')
-                                        ->exists();
-
-                                    // Jika user sudah pernah order, set biaya pendaftaran menjadi 0
-                                    if ($userOrders) {
-                                        $biayaPendaftaran = 0;
-                                    }
-                                }
-
-                                $totalPrice = array_sum(array_column($cart, 'price')); // Total harga keranjang
-                                $totalPriceWithPendaftaran = $totalPrice + $biayaPendaftaran; // Total dengan biaya pendaftaran
-                            @endphp
-
-                            <ul class="list-wrap">
                                 <li>Jumlah Quantity <span>{{ array_sum(array_column($cart, 'quantity')) }}</span></li>
-                                <li>Total Produk <span class="amount">Rp
-                                        {{ number_format($totalPrice, 0, ',', ',') }}</span></li>
-                                <li>Biaya Pendaftaran <span class="amount">Rp
-                                        {{ number_format($biayaPendaftaran, 0, ',', ',') }}</span></li>
-                                <li>Total <span class="amount">Rp
-                                        {{ number_format($totalPriceWithPendaftaran, 0, ',', ',') }}</span></li>
+                                <li>Subtotal <span>Rp.{{ array_sum(array_column($cart, 'price')) }}</span></li>
+                                <li>Total <span class="amount">Rp.{{ array_sum(array_column($cart, 'price')) }}</span>
+                                </li>
                             </ul>
-
-                            @auth
-                                <form action="{{ route('payment') }}" class="customer__form-wrap" method="POST">
-                                    @csrf
-                                    @foreach ($cart as $item)
-                                        <input type="hidden" name="cart_items[]" value="{{ $item['id'] }}">
-                                    @endforeach
-
-                                    <input type="hidden" name="biaya_pendaftaran" value="{{ $biayaPendaftaran }}">
-
-                                    <div class="form-grp" hidden>
-                                        <label for="name">Nama *</label>
-                                        <input type="text" id="name" name="name" value="{{ $user->name }}">
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <div class="form-grp" hidden>
-                                                <label for="phone">Telepon *</label>
-                                                <input type="number" id="phone" name="phone" min="0" required
-                                                    value="{{ $profile->phone_number }}" maxlength="12"
-                                                    placeholder="08**********">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="form-grp" hidden>
-                                                <label for="email">Alamat Email *</label>
-                                                <input type="email" id="email" name="email"
-                                                    value="{{ $user->email }}">
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <button type="submit" class="btn">Bayar</button>
-                                </form>
-                            @else
-                                <button type="button" class="btn" data-bs-toggle="modal"
-                                    data-bs-target="#registerbootcampcart">
-                                    Bayar
-                                </button>
-                            @endauth
-                        </div>
+                        @else
+                            <p>Keranjang Anda kosong. <a href="{{ route('search') }}">Lihat kelas yang tersedia.</a>
+                            </p>
+                        @endif
                     </div>
-                @else
-                    <p></p>
-                @endif
-
+                </div>
 
             </div>
-
         </div>
     </div>
 @endsection
