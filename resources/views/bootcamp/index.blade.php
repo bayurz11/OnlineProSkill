cb5b1b21da1c3a657bbfecad4a5daf968641156f
Bayu <bayurz1001@gmail.com>
2024-10-02 09:52:37 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -42,7 +42,7 @@
                             </a>
                         </div>
                     </div>
-                    <div class="banner__content" data-aos-delay="800"
+                    <div class="banner__content" data-aos-delay="800" data-aos="fade-top"
                         style="padding: 0; margin-top: 40px; display: flex; justify-content: space-between; align-items: center;">
                         <div style="flex: 1; color: white; padding: 8px; ">
                             <p style="margin-bottom: 10px;">Date</p>
