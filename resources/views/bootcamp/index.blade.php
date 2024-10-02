e7f2e628f6b287a517ebf0111ab52c9d7bd581ea
Bayu <bayurz1001@gmail.com>
2024-10-02 09:48:08 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -48,7 +48,7 @@
                             <p style="margin-bottom: 10px;">Date</p>
                             <p style="font-weight: bold;">Sept 21</p>
                         </div>
-                        <div style="border-left: 1px solid white; height: 50px; margin: 0 20px;"></div>
+                        <div style="border-left: 1px solid #007368; height: 50px; margin: 0 20px;"></div>
                         <div style="flex: 1; color: white; padding: 8px; text-align: center;">
                             <p style="margin-bottom: 10px;">Duration</p>
                             <p style="font-weight: bold;">5 Weeks</p>
