1e8768c220aa8281736d46346855f661d12712ef
Bayu <bayurz1001@gmail.com>
2024-10-04 14:18:13 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -43,8 +43,7 @@
                                 </svg>
                                 Power BI
                             </span>
-                            <br>
-                            <h3 style="color: #007368">Untuk <b>Data Analyst</b> Pemula </h3>
+                            <br> <b style="color: #007368">Untuk <b>Data Analyst</b> Pemula </b>
                         </h3> <br>
                         <p data-aos="fade-right" data-aos-delay="600" style="font-size: 1rem; margin-bottom: 15px;">
                             📊 Jadilah ahli dalam <b>visualisasi data</b> dan ambil langkah pertama menuju karier yang lebih
