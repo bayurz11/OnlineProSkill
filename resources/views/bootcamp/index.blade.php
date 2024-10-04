f53d67efeef2e96502b616eebf12fee80c8dd819
Bayu <bayurz1001@gmail.com>
2024-10-04 15:41:33 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -258,7 +258,7 @@
                         <div class="section__title">
                             <span class="sub-title" data-aos="fade-top" data-aos-delay="600">Detail</span>
                             <h2 class="title" data-aos="fade-top" data-aos-delay="600">
-                                Apa Yang Akan
+                                Target dan Sasaran Bootcamp
                                 <span class="position-relative">
                                     <svg x="0px" y="0px" preserveAspectRatio="none" viewBox="0 0 209 59" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
@@ -266,9 +266,9 @@
                                             d="M4.74438 7.70565C69.7006 -1.18799 136.097 -2.38304 203.934 4.1205C207.178 4.48495 209.422 7.14626 208.933 10.0534C206.793 23.6481 205.415 36.5704 204.801 48.8204C204.756 51.3291 202.246 53.5582 199.213 53.7955C136.093 59.7623 74.1922 60.5985 13.5091 56.3043C10.5653 56.0924 7.84371 53.7277 7.42158 51.0325C5.20725 38.2627 2.76333 25.6511 0.0898448 13.1978C-0.465589 10.5873 1.61173 8.1379 4.73327 7.70565"
                                             fill="currentcolor" />
                                     </svg>
-                                    Anda
+                                    Bootcamp
                                 </span>
-                                Pelajari
+
                             </h2>
                         </div>
 
