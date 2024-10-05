b0d3c04c5fc42483edb42fd20cc6ecb8642e496b
Bayu <bayurz1001@gmail.com>
2024-10-05 09:25:31 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -235,9 +235,9 @@
         <div class="container">
             <div class="row align-items-center justify-content-center">
                 <div class="col-lg-6 col-md-9" data-aos="fade-right" data-aos-delay="600">
-                    <div class="about__images">
+                    {{-- <div class="about__images">
                         <img src="public/assets/img/banner/instruktur.png" alt="img" class="main-img">
-                    </div>
+                    </div> --}}
                 </div>
                 <div class="col-lg-6">
                     <div class="about__content">
