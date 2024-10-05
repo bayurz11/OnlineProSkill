f4aaa6b06d8d842642b461ca6d45e43395245304
Bayu <bayurz1001@gmail.com>
2024-10-05 09:56:46 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -236,7 +236,7 @@
             <div class="row align-items-center justify-content-center">
                 <div class="col-lg-6 col-md-9" data-aos="fade-right" data-aos-delay="600">
                     <div class="container mt-5 d-flex justify-content-center align-items-center" style="height: 60vh;">
-                        <div>
+                        <div class="align-items-center">
                             <!-- Gambar bagian atas card -->
                             <img src="public/assets/img/banner/instruktur2.png" class="card-img-top" alt="User Photo"
                                 style="height: 22rem; width:18rem; object-fit: cover;">
