add27f9bb58408763b8377e97a6a7b45cb471a96
Bayu <bayurz1001@gmail.com>
2024-10-06 11:45:30 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -195,7 +195,8 @@
                             </div>
                             <div class="features__content">
                                 <h4 class="title" style="color: #007368">Group Alumni</h4>
-                                <p>Disediakan Group Alumni untuk Peserta</p>
+                                <p>Bangun jaringan, berbagi pengalaman, temukan peluang baru, dan Jadikan komunitas ini
+                                    sebagai wadah inspirasi dan kolaborasi.</p>
                             </div>
                         </div>
                     </div>
