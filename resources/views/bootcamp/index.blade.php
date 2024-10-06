77afa8d29876e9aa131a1d0c96d785d0d8656407
Bayu <bayurz1001@gmail.com>
2024-10-06 11:36:34 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -355,7 +355,7 @@
                                 <div class="accordion-item">
                                     <h2 class="accordion-header">
                                         <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                                            data-bs-target="#collapseOne" aria-expanded="false"
+                                            data-bs-target="#collapseOne" aria-expanded="true"
                                             aria-controls="collapseOne">
                                             Memulai Analisis Data dengan Power BI
                                         </button>
