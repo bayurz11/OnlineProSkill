c5b9373910ba25faf51d7b9065125b1892ff643e
Bayu <bayurz1001@gmail.com>
2024-10-06 12:43:16 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -221,7 +221,7 @@
     <!-- fact-area-end -->
 
     <!-- faq-area -->
-    <section class="faq__area" style="padding-top: 15px;">
+    <section class="faq__area" style="padding-top: 30px;">
         <div class="container">
             <div class="row align-items-center justify-content-center">
                 <!-- Konten Kiri: Target dan Sasaran -->
