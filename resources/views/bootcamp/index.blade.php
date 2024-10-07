cc4e3abcae958d11f604d991c72eeb9fdd74205a
Bayu <bayurz1001@gmail.com>
2024-10-07 18:38:50 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -112,7 +112,7 @@
                     </div>
 
                     <div
-                        style="position: absolute; bottom: 0; left: 0; right: 0; height: 400px; background: linear-gradient(to bottom, transparent, white); z-index: 1;">
+                        style="position: absolute; bottom: 0; left: 0; right: 0; height: 400px; background: linear-gradient(to bottom, transparent, white); z-index: 0;">
                     </div>
                 </div>
 
