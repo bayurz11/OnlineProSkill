82a93a1cb557017cc185d1ef2b019c14b540767c
Bayu <bayurz1001@gmail.com>
2024-10-07 16:46:00 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -502,23 +502,22 @@
                     $kelas = $KelasTatapMuka->firstWhere('id', 17);
                 @endphp
                 @if ($kelas && $kelas->status == 1)
-                    <div class="swiper courses-swiper-active">
+                    <div
+                        class="courses__item courses__item-two shine__animate-item d-flex flex-column w-75 h-50 justify-content-center align-items-center">
+                        <div class="text-center">
+                            <p style="font-size: 1rem; margin-top: 18px;">Investasi</p>
+                        </div>
+                        <div class="text-center">
+                            <del>Rp 599.000</del>
+                        </div>
                         <div
-                            class="courses__item courses__item-two shine__animate-item d-flex flex-column h-100 justify-content-center align-items-center">
-                            <div class="text-center">
-                                <p style="font-size: 1rem; margin-top: 18px;">Investasi</p>
-                            </div>
-                            <div class="text-center">
-                                <del>Rp 599.000</del>
-                            </div>
-                            <div
-                                class="courses__item-content courses__item-content-two d-flex flex-column flex-grow-1 justify-content-center align-items-center">
-                                <ul class="courses__item-meta list-unstyled text-center">
-                                    <li class="price h4">Rp {{ number_format($kelas->price, 0, '.', '.') }}</li>
-                                </ul>
-                            </div>
+                            class="courses__item-content courses__item-content-two d-flex flex-column flex-grow-1 justify-content-center align-items-center">
+                            <ul class="courses__item-meta list-unstyled text-center">
+                                <li class="price h4">Rp {{ number_format($kelas->price, 0, '.', '.') }}</li>
+                            </ul>
                         </div>
                     </div>
+
 
                     <a href="{{ route('cart_bootcamp.checkout', ['id' => $kelas->id]) }}" class="btn arrow-btn"
                         style="font-size: 1rem; padding: 15px 25px; display: flex; justify-content: center; align-items: center; width: 200px;">
