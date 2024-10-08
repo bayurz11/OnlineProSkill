4ab2b28bee47228c4c46aadd79e246884e0a47c6
Bayu <bayurz1001@gmail.com>
2024-10-08 15:32:02 +0700

    up

--- resources/views/bootcamp/index.blade.php
+++ resources/views/bootcamp/index.blade.php
@@ -525,7 +525,7 @@
                     $kelas = $KelasTatapMuka->firstWhere('id', 17);
                 @endphp
                 @if ($kelas && $kelas->status == 1)
-                    <div class="d-flex flex-column align-items-center mt-1" style="width: 300px;">
+                    <div class="d-flex flex-column align-items-center" style="width: 300px; margin-top: -20px;">
                         <div class="courses__item courses__item-two shine__animate-item d-flex flex-column h-100 justify-content-center align-items-center"
                             style="height: 300px; padding: 5px;">
                             <div class="text-center">
