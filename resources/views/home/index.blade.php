c94d45618080ccbaee06b2c12fa029ecc6a436d7
Bayu <bayurz1001@gmail.com>
2024-10-09 18:55:11 +0700

    up

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -59,8 +59,8 @@
                                         class="injectable">
                                 </div>
                                 <div class="content">
-                                    <span>peserta Bootcamp</span>
-                                    <h4 class="title">{{ $bootcamp->count() }} Siswa</h4>
+                                    <span>Siswa Terdaftar</span>
+                                    <h4 class="title">{{ $daftar_siswa->count() }} Siswa</h4>
                                 </div>
                             </div>
                         </div>
