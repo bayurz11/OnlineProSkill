c0aa740432a4200ee1fe69a33502b4511e8eaf54
Bayu <bayurz1001@gmail.com>
2024-10-12 10:36:49 +0700

    up

--- resources/views/home/classroomdetail.blade.php
+++ resources/views/home/classroomdetail.blade.php
@@ -300,14 +300,21 @@
                         <div class="video__section mt-5">
                             <h5 class="title">Video Penjelasan:</h5>
                             <div class="courses__details-video w-100">
-                                <iframe class="img-fluid" width="100%" height="315"
-                                    src="https://www.youtube.com/embed/J8s5kuaTiqo" title="Video Penjelasan"
-                                    frameborder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                    allowfullscreen>
-                                </iframe>
+                                <a href="https://www.youtube.com/watch?v=J8s5kuaTiqo" class="popup-video">
+                                    <img src="{{ asset('public/assets/img/courses/course_thumb02.jpg') }}" alt="img"
+                                        class="img-fluid">
+                                    <i class="fas fa-play"></i>
+                                </a>
                             </div>
                         </div>
+                        <script>
+                            $(document).ready(function() {
+                                $('.popup-video').magnificPopup({
+                                    type: 'iframe'
+                                });
+                            });
+                        </script>
+
 
                     </div>
                 </div>
