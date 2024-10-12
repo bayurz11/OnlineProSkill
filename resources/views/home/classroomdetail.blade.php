e59ec710fc1afa59835f00b5b685641e88892c34
Bayu <bayurz1001@gmail.com>
2024-10-12 10:06:22 +0700

    up

--- resources/views/home/classroomdetail.blade.php
+++ resources/views/home/classroomdetail.blade.php
@@ -228,7 +228,7 @@
                     <div class="courses__details-sidebar">
                         <div class="courses__details-video">
                             <img src="{{ asset('public/assets/img/courses/course_thumb02.jpg') }}" alt="img">
-                            <a href="https://www.youtube.com/watch?v=YwrHGratByU" class="popup-video"><i
+                            <a href="https://www.youtube.com/watch?v=J8s5kuaTiqo" class="popup-video"><i
                                     class="fas fa-play"></i></a>
                         </div>
                         <div class="courses__cost-wrap">
