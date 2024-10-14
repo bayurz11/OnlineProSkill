abecc41d57c546ab4d95c86575e023275ae72b2b
Bayu <bayurz1001@gmail.com>
2024-10-14 18:33:09 +0700

    up

--- resources/views/bootcamp/excel.blade.php
+++ resources/views/bootcamp/excel.blade.php
@@ -19,15 +19,15 @@
                         <h3 class="title tg-svg" data-aos="fade-right" data-aos-delay="400"
                             style="font-size: 2.5rem; line-height: 1.5; padding-top: 20px; margin-left: 0;">
                             <b style="color: #007368">FullStack Intensive Bootcamp</b>
-                            <span class="position-relative" style="display: inline-block; margin-top: 12px;">
+                            <span class="position-relative">
                                 <svg x="0px" y="0px" preserveAspectRatio="none" viewBox="0 0 209 59" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg" style="width: 100%; height: auto;">
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M4.74438 7.70565C69.7006 -1.18799 136.097 -2.38304 203.934 4.1205C207.178 4.48495 209.422 7.14626 208.933 10.0534C206.793 23.6481 205.415 36.5704 204.801 48.8204C204.756 51.3291 202.246 53.5582 199.213 53.7955C136.093 59.7623 74.1922 60.5985 13.5091 56.3043C10.5653 56.0924 7.84371 53.7277 7.42158 51.0325C5.20725 38.2627 2.76333 25.6511 0.0898448 13.1978C-0.465589 10.5873 1.61173 8.1379 4.73327 7.70565"
-                                        fill="currentColor" />
+                                        fill="currentcolor" />
                                 </svg>
                                 Microsoft Excel
-                            </span>
+                            </span><br>
                             <br>
                             <b style="color: #007368">untuk <b>Administrasi Perkantoran</b></b>
                         </h3>
@@ -121,8 +121,8 @@
                             <!-- Tautan untuk membuka video di modal popup -->
                             <a href="https://www.youtube.com/watch?v=1umLD1G-Ljo" class="popup-video" data-aos="fade-left"
                                 data-aos-delay="800">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"
-                                    viewBox="0 0 22 28" fill="none">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 22 28"
+                                    fill="none">
                                     <path
                                         d="M0.19043 26.3132V1.69421C0.190288 1.40603 0.245303 1.12259 0.350273 0.870694C0.455242 0.6188 0.606687 0.406797 0.79027 0.254768C0.973854 0.10274 1.1835 0.0157243 1.39936 0.00193865C1.61521 -0.011847 1.83014 0.0480663 2.02378 0.176003L20.4856 12.3292C20.6973 12.4694 20.8754 12.6856 20.9999 12.9535C21.1245 13.2214 21.1904 13.5304 21.1904 13.8456C21.1904 14.1608 21.1245 14.4697 20.9999 14.7376C20.8754 15.0055 20.6973 15.2217 20.4856 15.3619L2.02378 27.824C1.83056 27.9517 1.61615 28.0116 1.40076 27.9981C1.18536 27.9847 0.97607 27.8983 0.792638 27.7472C0.609205 27.596 0.457661 27.385 0.352299 27.1342C0.246938 26.8833 0.191236 26.6008 0.19043 26.3132Z"
                                         fill="red" />
