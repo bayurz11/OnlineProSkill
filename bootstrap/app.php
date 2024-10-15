b201e623e722894ca75717ca83506caa2af33bce
Bayu <bayurz1001@gmail.com>
2024-10-15 16:07:11 +0700

    up

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,7 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            '/webhook/xendit*'
+            'webhook/xendit*'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
