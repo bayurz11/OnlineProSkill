cb70a864dd1787772caf31cfcfa4040c24b64202
Bayu <bayurz1001@gmail.com>
2024-10-18 10:25:15 +0700

    up

--- app/Http/Controllers/DashboardInstrukturController.php
+++ app/Http/Controllers/DashboardInstrukturController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Carbon;
+use Closure;
 use App\Models\User;
 use App\Models\UserRoles;
 use App\Models\UserProfile;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
 
 class DashboardInstrukturController extends Controller
 {
@@ -28,20 +31,38 @@ class DashboardInstrukturController extends Controller
 
     public function register(Request $request)
     {
-        dd($request);
-
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:3|confirmed',
+            'phone_number' => 'string|max:12|unique:user_profile,phone_number',
+            'g-recaptcha-response' => ['required', function (string $attribute, mixed $value, Closure $fail) {
+                $g_response = Http::asForm()->post("https://www.google.com/recaptcha/api/siteverify", [
+                    'secret' => config('services.recaptcha_v3.secret'),
+                    'response' => $value,
+                    'remoteip' => \request()->ip()
+                ]);
+
+                $g_response = $g_response->json();
+                if (!$g_response['success']) {
+                    $fail("The {$attribute} is invalid: " . implode(', ', $g_response['error-codes']));
+                }
+            },]
         ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput()
+                ->with('error', 'Registrasi gagal! Email atau nomor telepon telah digunakan.');
+        }
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
             'last_login' => Carbon::now(),
-            'status' => 1,
+            'status' => false,
         ]);
 
         $userRole = new UserRoles();
@@ -54,6 +75,6 @@ class DashboardInstrukturController extends Controller
         $userProfile->role_id = 2;
         $userProfile->save();
 
-        return redirect()->route('login_admin')->with('success', 'Pendaftaran berhasil!');
+        return redirect()->route('/')->with('success', 'Pendaftaran berhasil!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/loginstuden', [AuthController::class, 'loginstuden'])->name('loginstuden');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::post('/regisInstruktur', [DashboardInstrukturController::class, 'register'])->name('regisInstruktur');
-// Route::get('/showregister', [DashboardInstrukturController::class, 'showregister'])->name('showregister');
+Route::get('/showregister', [DashboardInstrukturController::class, 'showregister'])->name('showregister');
 Route::post('/regisStuden', [AuthController::class, 'register'])->name('regisStuden');
 // Route::post('/guestregister/{id}', [AuthController::class, 'guestregister'])->name('guestregister');
 Route::post('/guestregister', [AuthController::class, 'guestregister'])->name('guestregister');
