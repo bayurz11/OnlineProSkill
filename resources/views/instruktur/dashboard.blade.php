26b8d186a241ee203b7a5a4830ad818d4a4efba8
Bayu <bayurz1001@gmail.com>
2024-10-21 14:31:41 +0700

    up

--- resources/views/instruktur/dashboard.blade.php
+++ resources/views/instruktur/dashboard.blade.php
@@ -61,39 +61,7 @@
                             <h4 class="title">Dashboard</h4>
                         </div>
                         <div class="row">
-                            <div class="col-lg-4 col-md-4 col-sm-6">
-                                <div class="dashboard__counter-item">
-                                    <div class="icon">
-                                        <i class="skillgro-book"></i>
-                                    </div>
-                                    <div class="content">
-                                        <span class="count odometer" data-count="30"></span>
-                                        <p>ENROLLED COURSES</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-md-4 col-sm-6">
-                                <div class="dashboard__counter-item">
-                                    <div class="icon">
-                                        <i class="skillgro-tutorial"></i>
-                                    </div>
-                                    <div class="content">
-                                        <span class="count odometer" data-count="10"></span>
-                                        <p>ACTIVE COURSES</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-md-4 col-sm-6">
-                                <div class="dashboard__counter-item">
-                                    <div class="icon">
-                                        <i class="skillgro-diploma-1"></i>
-                                    </div>
-                                    <div class="content">
-                                        <span class="count odometer" data-count="7"></span>
-                                        <p>COMPLETED COURSES</p>
-                                    </div>
-                                </div>
-                            </div>
+
                             <div class="col-lg-4 col-md-4 col-sm-6">
                                 <div class="dashboard__counter-item">
                                     <div class="icon">
