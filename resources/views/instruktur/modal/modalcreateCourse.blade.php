03c437a97f30a8b329a0b5e8ace73515233b81d0
Bayu <bayurz1001@gmail.com>
2024-10-21 17:11:03 +0700

    up

--- resources/views/instruktur/modal/modalcreateCourse.blade.php
+++ resources/views/instruktur/modal/modalcreateCourse.blade.php
@@ -1,8 +1,8 @@
-<div class="modal fade" id="CoursesModal" tabindex="-1" aria-labelledby="CoursesModal" aria-hidden="true">
+<div class="modal fade" id="CoursesModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="CoursesModal"></h5>
+                <h5 class="modal-title" id="exampleModalLabel"></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
