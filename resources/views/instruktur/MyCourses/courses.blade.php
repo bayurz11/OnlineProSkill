11bd7ae62eefd83e16443712869c29c2a2c648ce
Bayu <bayurz1001@gmail.com>
2024-10-21 18:24:27 +0700

    up

--- resources/views/instruktur/MyCourses/courses.blade.php
+++ resources/views/instruktur/MyCourses/courses.blade.php
@@ -55,44 +55,48 @@
                                     <div class="tab-pane fade show active" id="itemOne-tab-pane" role="tabpanel"
                                         aria-labelledby="itemOne-tab" tabindex="0">
                                         <div class="row">
-                                            <div class="col-xl-4 col-md-6">
-                                                <div class="courses__item courses__item-two shine__animate-item">
-                                                    <div class="courses__item-thumb courses__item-thumb-two">
-                                                        <a href="course-details.html" class="shine__animate-link">
-                                                            <img src="public/assets/img/courses/course_thumb02.jpg"
-                                                                alt="img">
-                                                        </a>
-                                                    </div>
-                                                    <div class="courses__item-content courses__item-content-two">
-                                                        <ul class="courses__item-meta list-wrap">
-                                                            <li class="courses__item-tag">
-                                                                <a href="course.html">Design</a>
-                                                            </li>
-                                                            <li class="price"><del>$20.00</del>$10.00</li>
-                                                        </ul>
-                                                        <h5 class="title"><a href="course-details.html">The Complete
-                                                                Graphic Design for Beginners</a></h5>
-                                                        <div class="courses__item-content-bottom">
-                                                            <div class="author-two">
-                                                                <a href="instructor-details.html"><img
-                                                                        src="assets/img/courses/course_author002.png"
-                                                                        alt="img">Wilson</a>
-                                                            </div>
-                                                            <div class="avg-rating">
-                                                                <i class="fas fa-star"></i> (4.5 Reviews)
+                                            <div class="alert alert-warning" role="alert">
+                                                Anda Belum Menambahkan kelas Apapun
+                                            </div>
+                                            @foreach ($KelasTatapMuka->where('status', 1)->take(3) as $kelas)
+                                                <div class="col-xl-4 col-md-6">
+                                                    <div class="courses__item courses__item-two shine__animate-item">
+                                                        <div class="courses__item-thumb courses__item-thumb-two">
+                                                            <a href="course-details.html" class="shine__animate-link">
+                                                                <img src="public/assets/img/courses/course_thumb02.jpg"
+                                                                    alt="img">
+                                                            </a>
+                                                        </div>
+                                                        <div class="courses__item-content courses__item-content-two">
+                                                            <ul class="courses__item-meta list-wrap">
+                                                                <li class="courses__item-tag">
+                                                                    <a href="course.html">Design</a>
+                                                                </li>
+                                                                <li class="price"><del>$20.00</del>$10.00</li>
+                                                            </ul>
+                                                            <h5 class="title"><a href="course-details.html">The Complete
+                                                                    Graphic Design for Beginners</a></h5>
+                                                            <div class="courses__item-content-bottom">
+                                                                <div class="author-two">
+                                                                    <a href="instructor-details.html"><img
+                                                                            src="assets/img/courses/course_author002.png"
+                                                                            alt="img">Wilson</a>
+                                                                </div>
+                                                                <div class="avg-rating">
+                                                                    <i class="fas fa-star"></i> (4.5 Reviews)
+                                                                </div>
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                    <div class="courses__item-bottom-two">
-                                                        <ul class="list-wrap">
-                                                            <li><i class="flaticon-book"></i>60</li>
-                                                            <li><i class="flaticon-clock"></i>70h 45m</li>
-                                                            <li><i class="flaticon-mortarboard"></i>202</li>
-                                                        </ul>
+                                                        <div class="courses__item-bottom-two">
+                                                            <ul class="list-wrap">
+                                                                <li><i class="flaticon-book"></i>60</li>
+                                                                <li><i class="flaticon-clock"></i>70h 45m</li>
+                                                                <li><i class="flaticon-mortarboard"></i>202</li>
+                                                            </ul>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-
+                                            @endforeach
                                         </div>
                                     </div>
 
