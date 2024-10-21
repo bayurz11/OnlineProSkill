adb0be09abe94c19bf4b791ba5be101a4d2c702c
Bayu <bayurz1001@gmail.com>
2024-10-21 17:46:35 +0700

    up

--- resources/views/instruktur/modal/modalcreateCourse.blade.php
+++ resources/views/instruktur/modal/modalcreateCourse.blade.php
@@ -7,7 +7,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form action="{{ route('storeCourse') }}" method="POST" enctype="multipart/form-data"
+                <form action="{{ route('courses.store') }}" method="POST" enctype="multipart/form-data"
                     id="createCourseForm">
                     @csrf
                     <div class="mb-3" hidden>
