8638b29dc3571f080a25691d588769d059c9a7e6
Bayu <bayurz1001@gmail.com>
2024-10-21 16:56:22 +0700

    up

--- resources/views/instruktur/profile.blade.php
+++ resources/views/instruktur/profile.blade.php
@@ -24,35 +24,8 @@
     <!-- dashboard-area -->
     <section class="dashboard__area section-pb-120">
         <div class="container">
-            <div class="dashboard__top-wrap">
-                <div class="dashboard__top-bg" data-background="{{ asset('public/assets/img/bg/student_bg.jpg') }}">
-                </div>
-                <div class="dashboard__instructor-info">
-                    <div class="dashboard__instructor-info-left">
-                        <div class="thumb">
-                            <img src="{{ $profile && $profile->gambar ? (strpos($profile->gambar, 'googleusercontent') !== false ? $profile->gambar : asset('public/uploads/' . $profile->gambar)) : asset('public/assets/img/courses/details_instructors02.jpg') }}"
-                                alt="img" width="120" height="120" style="object-fit: cover;">
-                        </div>
-                        <div class="content">
-                            <h4 class="title">{{ $user->name }}</h4>
-                            <div class="review__wrap review__wrap-two">
-                                <div class="rating">
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                </div>
-                                <span>(15 Reviews)</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="dashboard__instructor-info-right">
-                        <a href="#" class="btn btn-two arrow-btn">Create a New Course <img
-                                src="public/assets/img/icons/right_arrow.svg" alt="img" class="injectable"></a>
-                    </div>
-                </div>
-            </div>
+            @include('instruktur.nav.profile')
+
             <div class="row">
                 @include('instruktur.nav.navbar')
 
