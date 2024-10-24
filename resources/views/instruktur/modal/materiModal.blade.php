b7cac7a70dd1e0e6a71fadce1c0d839b5a400939
Bayu <bayurz1001@gmail.com>
2024-10-24 15:18:37 +0700

    up

--- resources/views/instruktur/modal/materiModal.blade.php
+++ resources/views/instruktur/modal/materiModal.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="materiModalModalLabel">Buat Kurikulum Baru</h5>
+                <h5 class="modal-title" id="materiModalModalLabel">Tambah Materi Baru</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
