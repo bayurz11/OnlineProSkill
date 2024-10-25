f6ab45a3567bd257c16ffbe71d696fedbd85020a
Bayu <bayurz1001@gmail.com>
2024-10-25 16:08:49 +0700

    up

--- resources/views/instruktur/profileSetting.blade.php
+++ resources/views/instruktur/profileSetting.blade.php
@@ -60,11 +60,11 @@
                                     <div class="tab-pane fade show active" id="itemOne-tab-pane" role="tabpanel"
                                         aria-labelledby="itemOne-tab" tabindex="0">
                                         <div class="instructor__cover-bg"
-                                            data-background="assets/img/bg/instructor_dashboard_bg.jpg">
+                                            data-background="public/assets/img/bg/instructor_dashboard_bg.jpg">
                                             <div class="instructor__cover-info">
                                                 <div class="instructor__cover-info-left">
                                                     <div class="thumb">
-                                                        <img src="assets/img/courses/details_instructors01.jpg"
+                                                        <img src="public/assets/img/courses/details_instructors01.jpg"
                                                             alt="img">
                                                     </div>
                                                     <button title="Upload Photo"><i class="fas fa-camera"></i></button>
