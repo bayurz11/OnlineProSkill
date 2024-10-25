fc1e95a0729843a8ffc01d8b67f17f1ee804a129
Bayu <bayurz1001@gmail.com>
2024-10-25 10:18:41 +0700

    up

--- resources/views/home/classroomdetail.blade.php
+++ resources/views/home/classroomdetail.blade.php
@@ -278,7 +278,7 @@
                                     <div><span style="font-size: 15px; text-decoration: line-through; opacity: 0.8;">
                                             Rp {{ number_format($courses->price, 0, ',', '.') }}
                                         </span>
-                                        <br>
+
                                         <span style="font-size: 30px; font-weight: bold;">
                                             Rp {{ number_format($courses->discountedPrice, 0, ',', '.') }}
                                         </span>
