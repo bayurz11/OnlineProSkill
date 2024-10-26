de3700b456b80337cf839106cc23f8dadb730cae
Bayu <bayurz1001@gmail.com>
2024-10-26 10:56:09 +0700

    up

--- resources/views/studen/aksespembelian.blade.php
+++ resources/views/studen/aksespembelian.blade.php
@@ -175,19 +175,17 @@
                                                                 <div class="courses__item-content-bottom mt-auto">
 
                                                                     <div class="author-two d-flex align-items-center">
-                                                                        <a href="#">
-                                                                            <?php
-                                                                            // Cek apakah $order, $KelasTatapMuka, dan user profile ada
-                                                                            $userProfile = $order->$KelasTatapMuka->user->userprofile ?? null;
-                                                                            $imageSrc = $userProfile && $userProfile->gambar ? (strpos($userProfile->gambar, 'googleusercontent') !== false ? $userProfile->gambar : asset('public/uploads/' . $userProfile->gambar)) : asset('public/assets/img/courses/details_instructors02.jpg');
-                                                                            ?>
-                                                                            <img src="{{ $imageSrc }}"
-                                                                                alt="Profile Image" class="rounded-circle"
-                                                                                style="border-radius: 50%; width: 30px; height: 30px; object-fit: cover;">
-                                                                            {{ $order->KelasTatapMuka->user->name ?? 'Nama Tidak Diketahui' }}
+                                                                        <a
+                                                                            href="{{ route('profile_instruktur', ['id' => $order->KelasTatapMuka->user->id]) }}">
+                                                                            <img src="{{ asset('public/assets/img/courses/course_author001.png') }}"
+                                                                                alt="img" class="rounded-circle"
+                                                                                style="width: 30px; height: 30px;">
+                                                                            {{ $order->KelasTatapMuka->user->name }}
                                                                         </a>
+                                                                        {{-- <img src="{{ $order->$KelasTatapMuka->user->userprofile && $order->$KelasTatapMuka->user->userprofile->gambar ? (strpos($order->$KelasTatapMuka->user->userprofile->gambar, 'googleusercontent') !== false ? $order->$KelasTatapMuka->user->userprofile->gambar : asset('public/uploads/' . $order->$KelasTatapMuka->user->userprofile->gambar)) : asset('public/assets/img/courses/details_instructors02.jpg') }}"
+                                                                            alt="Profile Image" class="rounded-circle"
+                                                                            style="border-radius: 50%; width: 30px; height: 30px; object-fit: cover;"> --}}
                                                                     </div>
-
                                                                 </div>
                                                             </div>
                                                         </div>
