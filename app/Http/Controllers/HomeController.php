c7c515b08e30317f9ccd134b3f0c1906b1c26cfb
Bayu <bayurz1001@gmail.com>
2024-10-26 14:21:49 +0700

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -186,12 +186,13 @@ class HomeController extends Controller
             $profile = UserProfile::where('user_id', $user->id)->first();
         }
 
-        // Tambahkan kondisi untuk filter course_type = online
+        // Tambahkan kondisi untuk filter course_type = offline
         $course = KelasTatapMuka::with('user')
             ->where('status', 1)
-            ->where('course_type', 'online')
+            ->where('course_type', 'offline')
             ->get();
         $count = $course->count();
+
 
         // Ambil notifikasi untuk pengguna yang sedang login
         $notifikasi = $user ? NotifikasiUser::where('user_id', $user->id)
@@ -210,53 +211,8 @@ class HomeController extends Controller
         // Ambil ID kursus yang telah diikuti oleh user
         $joinedCourses = $user ? Order::where('user_id', $user->id)->pluck('product_id')->toArray() : [];
 
-        return view('home.course', compact('user', 'count', 'course', 'categori', 'profile', 'cart', 'notifikasiCount', 'notifikasi', 'jumlahPendaftaran', 'joinedCourses'));
+        return view('home.course', compact('user', 'categori', 'count', 'course', 'profile', 'cart', 'notifikasiCount', 'notifikasi', 'jumlahPendaftaran', 'joinedCourses'));
     }
-
-
-
-    // public function coursedetail($id)
-    // {
-    //     $categori = Categories::all();
-    //     $user = Auth::user();
-    //     $profile = null;
-    //     $cart = Session::get('cart', []);
-    //     $kurikulum = Kurikulum::with('user')->where('course_id', $id)->get();
-    //     if ($user) {
-    //         $profile = UserProfile::where('user_id', $user->id)->first();
-    //     }
-
-    //     $courses = KelasTatapMuka::find($id);
-
-    //     if (!$courses) {
-    //         abort(404, 'Kelas tatap muka tidak ditemukan.');
-    //     }
-
-    //     $courseList = json_decode($courses->include, true);
-
-    //     if (!is_array($courseList)) {
-    //         $courseList = [];
-    //     }
-
-    //     $fasilitas = json_decode($courses->fasilitas, true);
-
-    //     // Ambil notifikasi untuk pengguna yang sedang login
-    //     $notifikasi = $user ? NotifikasiUser::where('user_id', $user->id)
-    //         ->orderBy('created_at', 'desc')
-    //         ->get()
-    //         : collect(); // Menggunakan collect() untuk membuat koleksi kosong jika pengguna belum login
-
-    //     // Hitung jumlah notifikasi dengan status = 1
-    //     $notifikasiCount = $notifikasi->where('status', 1)->count();
-    //     $jumlahPendaftaran = Order::where('product_id', $id)->count();
-
-    //     // Ambil section yang relevan dengan kurikulum
-    //     $section = Section::whereIn('kurikulum_id', $kurikulum->pluck('id'))->get()->groupBy('kurikulum_id');
-
-    //     $joinedCourses = $user ? Order::where('user_id', $user->id)->pluck('product_id')->toArray() : [];
-
-    //     return view('home.coursedetail', compact('user', 'categori', 'jumlahPendaftaran', 'courses', 'kurikulum', 'courseList', 'profile', 'cart', 'notifikasiCount', 'notifikasi', 'section', 'joinedCourses'));
-    // }
 
     public function coursedetail($id)
     {
