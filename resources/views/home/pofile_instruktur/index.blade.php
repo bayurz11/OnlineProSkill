9fa8fe91a2c7186fdc25a9e0f203180f8c8a3749
Bayu <bayurz1001@gmail.com>
2024-10-26 13:24:46 +0700

    up

--- resources/views/home/pofile_instruktur/index.blade.php
+++ resources/views/home/pofile_instruktur/index.blade.php
@@ -47,7 +47,8 @@
                         <div class="instructor__details-info">
                             <div class="instructor__details-thumb">
                                 <img src="{{ $instructorProfile && $instructorProfile->gambar ? (strpos($instructorProfile->gambar, 'googleusercontent') !== false ? $instructorProfile->gambar : asset('public/uploads/' . $instructorProfile->gambar)) : asset('public/assets/img/courses/details_instructors02.jpg') }}"
-                                    alt="img" style="object-fit: cover;">
+                                    alt="img"
+                                    style="border-radius: 50%; width: 100px; height: 100px; object-fit: cover;">
                             </div>
                             <div class="instructor__details-content">
                                 <h2 class="title">Robert Fox</h2>
