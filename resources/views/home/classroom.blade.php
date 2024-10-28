4ab091045a1236cb0584ed9e42d7255cdfc9927a
Bayu <bayurz1001@gmail.com>
2024-10-28 20:36:27 +0700

    up

--- resources/views/home/classroom.blade.php
+++ resources/views/home/classroom.blade.php
@@ -123,9 +123,8 @@
                                                         <a
                                                             href="{{ route('classroomdetail', ['id' => $cours->id]) }}">{{ $cours->nama_kursus }}</a>
                                                     </h5>
-                                                    <p class="author">By <a
-                                                            href="{{ route('profile_instruktur', ['id' => $cours->user->id]) }}">{{ $cours->user->name }}</a>&nbsp;&nbsp;
-                                                        <img src="{{ asset('public/assets/img/icons/course_icon06.svg') }}"
+                                                    <p class="author"><img
+                                                            src="{{ asset('public/assets/img/icons/course_icon06.svg') }}"
                                                             alt="img" class="injectable">
                                                         Kuota Kelas
                                                         <span>{{ $jumlahPendaftaran->get($cours->id, 0) }}/{{ $cours->kuota }}</span>
@@ -199,9 +198,8 @@
                                                     <h5 class="title"><a
                                                             href="{{ route('classroomdetail', ['id' => $cours->id]) }}">{{ $cours->nama_kursus }}</a>
                                                     </h5>
-                                                    <p class="author">By <a
-                                                            href="{{ route('profile_instruktur', ['id' => $cours->user->id]) }}">{{ $cours->user->name }}</a>&nbsp;&nbsp;
-                                                        <img src="{{ asset('public/assets/img/icons/course_icon06.svg') }}"
+                                                    <p class="author"><img
+                                                            src="{{ asset('public/assets/img/icons/course_icon06.svg') }}"
                                                             alt="img" class="injectable">
                                                         Kuota Kelas
                                                         <span>{{ $jumlahPendaftaran->get($cours->id, 0) }}/{{ $cours->kuota }}</span>
