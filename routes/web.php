f29276ba72ef1eb68efadc75c7b7c3b8ab2013cc
Bayu <bayurz1001@gmail.com>
2024-10-28 20:12:50 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -297,7 +297,7 @@ Route::get('/cart_bootcamp/checkout/{id}', [BootcampController::class, 'addToCar
 Route::get('/cart_bootcamp', [BootcampController::class, 'show'])->name('cart_bootcamp.view');
 Route::post('cart_bootcamp/remove/{id}', [BootcampController::class, 'removeFromCart'])->name('cart_bootcamp.remove');
 
-//PowerEXCEL
+//EXCEL
 Route::get('/excel', [BootcampController::class, 'indexexcel'])->name('excel');
 Route::get('/cart_bootcamp/checkout/{id}', [BootcampController::class, 'addToCartceckout'])->name('cart_bootcamp.checkout');
 Route::get('/cart_bootcamp', [BootcampController::class, 'show'])->name('cart_bootcamp.view');
