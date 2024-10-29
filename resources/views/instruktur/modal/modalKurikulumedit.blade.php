1d6a35ca2a55931f8256c729c58d8f2c0af39f7f
Bayu <bayurz1001@gmail.com>
2024-10-29 14:38:05 +0700

    up

--- resources/views/instruktur/modal/modalKurikulumedit.blade.php
+++ resources/views/instruktur/modal/modalKurikulumedit.blade.php
@@ -18,7 +18,7 @@
                     <div class="mb-3">
                         <label for="edittitle" class="form-label">Judul<span class="text-danger">*</span></label>
                         <input type="text" class="form-control" id="edittitle" name="title"
-                            placeholder="Masukkan judul Kurikulum Anda" required>
+                            value="{{ $kuri->title }}" placeholder="Masukkan judul Kurikulum Anda" required>
                     </div>
                 </form>
             </div>
