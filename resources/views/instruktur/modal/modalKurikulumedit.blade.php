61bbb989382c175d40eabd5454520ff2d7cab74c
Bayu <bayurz1001@gmail.com>
2024-10-29 14:25:05 +0700

    up

--- resources/views/instruktur/modal/modalKurikulumedit.blade.php
+++ resources/views/instruktur/modal/modalKurikulumedit.blade.php
@@ -1,3 +1,4 @@
+<!-- Modal Edit Kurikulum -->
 <div class="modal fade" id="kurikulumModalEdit" tabindex="-1" aria-labelledby="kurikulumModalEditModalLabel"
     aria-hidden="true">
     <div class="modal-dialog modal-lg">
@@ -7,51 +8,44 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form action="{{ route('instruktur_kurikulum.store') }}" method="POST" id="createKurikulumForm">
+                <form action="" method="POST" id="editKurikulumForm">
                     @csrf
-                    <!-- Hidden field for course_id -->
-                    <input type="hidden" name="course_id" id="course_id">
+                    @method('PUT') <!-- Tambahkan ini untuk menggunakan metode PUT -->
+                    <input type="hidden" name="course_id" id="edit_course_id">
+                    <input type="hidden" name="id" id="edit_kurikulum_id">
+                    <!-- Hidden field untuk ID kurikulum -->
 
                     <div class="mb-3">
                         <label for="edittitle" class="form-label">Judul<span class="text-danger">*</span></label>
                         <input type="text" class="form-control" id="edittitle" name="title"
-                            placeholder="Masukkan judul Kurikulum Anda">
+                            placeholder="Masukkan judul Kurikulum Anda" required>
                     </div>
-
                 </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Keluar</button>
-                <button type="submit" class="btn btn-primary" form="createKurikulumForm">Simpan</button>
+                <button type="submit" class="btn btn-primary" form="editKurikulumForm">Simpan</button>
             </div>
         </div>
     </div>
 </div>
+
 <script>
-    $(document).ready(function() {
-        $('#kurikulumModalEdit').on('show.bs.modal', function(event) {
-            var button = $(event.relatedTarget); // Tombol yang membuka modal
-            var kurikulumId = button.data('id'); // Ambil data-id dari tombol
-            console.log('Kurikulum ID:', kurikulumId); // Debugging line
+    $(document).on('click', '.editKurikulumBtn', function() {
+        var id = $(this).data('id'); // Ambil ID dari tombol edit
+        $.ajax({
+            url: '/instruktur_kurikulum/' + id + '/edit', // Ganti dengan route edit
+            type: 'GET',
+            success: function(response) {
+                $('#edit_kurikulum_id').val(response.id); // Set ID kurikulum
+                $('#edit_course_id').val(response.course_id); // Set course_id
+                $('#edittitle').val(response.title); // Set judul
 
-            // AJAX request untuk mengambil data kurikulum
-            $.ajax({
-                url: '/instruktur_kurikulum/' + kurikulumId + '/edit',
-                method: 'GET',
-                success: function(response) {
-                    console.log(response); // Debugging line
-                    $('#course_id').val(response.id); // Set nilai course_id di dalam modal
-                    $('#edittitle').val(response
-                        .title); // Set nilai judul kurikulum di dalam modal
-
-                    // Set action form dengan id yang benar
-                    $('#editKurikulumForm').attr('action', '/instruktur_kurikulumupdate/' +
-                        kurikulumId);
-                },
-                error: function(xhr) {
-                    console.log('Error:', xhr);
-                }
-            });
+                $('#kurikulumModalEdit').modal('show'); // Tampilkan modal
+            },
+            error: function() {
+                alert('Data tidak ditemukan');
+            }
         });
     });
 </script>
