0e575e3cce8eca409c0f905fcc81ca240a69a014
Bayu <bayurz1001@gmail.com>
2024-10-29 11:38:25 +0700

    up

--- resources/views/home/classroomdetail.blade.php
+++ resources/views/home/classroomdetail.blade.php
@@ -198,7 +198,7 @@
                                                         <ul class="list-wrap">
                                                             @foreach ($section[$kurikulumItem->id] ?? [] as $sectionItem)
                                                                 <li class="course-item">
-                                                                    <a href="{{ route('lesson', ['id' => $lessonId]) }}"
+                                                                    <a href="{{ route('lesson', ['id' => $id]) }}"
                                                                         class="course-item-link">
                                                                         {{ $sectionItem->title }}
                                                                         <div class="course-item-meta">
