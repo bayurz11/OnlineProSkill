3c680568b1874d6c4bc6d2b956fbaa7c3b37b311
Bayu <bayurz1001@gmail.com>
2024-10-30 14:20:49 +0700

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,14 +37,15 @@ class HomeController extends Controller
             ->pluck('course_type')
             ->map(function ($type) {
                 return $type === 'online' ? 'Kelas Online' : ($type === 'offline' ? 'Kelas Tatap Muka' : $type);
-            });
+            })
+            ->prepend('Semua Program'); // Menambahkan 'all' untuk semua program
 
         // Mengambil KelasTatapMuka berdasarkan course_type
         $KelasTatapMuka = KelasTatapMuka::whereIn('course_type', ['online', 'offline'])
             ->orderBy('created_at', 'asc')
             ->get()
             ->flatMap(function ($kelas) {
-                return [$kelas];
+                return [$kelas,];
             });
 
         // Mengambil Blog terbaru
