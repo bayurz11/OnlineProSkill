9a58ffaa61d921b323540df90ffdff797c7b5002
Bayu <bayurz1001@gmail.com>
2024-10-31 15:49:29 +0700

    up

--- resources/views/home/classroomdetail.blade.php
+++ resources/views/home/classroomdetail.blade.php
@@ -100,7 +100,11 @@
                                         aria-controls="jadwal-tab-pane" aria-selected="false">Jadwal Kelas</button>
                                 </li>
                             @endif
-
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link" id="reviews-tab" data-bs-toggle="tab"
+                                    data-bs-target="#reviews-tab-pane" type="button" role="tab"
+                                    aria-controls="reviews-tab-pane" aria-selected="false">reviews</button>
+                            </li>
                         </ul>
                         <div class="tab-content" id="myTabContent">
                             <div class="tab-pane fade show active" id="overview-tab-pane" role="tabpanel"
@@ -306,7 +310,105 @@
                                     </div>
                                 @endif
                             </div>
-
+                            <div class="tab-pane fade" id="reviews-tab-pane" role="tabpanel"
+                                aria-labelledby="reviews-tab" tabindex="0">
+                                <div class="courses__rating-wrap">
+                                    <h2 class="title">Reviews</h2>
+                                    <div class="course-rate">
+                                        <div class="course-rate__summary">
+                                            <div class="course-rate__summary-value">4.8</div>
+                                            <div class="course-rate__summary-stars">
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                            </div>
+                                            <div class="course-rate__summary-text">
+                                                12 Ratings
+                                            </div>
+                                        </div>
+                                        <div class="course-rate__details">
+                                            <div class="course-rate__details-row">
+                                                <div class="course-rate__details-row-star">
+                                                    5
+                                                    <i class="fas fa-star"></i>
+                                                </div>
+                                                <div class="course-rate__details-row-value">
+                                                    <div class="rating-gray"></div>
+                                                    <div class="rating" style="width:80%;" title="80%"></div>
+                                                    <span class="rating-count">2</span>
+                                                </div>
+                                            </div>
+                                            <div class="course-rate__details-row">
+                                                <div class="course-rate__details-row-star">
+                                                    4
+                                                    <i class="fas fa-star"></i>
+                                                </div>
+                                                <div class="course-rate__details-row-value">
+                                                    <div class="rating-gray"></div>
+                                                    <div class="rating" style="width:50%;" title="50%"></div>
+                                                    <span class="rating-count">1</span>
+                                                </div>
+                                            </div>
+                                            <div class="course-rate__details-row">
+                                                <div class="course-rate__details-row-star">
+                                                    3
+                                                    <i class="fas fa-star"></i>
+                                                </div>
+                                                <div class="course-rate__details-row-value">
+                                                    <div class="rating-gray"></div>
+                                                    <div class="rating" style="width:0%;" title="0%"></div>
+                                                    <span class="rating-count">0</span>
+                                                </div>
+                                            </div>
+                                            <div class="course-rate__details-row">
+                                                <div class="course-rate__details-row-star">
+                                                    2
+                                                    <i class="fas fa-star"></i>
+                                                </div>
+                                                <div class="course-rate__details-row-value">
+                                                    <div class="rating-gray"></div>
+                                                    <div class="rating" style="width:0%;" title="0%"></div>
+                                                    <span class="rating-count">0</span>
+                                                </div>
+                                            </div>
+                                            <div class="course-rate__details-row">
+                                                <div class="course-rate__details-row-star">
+                                                    1
+                                                    <i class="fas fa-star"></i>
+                                                </div>
+                                                <div class="course-rate__details-row-value">
+                                                    <div class="rating-gray"></div>
+                                                    <div class="rating" style="width:0%;" title="0%"></div>
+                                                    <span class="rating-count">0</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="course-review-head">
+                                        <div class="review-author-thumb">
+                                            <img src="assets/img/courses/review-author.png" alt="img">
+                                        </div>
+                                        <div class="review-author-content">
+                                            <div class="author-name">
+                                                <h5 class="name">Jura Hujaor <span>2 Days ago</span></h5>
+                                                <div class="author-rating">
+                                                    <i class="fas fa-star"></i>
+                                                    <i class="fas fa-star"></i>
+                                                    <i class="fas fa-star"></i>
+                                                    <i class="fas fa-star"></i>
+                                                    <i class="fas fa-star"></i>
+                                                </div>
+                                            </div>
+                                            <h4 class="title">The best LMS Design System</h4>
+                                            <p>Maximus ligula eleifend id nisl quis interdum. Sed malesuada tortor non
+                                                turpis semper bibendum nisi porta, malesuada risus nonerviverra dolor.
+                                                Vestibulum ante ipsum primis in faucibus.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
