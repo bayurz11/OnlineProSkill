9734c43a1609d92491453a9d08a19a971d81d89d
Bayu <bayurz1001@gmail.com>
2024-11-02 10:17:44 +0700

    up

--- app/Http/Controllers/AksesPembelianController.php
+++ app/Http/Controllers/AksesPembelianController.php
@@ -169,11 +169,11 @@ class AksesPembelianController extends Controller
         return view('studen.lesson', compact('user', 'sertifikat', 'categori', 'profile', 'cart', 'notifikasi', 'notifikasiCount', 'orders', 'kurikulum', 'allSectionsCompleted'));
     }
 
-    public function fetchContent()
+    public function fetchContent($id)
     {
 
         // $kurikulum = Kurikulum::with('sections')->where('id', $kurikulum_id)->get();
-        $kurikulum = Kurikulum::with('sections')->where('id')->get();
+        $kurikulum = Section::with('kurikulum')->where('kurikulum_id', $id)->get();
 
         return view('studen.partials.kurikulum-content', compact('kurikulum'));
     }
