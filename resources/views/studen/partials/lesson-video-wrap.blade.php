3d18fb5897c8b5499cb1e93a65ff0ca20be9cc26
Bayu <bayurz1001@gmail.com>
2024-11-07 16:52:34 +0700

    up

--- resources/views/studen/partials/lesson-video-wrap.blade.php
+++ resources/views/studen/partials/lesson-video-wrap.blade.php
@@ -1,3 +1,22 @@
+<div class="lesson__video-wrap">
+    <div class="lesson__video-wrap-top">
+        <div class="lesson__video-wrap-top-left">
+            <a href="#"><i class="flaticon-arrow-right"></i></a>
+            <span id="video-title">Choose a lesson</span>
+        </div>
+        <div class="lesson__video-wrap-top-right">
+            <a href="#"><i class="fas fa-times"></i></a>
+        </div>
+    </div>
+
+    <iframe id="player" width="100%" height="500px" src="" frameborder="0"></iframe>
+
+    <div class="lesson__next-prev-button">
+        <button class="prev-button" title="Previous Lesson"><i class="flaticon-arrow-right"></i></button>
+        <button class="next-button" title="Next Lesson"><i class="flaticon-arrow-right"></i></button>
+    </div>
+</div>
+
 <script>
     document.addEventListener('DOMContentLoaded', () => {
         // Ambil semua item kursus
