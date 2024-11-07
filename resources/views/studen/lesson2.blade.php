522137e3b6eff59172763ec475de8a0b31bac06c
Bayu <bayurz1001@gmail.com>
2024-11-07 15:32:08 +0700

    up

--- resources/views/studen/lesson2.blade.php
+++ resources/views/studen/lesson2.blade.php
@@ -82,28 +82,7 @@
                     </div>
                 </div>
                 <div class="col-xl-9 col-lg-8">
-                    <div class="lesson__video-wrap">
-                        <div class="lesson__video-wrap-top">
-                            <div class="lesson__video-wrap-top-left">
-                                <a href="#"><i class="flaticon-arrow-right"></i></a>
-                                <span>The Complete Design Course: From Zero to Expert!</span>
-                            </div>
-                            <div class="lesson__video-wrap-top-right">
-                                <a href="#"><i class="fas fa-times"></i></a>
-                            </div>
-                        </div>
-                        <video id="player" playsinline controls data-poster="assets/img/bg/video_bg.webp">
-                            <source src="assets/video/video.mp4" type="video/mp4" />
-                            <source src="/path/to/video.webm" type="video/webm" />
-                        </video>
-                        <div class="lesson__next-prev-button">
-                            <button class="prev-button" title="Create a Simple React App"><i
-                                    class="flaticon-arrow-right"></i></button>
-                            <button class="next-button" title="React for the Rest of us"><i
-                                    class="flaticon-arrow-right"></i></button>
-                        </div>
-                    </div>
-
+                    @include('studen.partials.lesson-video-wrap')
                 </div>
             </div>
         </div>
