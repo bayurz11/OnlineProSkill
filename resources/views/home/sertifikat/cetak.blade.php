31d1f8dcf7f02af5f02f5a6f5b8c94e14c6fe96e
Bayu <bayurz1001@gmail.com>
2024-11-09 09:21:01 +0700

    up

--- resources/views/home/sertifikat/cetak.blade.php
+++ resources/views/home/sertifikat/cetak.blade.php
@@ -21,16 +21,12 @@
             justify-content: center;
             align-items: center;
             background-color: #f0f0f0;
-            /* Background color to distinguish the certificate area */
             overflow: hidden;
-            /* Prevents scroll bars from appearing */
         }
 
         .certificate-container {
-            width: 148.5mm;
-            /* Half of 297mm */
-            height: 105mm;
-            /* Half of 210mm */
+            width: 297mm;
+            height: 210mm;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -41,15 +37,12 @@
 
         .certificate {
             width: 100%;
-            max-width: 100%;
             height: 100%;
-            max-height: 100%;
             display: flex;
             flex-direction: column;
             justify-content: space-between;
             align-items: center;
-            padding: 10mm;
-            /* Half of 20mm */
+            padding: 20mm;
             box-sizing: border-box;
             position: relative;
         }
@@ -62,83 +55,62 @@
 
         .content h1 {
             margin: 0;
-            font-size: 40px;
-            /* Half of 80px */
+            font-size: 80px;
             color: #007F73;
-            margin-top: 10mm;
-            /* Half of 20mm */
+            margin-top: 20mm;
         }
 
         .content h2 {
             margin: 0;
-            font-size: 30px;
-            /* Half of 60px */
+            font-size: 60px;
             color: #007F73;
-            margin-top: 5mm;
-            /* Half of 10mm */
+            margin-top: 10mm;
         }
 
         .content h3 {
             margin: 0;
-            font-size: 15px;
-            /* Half of 30px */
+            font-size: 30px;
             color: #007F73;
         }
 
         .content .underline {
             width: 50%;
-            height: 1px;
-            /* Half of 2px */
+            height: 2px;
             background-color: #757575;
-            margin: 3px auto;
-            /* Half of 6px */
+            margin: 6px auto;
         }
 
         .content p {
-            margin: 5mm 0;
-            /* Half of 10mm */
-            font-size: 9px;
-            /* Half of 18px */
+            margin: 10mm 0;
+            font-size: 18px;
             color: #666;
         }
 
         .content img.photo {
             position: absolute;
-            top: 7.5mm;
-            /* Half of 15mm */
-            right: -13px;
-            /* Half of -26px */
-            width: 90px;
-            /* Half of 180px */
-            height: 100px;
-            /* Half of 200px */
+            top: 15mm;
+            right: -26px;
+            width: 180px;
+            height: 200px;
             object-fit: cover;
             border-radius: 10%;
-            border: 2.5px solid #fdffff;
-            /* Half of 5px */
-        }
-
-        .qr svg {
-            width: 75px;
-            /* Half of 150px */
-            height: 75px;
-            /* Half of 150px */
+            border: 5px solid #fdffff;
         }
 
         .qr {
             position: absolute;
-            top: 65mm;
-            /* Half of 130mm */
-            left: 7.5mm;
-            /* Half of 15mm */
-            width: 75px;
-            /* Half of 150px */
-            height: 75px;
-            /* Half of 150px */
+            top: 130mm;
+            left: 15mm;
+            width: 150px;
+            height: 150px;
             object-fit: cover;
-            border: 2.5px solid #fdffff;
-            /* Half of 5px */
+            border: 5px solid #fdffff;
             border-radius: 10px;
+        }
+
+        .qr svg {
+            width: 150px;
+            height: 150px;
         }
 
         .signature {
@@ -150,8 +122,7 @@
 
         .signature p {
             margin: 0;
-            font-size: 9px;
-            /* Half of 18px */
+            font-size: 18px;
             color: #333;
             text-align: right;
         }
@@ -173,85 +144,72 @@
             background-color: #ff9800;
         }
 
-        @media print {
-            @page {
-                size: A4 landscape;
-                margin: 0;
-            }
-
-            body,
-            html {
-                -webkit-print-color-adjust: exact;
-                print-color-adjust: exact;
-                width: 297mm;
-                /* A4 landscape */
-                height: 210mm;
-                margin: 0;
-                background: url('{{ asset('public/1.png') }}') no-repeat center center !important;
-                background-size: cover !important;
-            }
-
+        @media (max-width: 768px) {
             .certificate-container {
-                width: 297mm;
-                height: 210mm;
+                width: 100%;
+                height: auto;
                 box-shadow: none;
+                margin: 10px;
+                padding: 0;
+                background-size: contain;
             }
 
             .certificate {
-                padding: 20mm;
-                max-width: 297mm;
-                max-height: 210mm;
+                padding: 10px;
+                max-width: 100%;
             }
 
             .content h1 {
-                font-size: 80px;
-                margin-top: 20mm;
+                font-size: 40px;
+                margin-top: 5mm;
             }
 
             .content h2 {
-                font-size: 60px;
-                margin-top: 10mm;
+                font-size: 30px;
+                margin-top: 5mm;
             }
 
             .content h3 {
-                font-size: 30px;
-            }
-
-            .content .underline {
-                height: 2px;
-                margin: 6px auto;
+                font-size: 20px;
             }
 
             .content p {
-                margin: 10mm 0;
-                font-size: 18px;
+                margin: 5mm 0;
+                font-size: 12px;
             }
 
             .content img.photo {
-                top: 15mm;
-                right: -26px;
-                width: 180px;
-                height: 200px;
-                border: 5px solid #fdffff;
+                top: 5mm;
+                right: -13px;
+                width: 90px;
+                height: 100px;
+                border: 2.5px solid #fdffff;
             }
 
             .qr {
-                top: 130mm;
-                left: 15mm;
-                width: 150px;
-                height: 150px;
-                border: 5px solid #fdffff;
+                top: 70%;
+                left: 10px;
+                width: 75px;
+                height: 75px;
+                border: 2.5px solid #fdffff;
             }
 
             .qr svg {
-                width: 150px;
-                height: 150px;
+                width: 75px;
+                height: 75px;
             }
 
             .signature p {
-                font-size: 18px;
+                font-size: 12px;
             }
 
+            .print-button {
+                bottom: 10px;
+                padding: 5px 10px;
+            }
+        }
+
+        @media print {
             .print-button {
                 display: none;
             }
@@ -273,7 +231,6 @@
                         <img class="photo" src="{{ asset('public/uploads/' . $profile->gambar) }}" alt="Foto Peserta">
                     @endif
                 @endif
-
                 <div class="qr">{!! $qrCode !!}</div>
                 <h2>{{ $user->name }}</h2>
                 <p>Atas Kelulusannya Pada Kelas</p>
