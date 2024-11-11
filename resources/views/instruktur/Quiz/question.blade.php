5175b77dc0c705f7140738f0c2dee482ff23d9f6
Bayu <bayurz1001@gmail.com>
2024-11-11 16:00:34 +0700

    up

--- resources/views/instruktur/Quiz/question.blade.php
+++ resources/views/instruktur/Quiz/question.blade.php
@@ -9,14 +9,14 @@
     <div class="breadcrumb__area breadcrumb__bg breadcrumb__bg-three"
         data-background="public/assets/img/bg/breadcrumb_bg.jpg">
         <div class="breadcrumb__shape-wrap">
-            <img src="public/assets/img/others/breadcrumb_shape01.svg" alt="img" class="alltuchtopdown">
-            <img src="public/assets/img/others/breadcrumb_shape02.svg" alt="img" data-aos="fade-right"
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape01.svg') }}" alt="img" class="alltuchtopdown">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape02.svg') }}" alt="img" data-aos="fade-right"
                 data-aos-delay="300">
-            <img src="public/assets/img/others/breadcrumb_shape03.svg" alt="img" data-aos="fade-up"
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape03.svg') }}" alt="img" data-aos="fade-up"
                 data-aos-delay="400">
-            <img src="public/assets/img/others/breadcrumb_shape04.svg" alt="img" data-aos="fade-down-left"
-                data-aos-delay="400">
-            <img src="public/assets/img/others/breadcrumb_shape05.svg" alt="img" data-aos="fade-left"
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape04.svg') }}" alt="img"
+                data-aos="fade-down-left" data-aos-delay="400">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape05.svg') }}" alt="img" data-aos="fade-left"
                 data-aos-delay="400">
         </div>
     </div>
@@ -50,51 +50,6 @@
         </div>
     </section>
     <!-- dashboard-area-end -->
-    <!-- Form untuk penghapusan dengan JavaScript -->
-    <form id="delete-form" action="" method="POST" style="display: none;">
-        @csrf
-        @method('DELETE')
-    </form>
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const deleteLinks = document.querySelectorAll('.delete-quiz');
-            const deleteForm = document.getElementById('delete-form');
-            const confirmationModal = document.getElementById('confirmationModal');
-            const confirmDeleteBtn = document.getElementById('confirmDelete');
-            const cancelDeleteBtn = document.getElementById('cancelDelete');
-
-            let quizId; // Variable to store quiz ID for deletion
-
-            deleteLinks.forEach(link => {
-                link.addEventListener('click', function(e) {
-                    e.preventDefault();
-                    quizId = this.getAttribute('data-id');
-                    confirmationModal.style.display = 'flex';
-                });
-            });
-
-            confirmDeleteBtn.addEventListener('click', function() {
-                deleteForm.action = `/instruktur_quiz/${quizId}`;
-                deleteForm.submit();
-                confirmationModal.style.display = 'none';
-            });
-
-            cancelDeleteBtn.addEventListener('click', function() {
-                confirmationModal.style.display = 'none';
-            });
-        });
-    </script>
-
-    <!-- HTML for the modal -->
-    <div id="confirmationModal"
-        style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); display: none; justify-content: center; align-items: center; z-index: 1000;">
-        <div style="background: white; padding: 40px; border-radius: 8px; text-align: center;">
-            <h4>Konfirmasi Penghapusan</h4><br>
-            <p>Apakah Anda yakin ingin menghapus quiz ini?</p><br>
-            <button id="confirmDelete" class="btn btn-danger btn-lg">Ya, Hapus</button>
-            <button id="cancelDelete" class="btn btn-secondary btn-lg">Batal</button>
-        </div>
-    </div>
 
 @endsection
