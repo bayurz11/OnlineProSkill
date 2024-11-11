7823ad65877eed4eed64981946f85d74ded294b1
Bayu <bayurz1001@gmail.com>
2024-11-11 17:40:59 +0700

    up

--- resources/views/instruktur/Quiz/quiz.blade.php
+++ resources/views/instruktur/Quiz/quiz.blade.php
@@ -162,12 +162,9 @@
             <button id="cancelDelete" class="btn btn-secondary btn-lg">Batal</button>
         </div>
     </div>
-
     <div id="confirmationModalQuestion"
-        style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); display: none; justify-content: center; align-items: center; z-index: 1000;"
-        onclick="hideModal(event)">
-        <div style="background: white; padding: 40px; border-radius: 8px; text-align: center; position: relative;"
-            onclick="event.stopPropagation()">
+        style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); display: none; justify-content: center; align-items: center; z-index: 1000;">
+        <div style="background: white; padding: 40px; border-radius: 8px; text-align: center; position: relative;">
             <h4>Pilih Jenis Pertanyaan</h4><br>
             <p>Silakan pilih jenis pertanyaan yang ingin ditambahkan.</p><br>
             <!-- Button menuju halaman pilihan ganda -->
@@ -180,6 +177,7 @@
             <button onclick="hideModal()"
                 style="position: absolute; top: 10px; right: 10px; background: none; border: none; font-size: 24px; color: #333;">&times;</button>
         </div>
+        <button onclick="hideModal()" class="btn btn-danger btn-lg">Batal</button>
     </div>
 
     <!-- Script JavaScript untuk menampilkan dan menyembunyikan modal -->
@@ -188,14 +186,9 @@
             document.getElementById('confirmationModalQuestion').style.display = 'flex';
         }
 
-        function hideModal(event) {
-            // Pastikan kita hanya menutup modal jika area luar yang diklik
-            if (event) {
-                event.stopPropagation(); // Mencegah event propagation ke modal
-            }
+        function hideModal() {
             document.getElementById('confirmationModalQuestion').style.display = 'none';
         }
     </script>
-
 
 @endsection
