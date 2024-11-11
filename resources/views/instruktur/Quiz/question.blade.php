75a1bb46a5b5809cfb003636d62e31b72aa148af
Bayu <bayurz1001@gmail.com>
2024-11-11 16:54:23 +0700

    up

--- resources/views/instruktur/Quiz/question.blade.php
+++ resources/views/instruktur/Quiz/question.blade.php
@@ -54,20 +54,19 @@
                                                     class="form-control" placeholder="Masukkan pilihan A">
                                             </div>
                                             <div class="choice">
-                                                <label for="optionE_1">Pilihan E</label>
-                                                <input type="text" id="optionE_1" name="questions[1][options][E]"
-                                                    class="form-control" placeholder="Masukkan pilihan E">
+                                                <label for="optionD_1">Pilihan D</label>
+                                                <input type="text" id="optionD_1" name="questions[1][options][D]"
+                                                    class="form-control" placeholder="Masukkan pilihan D">
                                             </div>
-
                                             <div class="choice">
                                                 <label for="optionB_1">Pilihan B</label>
                                                 <input type="text" id="optionB_1" name="questions[1][options][B]"
                                                     class="form-control" placeholder="Masukkan pilihan B">
                                             </div>
                                             <div class="choice">
-                                                <label for="optionD_1">Pilihan D</label>
-                                                <input type="text" id="optionD_1" name="questions[1][options][D]"
-                                                    class="form-control" placeholder="Masukkan pilihan D">
+                                                <label for="optionE_1">Pilihan E</label>
+                                                <input type="text" id="optionE_1" name="questions[1][options][E]"
+                                                    class="form-control" placeholder="Masukkan pilihan E">
                                             </div>
 
                                             <div class="choice">
@@ -123,39 +122,47 @@
                 
                 <!-- Grid untuk pilihan jawaban A, B, C, D, E -->
                 <div class="form-group">
-                    <div class="choices-grid">
-                        <div class="choice">
-                            <label for="optionA_${questionCount}">Pilihan A</label>
-                            <input type="text" id="optionA_${questionCount}" name="questions[${questionCount}][options][A]" class="form-control" placeholder="Masukkan pilihan A">
-                        </div>
-                        <div class="choice">
-                            <label for="optionB_${questionCount}">Pilihan B</label>
-                            <input type="text" id="optionB_${questionCount}" name="questions[${questionCount}][options][B]" class="form-control" placeholder="Masukkan pilihan B">
-                        </div>
-                        <div class="choice">
-                            <label for="optionC_${questionCount}">Pilihan C</label>
-                            <input type="text" id="optionC_${questionCount}" name="questions[${questionCount}][options][C]" class="form-control" placeholder="Masukkan pilihan C">
-                        </div>
-                        <div class="choice">
-                            <label for="optionD_${questionCount}">Pilihan D</label>
-                            <input type="text" id="optionD_${questionCount}" name="questions[${questionCount}][options][D]" class="form-control" placeholder="Masukkan pilihan D">
-                        </div>
-                        <div class="choice">
-                            <label for="optionE_${questionCount}">Pilihan E</label>
-                            <input type="text" id="optionE_${questionCount}" name="questions[${questionCount}][options][E]" class="form-control" placeholder="Masukkan pilihan E">
-                        </div>
-                        <div class="choice">
-                            <label for="correct_answer_${questionCount}">Jawaban Benar</label>
-                            <select id="correct_answer_${questionCount}" name="questions[${questionCount}][correct_answer]" class="form-control">
-                                <option value="A">Pilihan A</option>
-                                <option value="B">Pilihan B</option>
-                                <option value="C">Pilihan C</option>
-                                <option value="D">Pilihan D</option>
-                                <option value="E">Pilihan E</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
+                                        <div class="choices-grid">
+                                            <div class="choice">
+                                                <label for="optionA_1">Pilihan A</label>
+                                                <input type="text" id="optionA_1" name="questions[1][options][A]"
+                                                    class="form-control" placeholder="Masukkan pilihan A">
+                                            </div>
+                                            <div class="choice">
+                                                <label for="optionD_1">Pilihan D</label>
+                                                <input type="text" id="optionD_1" name="questions[1][options][D]"
+                                                    class="form-control" placeholder="Masukkan pilihan D">
+                                            </div>
+                                            <div class="choice">
+                                                <label for="optionB_1">Pilihan B</label>
+                                                <input type="text" id="optionB_1" name="questions[1][options][B]"
+                                                    class="form-control" placeholder="Masukkan pilihan B">
+                                            </div>
+                                            <div class="choice">
+                                                <label for="optionE_1">Pilihan E</label>
+                                                <input type="text" id="optionE_1" name="questions[1][options][E]"
+                                                    class="form-control" placeholder="Masukkan pilihan E">
+                                            </div>
+
+                                            <div class="choice">
+                                                <label for="optionC_1">Pilihan C</label>
+                                                <input type="text" id="optionC_1" name="questions[1][options][C]"
+                                                    class="form-control" placeholder="Masukkan pilihan C">
+                                            </div>
+                                            <div class="choice">
+                                                <label for="correct_answer_1">Jawaban Benar</label>
+                                                <select id="correct_answer_1" name="questions[1][correct_answer]"
+                                                    class="form-control">
+                                                    <option>Pilihan Jawaban</option>
+                                                    <option value="A">Pilihan A</option>
+                                                    <option value="B">Pilihan B</option>
+                                                    <option value="C">Pilihan C</option>
+                                                    <option value="D">Pilihan D</option>
+                                                    <option value="E">Pilihan E</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
             </div>
         `;
         // Menambahkan form baru ke dalam list
