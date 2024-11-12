1f16f536ea4cddce34adb03f73990bb357032109
Bayu <bayurz1001@gmail.com>
2024-11-12 19:04:23 +0700

    up

--- app/Models/Pertanyaan.php
+++ app/Models/Pertanyaan.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Tugas;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Pertanyaan extends Model
 {
