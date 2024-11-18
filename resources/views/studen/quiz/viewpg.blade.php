b9455d895908dadc245229480db1092c9071f00c
Bayu <bayurz1001@gmail.com>
2024-11-18 18:38:45 +0700

    up

--- resources/views/studen/quiz/viewpg.blade.php
+++ resources/views/studen/quiz/viewpg.blade.php
@@ -94,7 +94,7 @@
                             </div>
 
                             <div class="col-lg-4">
-                                <div class="card ">
+                                <div class="card">
                                     <div class="card-header">
                                         <strong>Navigasi Soal</strong>
                                     </div>
@@ -102,18 +102,18 @@
                                         <div class="d-flex justify-content flex-wrap gap-3 px-3">
                                             @foreach ($allQuestions as $index => $question)
                                                 <a href="{{ route('view_pg', ['id_tugas' => $tugas->id_tugas, 'current_question_number' => $index + 1]) }}"
-                                                    class="btn btn-sm rounded {{ $currentQuestionNumber == $index + 1 ? 'text-white' : 'text-dark' }}"
-                                                    style="background-color: {{ $currentQuestionNumber == $index + 1 ? '#319A58' : '#E0E0E0' }}; 
-                                                          margin-top: 8px; margin-bottom: 8px; box-shadow: none;">
+                                                    class="btn btn-sm rounded {{ $currentQuestionNumber == $index + 1 ? 'text-white border-3' : 'text-dark' }}"
+                                                    style="background-color: {{ $currentQuestionNumber == $index + 1 ? 'transparent' : '#E0E0E0' }}; 
+                                                         border-color: {{ $currentQuestionNumber == $index + 1 ? '#319A58' : '#E0E0E0' }}; 
+                                                         margin-top: 8px; margin-bottom: 8px; box-shadow: none;">
                                                     {{ $index + 1 }}
                                                 </a>
                                             @endforeach
                                         </div>
-
                                     </div>
-
                                 </div>
                             </div>
+
 
                         </div>
 
@@ -147,12 +147,12 @@
                                 <p>${data.currentQuestion.isi_pertanyaan}</p>
                                 <ul class="list-unstyled">
                                     ${data.options.map((option, index) => ` 
-                                                                <li> 
-                                                                    <label>
-                                                                        <span class="option-label">${String.fromCharCode(65 + index)}. ${option.isi_pilihan}</span>
-                                                                    </label>
-                                                                </li>
-                                                                `).join('')}
+                                                                    <li> 
+                                                                        <label>
+                                                                            <span class="option-label">${String.fromCharCode(65 + index)}. ${option.isi_pilihan}</span>
+                                                                        </label>
+                                                                    </li>
+                                                                    `).join('')}
                                 </ul>
                             </div>
                         </div>
