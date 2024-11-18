098ffe1db9c279a9fc12497be2a3b7ee8153bfa1
Bayu <bayurz1001@gmail.com>
2024-11-18 15:38:57 +0700

    up

--- resources/views/studen/quiz/viewpg.blade.php
+++ resources/views/studen/quiz/viewpg.blade.php
@@ -48,7 +48,7 @@
 
                         <!-- Question and Answer Section -->
                         <div class="row mt-4">
-                            <div class="col-lg-8" id="question-container">
+                            <div class="col-lg-9" id="question-container">
                                 <div class="card">
                                     <div class="card-header">
                                         <strong>Soal No. {{ $currentQuestionNumber }}</strong>
@@ -72,7 +72,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-lg-4">
+                            <div class="col-lg-3">
                                 <div class="card">
                                     <div class="card-header">
                                         <strong>Navigasi Soal</strong>
@@ -150,14 +150,14 @@
                                 <p>${data.currentQuestion.isi_pertanyaan}</p>
                                 <ul class="list-unstyled">
                                     ${data.options.map((option, index) => `
-                                                                                                                                                            <li>
-                                                                                                                                                                <label>
-                                                                                                                                                                    <span class="option-label">
-                                                                                                                                                                        ${String.fromCharCode(65 + index)}. ${option.isi_pilihan}
-                                                                                                                                                                    </span>
-                                                                                                                                                                </label>
-                                                                                                                                                            </li>
-                                                                                                                                                        `).join('')}
+                                                                                                                                                                <li>
+                                                                                                                                                                    <label>
+                                                                                                                                                                        <span class="option-label">
+                                                                                                                                                                            ${String.fromCharCode(65 + index)}. ${option.isi_pilihan}
+                                                                                                                                                                        </span>
+                                                                                                                                                                    </label>
+                                                                                                                                                                </li>
+                                                                                                                                                            `).join('')}
                                 </ul>
                             </div>
                         </div>
