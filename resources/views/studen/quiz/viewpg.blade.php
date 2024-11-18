a1201619bf9e9bf81e4d6a31cecd44d5986709ec
Bayu <bayurz1001@gmail.com>
2024-11-18 15:22:41 +0700

    up

--- resources/views/studen/quiz/viewpg.blade.php
+++ resources/views/studen/quiz/viewpg.blade.php
@@ -130,12 +130,12 @@
                         <!-- Navigation Buttons -->
                         @if ($totalQuestions > 1)
                             <div class="mt-4 text-center">
-                                <a href="{{ route('instruktur_view_pg', ['id_tugas' => $tugas->id_tugas, 'current_question_number' => max($currentQuestionNumber - 1, 1)]) }}"
+                                <a href="{{ route('view_pg', ['id_tugas' => $tugas->id_tugas, 'current_question_number' => max($currentQuestionNumber - 1, 1)]) }}"
                                     class="text-primary px-2 fs-4">
                                     &laquo;&laquo;
                                 </a>
                                 <span>No {{ $currentQuestionNumber }} Dari {{ $totalQuestions }} Soal</span>
-                                <a href="{{ route('instruktur_view_pg', ['id_tugas' => $tugas->id_tugas, 'current_question_number' => min($currentQuestionNumber + 1, $totalQuestions)]) }}"
+                                <a href="{{ route('view_pg', ['id_tugas' => $tugas->id_tugas, 'current_question_number' => min($currentQuestionNumber + 1, $totalQuestions)]) }}"
                                     class="text-primary px-2 fs-4">
                                     &raquo;&raquo;
                                 </a>
@@ -173,14 +173,14 @@
                                 <p>${data.currentQuestion.isi_pertanyaan}</p>
                                 <ul class="list-unstyled">
                                     ${data.options.map((option, index) => `
-                                                                                                                <li>
-                                                                                                                    <label>
-                                                                                                                        <span class="option-label">
-                                                                                                                            ${String.fromCharCode(65 + index)}. ${option.isi_pilihan}
-                                                                                                                        </span>
-                                                                                                                    </label>
-                                                                                                                </li>
-                                                                                                            `).join('')}
+                                                                                                                    <li>
+                                                                                                                        <label>
+                                                                                                                            <span class="option-label">
+                                                                                                                                ${String.fromCharCode(65 + index)}. ${option.isi_pilihan}
+                                                                                                                            </span>
+                                                                                                                        </label>
+                                                                                                                    </li>
+                                                                                                                `).join('')}
                                 </ul>
                             </div>
                         </div>
