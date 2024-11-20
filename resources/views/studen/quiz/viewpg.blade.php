9f6031b1eae51dabba6d9e62bf4a71766e23a920
Bayu <bayurz1001@gmail.com>
2024-11-20 16:29:33 +0700

    up

--- resources/views/studen/quiz/viewpg.blade.php
+++ resources/views/studen/quiz/viewpg.blade.php
@@ -68,11 +68,6 @@
                                                 margin-right: 10px;
                                                 /* Jarak antara ikon dan teks */
                                             }
-
-                                            .timer-text {
-                                                margin-left: 10px;
-                                                /* Jarak tambahan jika dibutuhkan */
-                                            }
                                         </style>
 
 
@@ -167,15 +162,15 @@
                         <p>${data.currentQuestion.isi_pertanyaan}</p>
                         <ul class="list-unstyled">
                             ${data.options.map((option, index) => `
-                                                                                                                                                                    <li>
-                                                                                                                                                                        <label>
-                                                                                                                                                                            <input type="radio" name="answer_${data.currentQuestion.id}"
-                                                                                                                                                                                value="${option.id_pilihan}" class="me-2"
-                                                                                                                                                                                onchange="handleAnswerChange('${id_tugas}', '${data.currentQuestion.id_pertanyaan}', '${option.id_pilihan}')">
-                                                                                                                                                                            <span class="option-label">${String.fromCharCode(65 + index)}. ${option.isi_pilihan}</span>
-                                                                                                                                                                        </label>
-                                                                                                                                                                    </li>
-                                                                                                                                                                `).join('')}
+                                                                                                                                                                        <li>
+                                                                                                                                                                            <label>
+                                                                                                                                                                                <input type="radio" name="answer_${data.currentQuestion.id}"
+                                                                                                                                                                                    value="${option.id_pilihan}" class="me-2"
+                                                                                                                                                                                    onchange="handleAnswerChange('${id_tugas}', '${data.currentQuestion.id_pertanyaan}', '${option.id_pilihan}')">
+                                                                                                                                                                                <span class="option-label">${String.fromCharCode(65 + index)}. ${option.isi_pilihan}</span>
+                                                                                                                                                                            </label>
+                                                                                                                                                                        </li>
+                                                                                                                                                                    `).join('')}
                         </ul>
                     </div>
                 </div>
