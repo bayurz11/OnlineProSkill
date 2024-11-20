2b94cd1f2f93bda5ad5a5f384144b788233f01c9
Bayu <bayurz1001@gmail.com>
2024-11-20 16:24:40 +0700

    up

--- resources/views/studen/quiz/viewpg.blade.php
+++ resources/views/studen/quiz/viewpg.blade.php
@@ -57,10 +57,15 @@
                                         @else
                                             <strong>Quiz Selesai</strong>
                                         @endif
-                                        <div class="d-flex align-items-center">
-                                            <i class="fas fa-clock mr-3 text-primary"></i>
-                                            <span id="timer-text" class="ml-2">00:00</span>
+                                        <div class="card bg-green-500 p-6">
+                                            <div class="d-flex align-items-center">
+                                                <i class="fas fa-clock text-white mr-5"></i>
+                                                <!-- Menambah jarak yang lebih besar -->
+                                                <span id="timer-text" class="text-white ml-3">00:00</span>
+                                                <!-- Menambah jarak di kiri -->
+                                            </div>
                                         </div>
+
                                     </div>
 
 
@@ -152,15 +157,15 @@
                         <p>${data.currentQuestion.isi_pertanyaan}</p>
                         <ul class="list-unstyled">
                             ${data.options.map((option, index) => `
-                                                                                                                                                <li>
-                                                                                                                                                    <label>
-                                                                                                                                                        <input type="radio" name="answer_${data.currentQuestion.id}"
-                                                                                                                                                            value="${option.id_pilihan}" class="me-2"
-                                                                                                                                                            onchange="handleAnswerChange('${id_tugas}', '${data.currentQuestion.id_pertanyaan}', '${option.id_pilihan}')">
-                                                                                                                                                        <span class="option-label">${String.fromCharCode(65 + index)}. ${option.isi_pilihan}</span>
-                                                                                                                                                    </label>
-                                                                                                                                                </li>
-                                                                                                                                            `).join('')}
+                                                                                                                                                    <li>
+                                                                                                                                                        <label>
+                                                                                                                                                            <input type="radio" name="answer_${data.currentQuestion.id}"
+                                                                                                                                                                value="${option.id_pilihan}" class="me-2"
+                                                                                                                                                                onchange="handleAnswerChange('${id_tugas}', '${data.currentQuestion.id_pertanyaan}', '${option.id_pilihan}')">
+                                                                                                                                                            <span class="option-label">${String.fromCharCode(65 + index)}. ${option.isi_pilihan}</span>
+                                                                                                                                                        </label>
+                                                                                                                                                    </li>
+                                                                                                                                                `).join('')}
                         </ul>
                     </div>
                 </div>
