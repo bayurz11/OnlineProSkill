0a1f724ea95b952532952caebe53765abf877513
Bayu <bayurz1001@gmail.com>
2024-11-22 09:57:52 +0700

    up

--- resources/views/studen/quiz/viewpg.blade.php
+++ resources/views/studen/quiz/viewpg.blade.php
@@ -74,9 +74,9 @@
                                             <ul class="list-unstyled">
                                                 @foreach ($currentQuestion->pilihanJawaban as $index => $option)
                                                     <li>
-                                                        <label for="option_{{ $option->id }}">
+                                                        <label for="option_{{ $option->id_pilihan }}">
                                                             <input type="radio" name="answer_{{ $currentQuestion->id }}"
-                                                                value="{{ $option->id }}" class="me-2"
+                                                                value="{{ $option->id_pilihan }}" class="me-2"
                                                                 onchange="handleAnswerChange('{{ $tugas->id_tugas }}', '{{ $currentQuestion->id_pertanyaan }}', '{{ auth()->user()->id }}', '{{ $option->id_pilihan }}', this.value, '')"
                                                                 @if ($currentQuestion->jawaban->first() && $currentQuestion->jawaban->first()->pilihan_id == $option->id) checked @endif />
                                                             <span class="option-label">{{ chr(65 + $index) }}.
@@ -154,15 +154,15 @@
                         <p>${data.currentQuestion.isi_pertanyaan}</p>
                         <ul class="list-unstyled">
                             ${data.options.map((option, index) => `
-                                                                                                                                                                                                            <li>
-                                                                                                                                                                                                                <label>
-                                                                                                                                                                                                                    <input type="radio" name="answer_${data.currentQuestion.id}"
-                                                                                                                                                                                                                        value="${option.id_pilihan}" class="me-2"
-                                                                                                                                                                                                                        onchange="handleAnswerChange('${id_tugas}', '${data.currentQuestion.id_pertanyaan}', '${option.id_pilihan}')">
-                                                                                                                                                                                                                    <span class="option-label">${String.fromCharCode(65 + index)}. ${option.isi_pilihan}</span>
-                                                                                                                                                                                                                </label>
-                                                                                                                                                                                                            </li>
-                                                                                                                                                                                                        `).join('')}
+                                                                                                                                                                                                                <li>
+                                                                                                                                                                                                                    <label>
+                                                                                                                                                                                                                        <input type="radio" name="answer_${data.currentQuestion.id}"
+                                                                                                                                                                                                                            value="${option.id_pilihan}" class="me-2"
+                                                                                                                                                                                                                            onchange="handleAnswerChange('${id_tugas}', '${data.currentQuestion.id_pertanyaan}', '${option.id_pilihan}')">
+                                                                                                                                                                                                                        <span class="option-label">${String.fromCharCode(65 + index)}. ${option.isi_pilihan}</span>
+                                                                                                                                                                                                                    </label>
+                                                                                                                                                                                                                </li>
+                                                                                                                                                                                                            `).join('')}
                         </ul>
                     </div>
                 </div>
