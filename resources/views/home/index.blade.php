19e7b276be0ea3ffb3bf7eed209218ebe45334b1
Bayu <bayurz1001@gmail.com>
2024-11-26 14:10:45 +0700

    up

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -54,7 +54,8 @@
                                 <img src="{{ asset('public/assets/img/others/1.png') }}" alt="img">
                             </div>
 
-                            <div class="banner__student" data-aos="fade-left" data-aos-delay="200" style="margin-top: -50px;">
+                            <div class="banner__student" data-aos="fade-left" data-aos-delay="200"
+                                style="margin-top: -50px; margin-left: 10px;">
                                 <div class="icon">
                                     <img src="{{ asset('public/assets/img/banner/h2_banner_icon.svg') }}" alt="img"
                                         class="injectable">
