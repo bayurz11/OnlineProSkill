e275cb2c9ad5bb850284aa4d0002bbe597d94cb9
Bayu <bayurz1001@gmail.com>
2024-11-26 14:59:13 +0700

    up

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -50,7 +50,7 @@
                             <span class="svg-icon" id="banner-svg"
                                 data-svg-icon="{{ asset('public/assets/img/banner/h2_banner_shape02.svg') }}"></span>
                             <div class="about__enrolled" data-aos="fade-right" data-aos-delay="200"
-                                style="margin-left: -40px; margin-top: -40px;">
+                                style="margin-left: -40px; margin-top: -60px;">
                                 <p class="title"><span>{{ $sertifikat->count() }}</span> Sertifikat</p>
                                 <img src="{{ asset('public/assets/img/others/1.png') }}" alt="img">
                             </div>
