f989f69571bdd8160ca7840d3a02266cd9dec3ef
Bayu <bayurz1001@gmail.com>
2024-11-28 14:20:59 +0700

    up

--- app/Http/Controllers/InstrukturQuestionController.php
+++ app/Http/Controllers/InstrukturQuestionController.php
@@ -248,16 +248,7 @@ class InstrukturQuestionController extends Controller
                 ->count();
         }
 
-        $nilaiSiswa = Jawaban_Siswa::select('id_siswa')
-            ->selectRaw('SUM(CASE WHEN nilai = 1.00 THEN 1 ELSE 0 END) as benar')
-            ->selectRaw('SUM(CASE WHEN nilai = 0.00 THEN 1 ELSE 0 END) as salah')
-            ->selectRaw('SUM(CASE WHEN nilai IS NULL THEN 1 ELSE 0 END) as tidak_dijawab')
-            // Menambahkan join dengan tabel Pertanyaan dan Tugas
-            ->join('pertanyaan', 'jawaban_siswa.id_pertanyaan', '=', 'pertanyaan.id_pertanyaan')
-            ->join('tugas', 'pertanyaan.id_tugas', '=', 'tugas.id_tugas')
-            ->groupBy('id_siswa')
-            ->with('siswa') // Asosiasi dengan model Siswa untuk mendapatkan nama siswa
-            ->get();
+        $nilaiSiswa = Pertanyaan::all();
 
 
         return view('instruktur.Quiz.viewpg', compact(
