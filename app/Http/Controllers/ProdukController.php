469bdf89057d99eb6b3a4165fce9dba084fd3215
Bayu <bayurz1001@gmail.com>
2024-11-29 20:12:26 +0700

    up

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -2,9 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+use App\Models\Blog;
+use App\Models\Order;
+use App\Models\AdminEvent;
+use App\Models\Categories;
+use App\Models\Sertifikat;
+use App\Models\UserProfile;
 use Illuminate\Http\Request;
+use App\Models\KelasTatapMuka;
+use App\Models\NotifikasiUser;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class ProdukController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = Auth::user();
+        $profile = $user ? UserProfile::where('user_id', $user->id)->first() : null;
+        $cart = Session::get('cart', []);
+        $categori = Categories::all();
+
+        // Mengambil daftar course_type unik dari KelasTatapMuka, kecuali 'bootcamp'
+        $courseTypes = KelasTatapMuka::distinct()
+            ->where('course_type', '!=', 'bootcamp')
+            ->pluck('course_type');
+
+        // Mengambil KelasTatapMuka yang aktif dengan filter berdasarkan status dan mengabaikan 'bootcamp'
+        $KelasTatapMuka = KelasTatapMuka::where('status', 1)
+            ->where('course_type', '!=', 'bootcamp')
+            ->orderBy('created_at', 'asc')
+            ->get();
+
+        // Data tambahan
+        $blog = Blog::latest()->take(4)->get();
+        $event = AdminEvent::where('tgl', '>=', Carbon::now())->latest()->take(3)->get();
+        $daftar_siswa = UserProfile::where('role_id', 3)->get();
+        $sertifikat = Sertifikat::whereIn('kategori_id', [13, 14])->get();
+
+        // Notifikasi
+        $notifikasi = $user ? NotifikasiUser::where('user_id', $user->id)->latest()->get() : collect();
+        $notifikasiCount = $notifikasi->where('status', 1)->count();
+
+        // Joined courses
+        $joinedCourses = $user ? Order::where('user_id', $user->id)->pluck('product_id')->toArray() : [];
+
+        return view('home.produk.index', compact(
+            'user',
+            'profile',
+            'joinedCourses',
+            'cart',
+            'notifikasiCount',
+            'notifikasi',
+            'categori',
+            'KelasTatapMuka',
+            'event',
+            'blog',
+            'daftar_siswa',
+            'sertifikat',
+            'courseTypes'
+        ));
+    }
 }
---
+++ resources/views/home/produk/index.blade.php
@@ -0,0 +1,41 @@
+@section('title', 'ProSkill Akademia | Produk')
+<?php $page = 'index'; ?>
+
+@extends('layout.mainlayout')
+
+@section('content')
+
+    <!-- breadcrumb-area -->
+    <div class="breadcrumb__area breadcrumb__bg breadcrumb__bg-two"
+        data-background="{{ asset('public/assets/img/bg/breadcrumb_bg.jpg') }}">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="breadcrumb__content">
+                        <h3 class="title">Produk</h3>
+                        <nav class="breadcrumb">
+                            <span property="itemListElement" typeof="ListItem">
+                                <a href="{{ route('/') }}">Beranda</a>
+                            </span>
+                            <span class="breadcrumb-separator"><i class="fas fa-angle-right"></i></span>
+                            <span property="itemListElement" typeof="ListItem">Produk</span>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="breadcrumb__shape-wrap">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape01.svg') }}" alt="img" class="alltuchtopdown">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape02.svg') }}" alt="img" data-aos="fade-right"
+                data-aos-delay="300">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape03.svg') }}" alt="img" data-aos="fade-up"
+                data-aos-delay="400">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape04.svg') }}" alt="img"
+                data-aos="fade-down-left" data-aos-delay="400">
+            <img src="{{ asset('public/assets/img/others/breadcrumb_shape05.svg') }}" alt="img" data-aos="fade-left"
+                data-aos-delay="400">
+        </div>
+    </div>
+    <!-- breadcrumb-area-end -->
+
+@endsection
