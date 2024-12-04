0787cd6806adddb50dde65e11c27dd1cf60612ce
Bayu <bayurz1001@gmail.com>
2024-12-04 15:48:34 +0700

    up

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use App\Models\Blog;
 use App\Models\Order;
+use App\Models\Reviews;
+use App\Models\Kurikulum;
 use App\Models\AdminEvent;
 use App\Models\Categories;
 use App\Models\Sertifikat;
@@ -12,59 +14,131 @@ use App\Models\UserProfile;
 use Illuminate\Http\Request;
 use App\Models\KelasTatapMuka;
 use App\Models\NotifikasiUser;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
 class ProdukController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $user = Auth::user();
-        $profile = $user ? UserProfile::where('user_id', $user->id)->first() : null;
         $cart = Session::get('cart', []);
         $categori = Categories::all();
+        $profile = $user ? UserProfile::where('user_id', $user->id)->first() : null;
+        $category_ids = $request->input('categories', []);
+        $tingkatLevels = KelasTatapMuka::where('course_type', '!=', 'bootcamp')
+            ->distinct()
+            ->pluck('tingkat');
 
-        // Mengambil daftar course_type unik dari KelasTatapMuka, kecuali 'bootcamp'
-        $courseTypes = KelasTatapMuka::distinct()
-            ->where('course_type', '!=', 'bootcamp')
-            ->pluck('course_type');
+        // Ambil semua course_id yang ada di model Kurikulum
+        $kurikulumCourseIds = Kurikulum::pluck('course_id')->toArray();
 
-        // Mengambil KelasTatapMuka yang aktif dengan filter berdasarkan status dan mengabaikan 'bootcamp'
-        $KelasTatapMuka = KelasTatapMuka::where('status', 1)
+        // Menghitung jumlah kursus per kategori yang ada di Kurikulum
+        $categoryCounts = KelasTatapMuka::select('kategori_id', DB::raw('count(*) as total'))
+            ->where('status', 1)
             ->where('course_type', '!=', 'bootcamp')
-            ->orderBy('created_at', 'asc')
+            ->whereIn('id', $kurikulumCourseIds)
+            ->groupBy('kategori_id')
+            ->pluck('total', 'kategori_id');
+
+        // Menghitung jumlah kursus per tingkat yang ada di Kurikulum
+        $tingkatCounts = KelasTatapMuka::select('tingkat', DB::raw('count(*) as total'))
+            ->where('status', 1)
+            ->where('course_type', '!=', 'bootcamp')
+            ->whereIn('id', $kurikulumCourseIds)
+            ->groupBy('tingkat')
+            ->pluck('total', 'tingkat');
+
+        // Hitung jumlah rating untuk setiap kursus
+        $ratingCounts = Reviews::select('class_id', DB::raw('count(*) as total'))
+            ->groupBy('class_id')
+            ->pluck('total', 'class_id');
+
+        // Pastikan category_ids adalah array
+        if (!is_array($category_ids)) {
+            $category_ids = explode(',', $category_ids);
+        }
+        $category_ids = array_filter($category_ids);
+
+        $search_term = $request->input('search_term');
+        $orderby = $request->input('orderby', 'latest');
+        $selectedTingkat = $request->input('tingkat', []);
+
+        // Pastikan selectedTingkat adalah array
+        if (!is_array($selectedTingkat)) {
+            $selectedTingkat = explode(',', $selectedTingkat);
+        }
+        $selectedTingkat = array_filter($selectedTingkat);
+
+        // Dapatkan course_type dari request
+        $courseType = $request->input('course_type', []);
+        if (!is_array($courseType)) {
+            $courseType = explode(',', $courseType);
+        }
+        $courseType = array_filter($courseType);
+
+        // Mencari berdasarkan kategori, tingkat, course_type, dan term pencarian dengan pagination
+        $results = KelasTatapMuka::query()
+            ->where('status', 1)
+            ->whereNotIn('course_type', ['bootcamp'])
+            ->when(!empty($category_ids), function ($query) use ($category_ids) {
+                return $query->whereIn('kategori_id', $category_ids);
+            })
+            ->when($search_term, function ($query, $search_term) {
+                return $query->where('nama_kursus', 'like', '%' . $search_term . '%')
+                    ->orWhere('tag', 'like', '%' . $search_term . '%');
+            })
+            ->when(!empty($selectedTingkat), function ($query) use ($selectedTingkat) {
+                return $query->whereIn('tingkat', $selectedTingkat);
+            })
+            ->when(!empty($courseType), function ($query) use ($courseType) {
+                return $query->whereIn('course_type', $courseType);
+            })
+
+            ->when($orderby, function ($query, $orderby) {
+                if ($orderby == 'latest') {
+                    return $query->orderBy('created_at', 'desc');
+                } elseif ($orderby == 'oldest') {
+                    return $query->orderBy('created_at', 'asc');
+                } elseif ($orderby == 'highest_price') {
+                    return $query->orderBy('price', 'desc');
+                } elseif ($orderby == 'lowest_price') {
+                    return $query->orderBy('price', 'asc');
+                }
+            })
+            ->whereIn('id', $kurikulumCourseIds)
+            ->paginate(6);
+
+        // Ambil notifikasi untuk pengguna yang sedang login
+        $notifikasi = $user ? NotifikasiUser::where('user_id', $user->id)
+            ->orderBy('created_at', 'desc')
+            ->get() : collect();
+
+        $course = KelasTatapMuka::with('user')
+            ->where('status', 1)
+            ->whereIn('course_type', ['offline', 'online'])
+            ->whereNotIn('course_type', ['bootcamp'])
             ->get();
+        $count = $course->count();
 
-        // Data tambahan
-        $blog = Blog::latest()->take(4)->get();
-        $event = AdminEvent::where('tgl', '>=', Carbon::now())->latest()->take(3)->get();
-        $daftar_siswa = UserProfile::where('role_id', 3)->get();
-        $sertifikat = Sertifikat::whereIn('kategori_id', [13, 14])->get();
-
-        // Notifikasi
-        $notifikasi = $user ? NotifikasiUser::where('user_id', $user->id)->latest()->get() : collect();
+        // Hitung jumlah notifikasi dengan status = 1
         $notifikasiCount = $notifikasi->where('status', 1)->count();
 
-        // Joined courses
+        // Ambil jumlah pendaftaran untuk setiap kursus
+        $jumlahPendaftaran = Order::select('product_id', DB::raw('count(*) as total'))
+            ->groupBy('product_id')
+            ->pluck('total', 'product_id');
+
+        // Ambil ID kursus yang telah diikuti oleh user
         $joinedCourses = $user ? Order::where('user_id', $user->id)->pluck('product_id')->toArray() : [];
 
-        return view('home.produk.index', compact(
-            'user',
-            'profile',
-            'joinedCourses',
-            'cart',
-            'notifikasiCount',
-            'notifikasi',
-            'categori',
-            'KelasTatapMuka',
-            'event',
-            'blog',
-            'daftar_siswa',
-            'sertifikat',
-            'courseTypes'
-        ));
+        return view('home.produk.index', compact('results', 'cart', 'notifikasi', 'notifikasiCount', 'user', 'profile', 'jumlahPendaftaran', 'joinedCourses', 'course', 'categoryCounts', 'category_ids', 'tingkatLevels', 'tingkatCounts', 'categori', 'ratingCounts'))
+            ->with('paginationView', 'vendor.custom');
     }
+
+
     public function detail()
     {
         $user = Auth::user();
