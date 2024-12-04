3b1f89e8bd9e59a1e0c3140ab1293c9a45f033fe
Bayu <bayurz1001@gmail.com>
2024-12-04 15:11:32 +0700

    up

--- resources/views/admin/modal/add_produk.blade.php
+++ resources/views/admin/modal/add_produk.blade.php
@@ -39,7 +39,7 @@
                             <option value="">Pilih Kategori</option>
                             @foreach ($categori as $category)
                                 @if ($category->status == 1)
-                                    <option value="{{ $category->id }}">{{ $category->name_kategori }}</option>
+                                    <option value="{{ $category->id }}">{{ $category->name_category }}</option>
                                 @endif
                             @endforeach
                         </select>
