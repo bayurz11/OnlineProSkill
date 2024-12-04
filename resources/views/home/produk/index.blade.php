3f4f3ef500d8dd10e9c6cb538dc84fc7b3d7cf49
Bayu <bayurz1001@gmail.com>
2024-12-04 16:04:29 +0700

    up

--- resources/views/home/produk/index.blade.php
+++ resources/views/home/produk/index.blade.php
@@ -79,8 +79,7 @@
                                     <div class="shop-item">
                                         <div class="shop-thumb">
                                             <a href="shop-details.html">
-                                                <img src="{{ asset('public/assets/img/shop/shop_img01.jpg') }}"
-                                                    alt="img">
+                                                <img src="{{ asset('public/uploads/' . $cours->gambar) }}" alt="img">
                                             </a>
                                             <span class="flash-sale">Sale</span>
                                             <ul class="list-wrap shop-action">
