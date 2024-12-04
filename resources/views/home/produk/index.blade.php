caf301fe2da315965236dbc57e6d6171f3086fee
Bayu <bayurz1001@gmail.com>
2024-12-04 16:21:22 +0700

    up

--- resources/views/home/produk/index.blade.php
+++ resources/views/home/produk/index.blade.php
@@ -81,7 +81,9 @@
                                             <a href="shop-details.html">
                                                 <img src="{{ asset('public/uploads/' . $cours->gambar) }}" alt="img">
                                             </a>
-                                            <span class="flash-sale">{{ $cours->discount }}%</span>
+                                            @if (!empty($cours->discount))
+                                                <span class="flash-sale">{{ $cours->discount }}%</span>
+                                            @endif
                                             <ul class="list-wrap shop-action">
                                                 <li><a href="{{ route('produk-detail') }}"><i
                                                             class="fas fa-shopping-cart"></i></a></li>
