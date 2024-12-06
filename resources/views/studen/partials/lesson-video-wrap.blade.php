45a206fcbda986f8f4fd0dc0e1c419e8e6ef5aa3
Bayu <bayurz1001@gmail.com>
2024-12-06 14:56:31 +0700

    up

--- resources/views/studen/partials/lesson-video-wrap.blade.php
+++ resources/views/studen/partials/lesson-video-wrap.blade.php
@@ -23,10 +23,11 @@
 
     <button id="reviewButton" class="btn btn-primary me-2" style="display: none;">Review Course</button>
 
-    @if ($allSectionsCompleted && $hasReviewed)
+    @if ($allSectionsCompleted && $hasReviewed && $sertifikat)
         <form action="{{ route('print_certificate', ['id' => $sertifikat->id]) }}" method="POST">
             @csrf
             <button type="submit" class="btn btn-success">Cetak Sertifikat</button>
         </form>
     @endif
+
 </div>
