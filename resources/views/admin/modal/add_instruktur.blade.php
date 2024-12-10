7b87e24741aa743f6aa279fe3ce02f5b7a01d487
Bayu <bayurz1001@gmail.com>
2024-12-10 14:53:10 +0700

    up

--- resources/views/admin/modal/add_instruktur.blade.php
+++ resources/views/admin/modal/add_instruktur.blade.php
@@ -1,14 +1,39 @@
 <div class="modal fade" id="InstrukturModal" tabindex="-1" aria-labelledby="InstrukturModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <form id="articleForm" action="#" method="POST" enctype="multipart/form-data">
+            <form action="{{ route('regisInstruktur') }}" class="account__form" method="POST" id="regisInstruktur">
                 @csrf
                 <div class="modal-header">
-                    <h5 class="modal-title" id="InstrukturModalLabel">Tambah Artikel</h5>
+                    <h5 class="modal-title" id="InstrukturModalLabel">Tambah Instruktur Baru</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="btn-close"></button>
                 </div>
                 <div class="modal-body">
 
+                    <div class="form-grp">
+                        <input type="text" id="name" name="name" placeholder="Masukkan Nama Lengkap Anda">
+                    </div>
+                    <div class="form-grp">
+                        <input type="email" id="email" placeholder="Email" name="email">
+                    </div>
+                    <div class="form-grp">
+                        <input type="phone" id="phone_number" placeholder="08**********" name="phone_number"
+                            maxlength="12">
+                    </div>
+                    <div class="form-grp">
+                        <input type="password" id="password" placeholder="Password" name="password">
+                    </div>
+                    <div class="form-grp">
+                        <input type="password" id="password_confirmation" placeholder="Konfirmasi Password"
+                            name="password_confirmation">
+                    </div>
+                    <span>Password minimal 8 karakter terdiri simbol,
+                        huruf, dan angka</span>
+                    <button class="g-recaptcha btn btn-two arrow-btn"
+                        data-sitekey="{{ config('services.recaptcha_v3.siteKey') }}"
+                        data-callback="onSubmitregisInstruktur" data-action='submit'>Daftar
+                        <img src="{{ asset('public/assets/img/icons/right_arrow.svg') }}" alt="img"
+                            class="injectable">
+                    </button>
 
                 </div>
 
