4d42f0e38e0c88cecc14c9c533ae6e55ea0bfbd0
Bayu <bayurz1001@gmail.com>
2024-12-11 16:16:58 +0700

    up

--- resources/views/admin/quiz/pertanyaan.blade.php
+++ resources/views/admin/quiz/pertanyaan.blade.php
@@ -8,12 +8,12 @@
         <nav class="page-breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Facebook Setting</li>
+                <li class="breadcrumb-item active" aria-current="page">Pertanyaan Pilihan Ganda</li>
             </ol>
         </nav>
 
         <div class="row">
-            <div class="col-lg-9">
+            <div class="col-lg-12">
                 <!-- Combined Form to Add New Quiz and Questions -->
                 <div class="dashboard__content-wrap">
                     <div class="dashboard__content-title d-flex justify-content-between align-items-center">
