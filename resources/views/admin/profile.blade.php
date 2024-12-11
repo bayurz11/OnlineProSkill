a7c93fcb3f18dcf31ee5b43128d70a5d33247291
Bayu <bayurz1001@gmail.com>
2024-12-11 15:05:45 +0700

    up

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -2,187 +2,65 @@
 <?php $page = 'profil_admin'; ?>
 
 @extends('layout.mainlayout_admin')
+
 @section('content')
+    <div class="container mt-4">
+        <div class="card">
+            <div class="card-header bg-primary text-white">
+                <h4 class="mb-0">User Profile</h4>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('profile.update') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
 
+                    <div class="row mb-3">
+                        <label for="name" class="col-md-3 col-form-label">Name</label>
+                        <div class="col-md-9">
+                            <input type="text" name="name" id="name" class="form-control"
+                                value="{{ auth()->user()->name }}" required>
+                        </div>
+                    </div>
 
-    <!-- content @s -->
-    <div class="nk-content ">
-        <div class="container-fluid">
-            <div class="nk-content-inner">
-                <div class="nk-content-body">
-                    <div class="nk-block">
-                        <div class="card card-bordered">
-                            <div class="card-aside-wrap">
-                                <div class="card-inner card-inner-lg">
-                                    <div class="nk-block-head nk-block-head-lg">
-                                        <div class="nk-block-between">
-                                            <div class="nk-block-head-content">
-                                                <h4 class="nk-block-title">Personal Information</h4>
-                                                <div class="nk-block-des">
-                                                    <p>Basic info, like your name and address, that you use on Nio Platform.
-                                                    </p>
-                                                </div>
-                                            </div>
-                                            <div class="nk-block-head-content align-self-start d-lg-none">
-                                                <a href="#" class="toggle btn btn-icon btn-trigger mt-n1"
-                                                    data-target="userAside"><em class="icon ni ni-menu-alt-r"></em></a>
-                                            </div>
-                                        </div>
-                                    </div><!-- .nk-block-head -->
-                                    <div class="nk-block">
-                                        <div class="nk-data data-list">
-                                            <div class="data-head">
-                                                <h6 class="overline-title">Basics</h6>
-                                            </div>
-                                            <div class="data-item" data-bs-toggle="modal" data-bs-target="#profile-edit">
-                                                <div class="data-col">
-                                                    <span class="data-label">Full Name</span>
-                                                    <span class="data-value">Abu Bin Ishtiyak</span>
-                                                </div>
-                                                <div class="data-col data-col-end"><span class="data-more"><em
-                                                            class="icon ni ni-forward-ios"></em></span></div>
-                                            </div><!-- data-item -->
-                                            <div class="data-item" data-bs-toggle="modal" data-bs-target="#profile-edit">
-                                                <div class="data-col">
-                                                    <span class="data-label">Display Name</span>
-                                                    <span class="data-value">Ishtiyak</span>
-                                                </div>
-                                                <div class="data-col data-col-end"><span class="data-more"><em
-                                                            class="icon ni ni-forward-ios"></em></span></div>
-                                            </div><!-- data-item -->
-                                            <div class="data-item">
-                                                <div class="data-col">
-                                                    <span class="data-label">Email</span>
-                                                    <span class="data-value">info@softnio.com</span>
-                                                </div>
-                                                <div class="data-col data-col-end"><span class="data-more disable"><em
-                                                            class="icon ni ni-lock-alt"></em></span></div>
-                                            </div><!-- data-item -->
-                                            <div class="data-item" data-bs-toggle="modal" data-bs-target="#profile-edit">
-                                                <div class="data-col">
-                                                    <span class="data-label">Phone Number</span>
-                                                    <span class="data-value text-soft">Not add yet</span>
-                                                </div>
-                                                <div class="data-col data-col-end"><span class="data-more"><em
-                                                            class="icon ni ni-forward-ios"></em></span></div>
-                                            </div><!-- data-item -->
-                                            <div class="data-item" data-bs-toggle="modal" data-bs-target="#profile-edit">
-                                                <div class="data-col">
-                                                    <span class="data-label">Date of Birth</span>
-                                                    <span class="data-value">29 Feb, 1986</span>
-                                                </div>
-                                                <div class="data-col data-col-end"><span class="data-more"><em
-                                                            class="icon ni ni-forward-ios"></em></span></div>
-                                            </div><!-- data-item -->
-                                            <div class="data-item" data-bs-toggle="modal" data-bs-target="#profile-edit"
-                                                data-tab-target="#address">
-                                                <div class="data-col">
-                                                    <span class="data-label">Address</span>
-                                                    <span class="data-value">2337 Kildeer Drive,<br>Kentucky, Canada</span>
-                                                </div>
-                                                <div class="data-col data-col-end"><span class="data-more"><em
-                                                            class="icon ni ni-forward-ios"></em></span></div>
-                                            </div><!-- data-item -->
-                                        </div><!-- data-list -->
-                                        <div class="nk-data data-list">
-                                            <div class="data-head">
-                                                <h6 class="overline-title">Preferences</h6>
-                                            </div>
-                                            <div class="data-item">
-                                                <div class="data-col">
-                                                    <span class="data-label">Language</span>
-                                                    <span class="data-value">English (United State)</span>
-                                                </div>
-                                                <div class="data-col data-col-end"><a href="#"
-                                                        class="link link-primary">Change Language</a></div>
-                                            </div><!-- data-item -->
-                                            <div class="data-item">
-                                                <div class="data-col">
-                                                    <span class="data-label">Date Format</span>
-                                                    <span class="data-value">M d, YYYY</span>
-                                                </div>
-                                                <div class="data-col data-col-end"><a href="#"
-                                                        class="link link-primary">Change</a></div>
-                                            </div><!-- data-item -->
-                                            <div class="data-item">
-                                                <div class="data-col">
-                                                    <span class="data-label">Timezone</span>
-                                                    <span class="data-value">Bangladesh (GMT +6)</span>
-                                                </div>
-                                                <div class="data-col data-col-end"><a href="#"
-                                                        class="link link-primary">Change</a></div>
-                                            </div><!-- data-item -->
-                                        </div><!-- data-list -->
-                                    </div><!-- .nk-block -->
-                                </div>
-                                <div class="card-aside card-aside-left user-aside toggle-slide toggle-slide-left toggle-break-lg"
-                                    data-toggle-body="true" data-content="userAside" data-toggle-screen="lg"
-                                    data-toggle-overlay="true">
-                                    <div class="card-inner-group" data-simplebar>
-                                        <div class="card-inner">
-                                            <div class="user-card">
-                                                <div class="user-avatar bg-primary">
-                                                    <span>AB</span>
-                                                </div>
-                                                <div class="user-info">
-                                                    <span class="lead-text">Abu Bin Ishtiyak</span>
-                                                    <span class="sub-text">info@softnio.com</span>
-                                                </div>
-                                                <div class="user-action">
-                                                    <div class="dropdown">
-                                                        <a class="btn btn-icon btn-trigger me-n2"
-                                                            data-bs-toggle="dropdown" href="#"><em
-                                                                class="icon ni ni-more-v"></em></a>
-                                                        <div class="dropdown-menu dropdown-menu-end">
-                                                            <ul class="link-list-opt no-bdr">
-                                                                <li><a href="#"><em
-                                                                            class="icon ni ni-camera-fill"></em><span>Change
-                                                                            Photo</span></a></li>
-                                                                <li><a href="#"><em
-                                                                            class="icon ni ni-edit-fill"></em><span>Update
-                                                                            Profile</span></a></li>
-                                                            </ul>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div><!-- .user-card -->
-                                        </div><!-- .card-inner -->
-                                        <div class="card-inner">
-                                            <div class="user-account-info py-0">
-                                                <h6 class="overline-title-alt">Nio Wallet Account</h6>
-                                                <div class="user-balance">12.395769 <small
-                                                        class="currency currency-btc">BTC</small></div>
-                                                <div class="user-balance-sub">Locked <span>0.344939 <span
-                                                            class="currency currency-btc">BTC</span></span></div>
-                                            </div>
-                                        </div><!-- .card-inner -->
-                                        <div class="card-inner p-0">
-                                            <ul class="link-list-menu">
-                                                <li><a class="active" href="html/user-profile-regular.html"><em
-                                                            class="icon ni ni-user-fill-c"></em><span>Personal
-                                                            Infomation</span></a></li>
-                                                <li><a href="html/user-profile-notification.html"><em
-                                                            class="icon ni ni-bell-fill"></em><span>Notifications</span></a>
-                                                </li>
-                                                <li><a href="html/user-profile-activity.html"><em
-                                                            class="icon ni ni-activity-round-fill"></em><span>Account
-                                                            Activity</span></a></li>
-                                                <li><a href="html/user-profile-setting.html"><em
-                                                            class="icon ni ni-lock-alt-fill"></em><span>Security
-                                                            Settings</span></a></li>
-                                                <li><a href="html/user-profile-social.html"><em
-                                                            class="icon ni ni-grid-add-fill-c"></em><span>Connected with
-                                                            Social</span></a></li>
-                                            </ul>
-                                        </div><!-- .card-inner -->
-                                    </div><!-- .card-inner-group -->
-                                </div><!-- card-aside -->
-                            </div><!-- .card-aside-wrap -->
-                        </div><!-- .card -->
-                    </div><!-- .nk-block -->
-                </div>
+                    <div class="row mb-3">
+                        <label for="email" class="col-md-3 col-form-label">Email</label>
+                        <div class="col-md-9">
+                            <input type="email" name="email" id="email" class="form-control"
+                                value="{{ auth()->user()->email }}" required>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label for="phone" class="col-md-3 col-form-label">Phone</label>
+                        <div class="col-md-9">
+                            <input type="text" name="phone" id="phone" class="form-control"
+                                value="{{ auth()->user()->phone }}">
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label for="profile_picture" class="col-md-3 col-form-label">Profile Picture</label>
+                        <div class="col-md-9">
+                            <input type="file" name="profile_picture" id="profile_picture" class="form-control">
+                            @if (auth()->user()->profile_picture)
+                                <img src="{{ asset('storage/' . auth()->user()->profile_picture) }}" alt="Profile Picture"
+                                    class="img-thumbnail mt-2" width="100">
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label for="address" class="col-md-3 col-form-label">Address</label>
+                        <div class="col-md-9">
+                            <textarea name="address" id="address" class="form-control" rows="3">{{ auth()->user()->address }}</textarea>
+                        </div>
+                    </div>
+
+                    <div class="text-end">
+                        <button type="submit" class="btn btn-success">Save Changes</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-    <!-- content @e -->
 @endsection
