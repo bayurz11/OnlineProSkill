10cc12156f8fafbaf1e0a487cc580417f105c521
Bayu <bayurz1001@gmail.com>
2024-12-11 20:57:30 +0700

    up

--- resources/views/home/modal/register.blade.php
+++ resources/views/home/modal/register.blade.php
@@ -27,14 +27,14 @@
                             <input type="password" id="password" placeholder="Password" name="password">
                             <i class="toggle-password bi bi-eye position-absolute"
                                 style="right: 15px; top: 50%; transform: translateY(-50%); cursor: pointer;"
-                                onclick="togglePasswordVisibilityregister1('password', this)"></i>
+                                onclick="togglePasswordVisibilityregis('password', this)"></i>
                         </div>
                         <div class="form-grp position-relative">
                             <input type="password" id="password_confirmation" placeholder="Konfirmasi Password"
                                 name="password_confirmation">
                             <i class="toggle-password bi bi-eye position-absolute"
                                 style="right: 15px; top: 50%; transform: translateY(-50%); cursor: pointer;"
-                                onclick="togglePasswordVisibilityregister2('password_confirmation', this)"></i>
+                                onclick="togglePasswordVisibilityregis('password_confirmation', this)"></i>
                         </div>
 
                         <span>Password minimal 8 karakter terdiri simbol,
@@ -57,7 +57,7 @@
     </div>
 </div>
 <script>
-    function togglePasswordVisibilityregister1(inputId, iconElement) {
+    function togglePasswordVisibilityregis(inputId, iconElement) {
         const passwordField = document.getElementById(inputId);
 
         if (passwordField.type === 'password') {
@@ -70,19 +70,6 @@
             iconElement.classList.add('bi-eye');
         }
     }
-
-    function togglePasswordVisibilityregister2(inputId, iconElement) {
-        const passwordField = document.getElementById(inputId);
-
-        if (passwordField.type === 'password_confirmation') {
-            passwordField.type = 'text';
-            iconElement.classList.remove('bi-eye');
-            iconElement.classList.add('bi-eye-slash');
-        } else {
-            passwordField.type = 'password_confirmation';
-            iconElement.classList.remove('bi-eye-slash');
-            iconElement.classList.add('bi-eye');
-        }
-    }
 </script>
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
