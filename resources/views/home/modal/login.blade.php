5e0c19322fbe8b3b7cba1d37aff81e04a89bb045
Bayu <bayurz1001@gmail.com>
2024-12-11 21:01:20 +0700

    up

--- resources/views/home/modal/login.blade.php
+++ resources/views/home/modal/login.blade.php
@@ -63,12 +63,12 @@
 
         if (passwordField.type === 'password') {
             passwordField.type = 'text';
-            toggleIcon.classList.remove('bi-eye-slash ');
-            toggleIcon.classList.add('bi-eye');
+            toggleIcon.classList.remove('bi-eye ');
+            toggleIcon.classList.add('bi-eye-slash');
         } else {
             passwordField.type = 'password';
-            toggleIcon.classList.remove('bi-eye');
-            toggleIcon.classList.add('bi-eye-slash');
+            toggleIcon.classList.remove('bi-eye-slash');
+            toggleIcon.classList.add('bi-eye');
         }
     }
 </script>
