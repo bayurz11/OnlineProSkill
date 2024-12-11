4e038b084951413c7e0aebaea566f118436c1567
Bayu <bayurz1001@gmail.com>
2024-12-11 14:04:13 +0700

    up

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -323,7 +323,7 @@
 
             <div class="all-courses-btn mt-30">
                 <div class="tg-button-wrap justify-content-center">
-                    <a href="{{ route('search') }}" class="btn arrow-btn">Lihat Semua Kelas <img
+                    <a href="{{ route('search') }}" class="btn arrow-btn">Lihat Semua Kelas & Produk<img
                             src="public/assets/img/icons/right_arrow.svg" alt="img" class="injectable"></a>
                 </div>
             </div>
