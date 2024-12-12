2e828d0a987b0da3ecb3c927919bb65a92b24573
Bayu <bayurz1001@gmail.com>
2024-12-12 16:23:19 +0700

    up

--- resources/views/home/modal/register.blade.php
+++ resources/views/home/modal/register.blade.php
@@ -24,12 +24,20 @@
                                 maxlength="12">
                         </div>
                         <div class="form-grp">
-                            <input type="password" id="password" placeholder="Password" name="password">
+                            <input type="password" id="password" placeholder="Password" name="password"
+                                class="password-field">
+                            <button type="button" id="togglePassword" class="toggle-password">
+                                <i class="fa fa-eye"></i> <!-- Icon mata -->
+                            </button>
                         </div>
                         <div class="form-grp">
                             <input type="password" id="password_confirmation" placeholder="Konfirmasi Password"
-                                name="password_confirmation">
+                                name="password_confirmation" class="password-field">
+                            <button type="button" id="togglePasswordConfirmation" class="toggle-password">
+                                <i class="fa fa-eye"></i> <!-- Icon mata -->
+                            </button>
                         </div>
+
                         <span>Password minimal 8 karakter terdiri simbol,
                             huruf, dan angka</span>
                         <button class="g-recaptcha btn btn-two arrow-btn"
@@ -49,3 +57,45 @@
         </div>
     </div>
 </div>
+<style>
+    .toggle-password {
+        position: absolute;
+        right: 10px;
+        top: 50%;
+        transform: translateY(-50%);
+        background: none;
+        border: none;
+        cursor: pointer;
+    }
+
+    .password-field {
+        position: relative;
+        padding-right: 40px;
+        /* Memberikan ruang untuk tombol toggle */
+    }
+</style>
+<script>
+    // Menangani toggle untuk password
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordField = document.getElementById('password');
+    togglePassword.addEventListener('click', function() {
+        // Cek tipe input password
+        const type = passwordField.type === 'password' ? 'text' : 'password';
+        passwordField.type = type;
+        // Ganti ikon mata
+        togglePassword.innerHTML = type === 'password' ? '<i class="fa fa-eye"></i>' :
+            '<i class="fa fa-eye-slash"></i>';
+    });
+
+    // Menangani toggle untuk password_confirmation
+    const togglePasswordConfirmation = document.getElementById('togglePasswordConfirmation');
+    const passwordConfirmationField = document.getElementById('password_confirmation');
+    togglePasswordConfirmation.addEventListener('click', function() {
+        // Cek tipe input password_confirmation
+        const type = passwordConfirmationField.type === 'password' ? 'text' : 'password';
+        passwordConfirmationField.type = type;
+        // Ganti ikon mata
+        togglePasswordConfirmation.innerHTML = type === 'password' ? '<i class="fa fa-eye"></i>' :
+            '<i class="fa fa-eye-slash"></i>';
+    });
+</script>
