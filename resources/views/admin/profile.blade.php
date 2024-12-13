901df00933092e4bdbf5990cf84c8f32af33f0d3
Bayu <bayurz1001@gmail.com>
2024-12-13 10:25:49 +0700

    up

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -105,3 +105,19 @@
         </div>
     </div>
 @endsection
+<script>
+    document.getElementById('profile_picture').addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        const imgElement = document.querySelector('.position-relative img');
+
+        if (file) {
+            const reader = new FileReader();
+
+            reader.onload = function(e) {
+                imgElement.src = e.target.result;
+            }
+
+            reader.readAsDataURL(file);
+        }
+    });
+</script>
